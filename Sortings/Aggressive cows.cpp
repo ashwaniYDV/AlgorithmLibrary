d8b18f1d50eb4d3717e81a8eeb75f5e6ae5dadc5
Ashwani Yadav <ashyadavash@gmail.com>
2021-01-09 04:05:55 +0530

    Create Aggressive cows.cpp

---
+++ Sortings/Aggressive cows.cpp
@@ -0,0 +1,72 @@
+// https://www.spoj.com/problems/AGGRCOW/
+// https://youtu.be/SiE1XFhYoaA
+
+
+#include<bits/stdc++.h>
+using namespace std;
+ 
+typedef long long int ll;
+#define int long long int
+#define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define FRE freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
+#define PRECISION(x) cout << setprecision(x); cout << fixed;
+#define debug(x) cout << #x << " is: " << (x) << endl;
+#define debugx(x) cout << #x << " is: " << (x) << endl; exit(0);
+#define f(i,n) for(int i=0;i<n;i++)
+#define fa(i,a,n) for(int i=a;i<n;i++)
+#define loop(i,a, n) for(int i = a; i <= n; i++)
+#define loopb(i,a, n) for(int i = a; i >= n; i--)
+#define pb push_back
+#define pf push_front
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define uniq(v) (v).erase(unique(all(v)),(v).end())
+#define PI 3.1415926535897932384626
+#define MOD 1000000007
+#define INF 1000111000111000111LL
+// comment below line in interactive mode (since endl flushes stdout)
+#define endl "\n"
+typedef vector<vector<int>> matrix;
+int dx[] = {0, 1, 0, -1, -1, 1, -1, 1};
+int dy[] = {-1, 0, 1, 0, -1, 1, 1, -1};
+const int MAXN = 5e5+5;
+const int N = 1e5+5;
+int n, m, k;
+
+
+void solve() {
+    int x, y, c;
+    cin >> n >> c;
+    vector<int> a(n);
+    f(i, n) cin >> a[i];
+
+    sort(all(a));
+
+    int low = 0, high = a[n-1], mid, best = 0;
+    while(low <= high){
+        mid = (low + high) / 2;
+        int cnt = 1, left = 0;
+        for(int i = 1; i < n; i ++){
+            if(a[i] - a[left] >= mid)
+                left = i, cnt ++;
+        }
+        if(cnt >= c){
+            best = mid;
+            low = mid + 1;
+        }
+        else{
+            high = mid - 1;
+        }
+    }
+    cout << best << endl;
+}
+
+int32_t main() {
+    IOS
+    int T = 1;
+    cin >> T;
+    while(T--)
+    solve();
+    return 0;
+}
