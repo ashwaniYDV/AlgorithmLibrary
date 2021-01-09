fcc5eb453098e7ce1c795046b0d920d9c0719c4a
Ashwani Yadav <ashyadavash@gmail.com>
2021-01-10 04:37:19 +0530

    Create Extended Euclidean Algorithm.cpp

---
+++ Number Theory/Extended Euclidean Algorithm.cpp
@@ -0,0 +1,63 @@
+// https://cp-algorithms.com/algebra/extended-euclid-algorithm.html
+// https://www.hackerrank.com/contests/test-contest-47/challenges/m158-multiple-euclid/problem
+
+#include<bits/stdc++.h>
+using namespace std;
+ 
+typedef long long int ll;
+#define int long long int
+#define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define FRE freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
+#define PRECISION(x) cout << setprecision(x); cout << fixed;
+#define debug(x) cout << #x << " is: " << (x) << endl;
+#define debugx(x) cout << #x << " is: " << (x) << endl; exit(0);
+#define f(i,n) for(int i=0;i<n;i++)
+#define fa(i,a,n) for(int i=a;i<n;i++)
+#define loop(i,a, n) for(int i = a; i <= n; i++)
+#define loopb(i,a, n) for(int i = a; i >= n; i--)
+#define pb push_back
+#define pf push_front
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define uniq(v) (v).erase(unique(all(v)),(v).end())
+#define PI 3.1415926535897932384626
+#define MOD 1000000007
+#define INF 1000111000111000111LL
+// comment below line in interactive mode (since endl flushes stdout)
+#define endl "\n"
+typedef vector<vector<int>> matrix;
+int dx[] = {0, 1, 0, -1, -1, 1, -1, 1};
+int dy[] = {-1, 0, 1, 0, -1, 1, 1, -1};
+const int MAXN = 5e5+5;
+const int N = 1e6+5;
+int n, m;
+
+int extended_euclid(int a, int b, int& x, int& y) {
+    if (b == 0) {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    int x1, y1;
+    int d = extended_euclid(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - y1 * (a / b);
+    return d;
+}
+
+void solve() {
+    int x, y, z, a, b;
+    cin >> a >> b;
+    z = extended_euclid(a, b, x, y);
+    cout << x << " " << y << endl;
+}
+
+int32_t main() {
+    IOS
+    int T = 1;
+    // cin >> T;
+    while(T--)
+    solve();
+    return 0;
+}
