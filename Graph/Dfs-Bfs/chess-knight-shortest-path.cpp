e88ba867f0cf9b3a7f0948999e40b12133863d6a
Ashwani Yadav <ashyadavash@gmail.com>
2021-01-13 18:16:38 +0530

    Update chess-knight-shortest-path.cpp

--- Graph/Dfs-Bfs/chess-knight-shortest-path.cpp
+++ Graph/Dfs-Bfs/chess-knight-shortest-path.cpp
@@ -1,91 +1,100 @@
-// https://www.techiedelight.com/chess-knight-problem-find-shortest-path-source-destination/
+// https://www.geeksforgeeks.org/minimum-steps-reach-target-knight/
 
-#include <iostream>
 #include<bits/stdc++.h>
-#include<vector>
 using namespace std;
- 
-#define ll long long int
+
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
+using namespace __gnu_pbds;
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+typedef long long int ll;
+#define int long long int
 #define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 #define FRE freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
-#define f(i,n) for(ll i=0;i<n;i++)
-#define fa(i,a,n) for(ll i=a;a<n?i<n:i>n;a<n?i+=1:i-=1)
+#define PRECISION(x) cout << setprecision(x); cout << fixed;
+#define debug(x) cout << #x << " is: " << (x) << endl;
+#define debugx(x) cout << #x << " is: " << (x) << endl; exit(0);
+#define f(i,n) for(int i=0;i<n;i++)
+#define fa(i,a,n) for(int i=a;i<n;i++)
+#define loop(i,a, n) for(int i = a; i <= n; i++)
+#define loopb(i,a, n) for(int i = a; i >= n; i--)
 #define pb push_back
+#define pf push_front
 #define F first
 #define S second
 #define all(x) x.begin(), x.end()
-#define clr(x) memset(x, 0, sizeof(x))
-#define sortall(x) sort(all(x))
+#define uniq(v) (v).erase(unique(all(v)),(v).end())
+#define ppc __builtin_popcount
+#define ppcll __builtin_popcountll
 #define PI 3.1415926535897932384626
 #define MOD 1000000007
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-typedef vector<int>	vi;
-typedef vector<ll> vl;
-typedef vector<pii> vpii;
-typedef vector<pll> vpll;
-typedef vector<vi> vvi;
-typedef vector<vl> vvl;
-typedef long double ld;
+#define MOD2 998244353
+#define INF 1000111000111000111LL
+// comment below line in interactive mode (since endl flushes stdout)
+#define endl "\n"
+typedef vector<vector<int>> matrix;
+int dx[] = {0, 1, 0, -1, -1, 1, -1, 1};
+int dy[] = {-1, 0, 1, 0, -1, 1, 1, -1};
+const int MAXN = 1e5+5;
+const int N = 1e5+5;
+int n, m;
 
-const ll mod = 1000000007;
-const ll N = 1e6+5, M = N, ninf = -2e5, inf = (int)1e9;
-vl g[N], dist;
-ll a[N], b[N], dp[N], in[N], out[N], vis[N], level[N];
-ll n, m;
-
-
-ll row[]={2,2,-2,-2,1,1,-1,-1};
-ll col[]={-1,1,1,-1,2,-2,2,-2};
-
-bool valid(ll x, ll y){
-	if(x<0||y<0||x>=8||y>=8) return false;
-	return true;
-}
+int row[] = {2, 2, -2, -2, 1, 1, -1, -1};
+int col[] = {-1, 1, 1, -1, 2, -2, 2, -2};
 
 struct Node {
-    ll x, y, dist;
-    Node(ll x, ll y, ll dist=0): x(x), y(y), dist(dist) {}
-    bool operator<(const Node& ob) const{
-    	return x<ob.x || (x==ob.x && y<ob.y); 
-    }
+    int x, y, dist;
+    Node(int x, int y, int dist = 0): x(x), y(y), dist(dist) {}
 };
 
-ll bfs(Node src, Node dest){
-	map<Node, ll> visited;
-	queue<Node>q;
-	q.push(src);
-	while(!q.empty()){
-		Node node = q.front();
-		q.pop();
-		ll x=node.x;
-		ll y=node.y;
-		ll dist=node.dist;
-
-		if(x==dest.x && y==dest.y){
-			return dist;
-		}
-
-		if(!visited[node]){
-			visited[node]=1;
-			f(i,8){
-				ll x1=x+row[i];
-				ll y1=y+col[i];
-				if(valid(x1, y1)){
-					q.push({x1, y1, dist+1});
-				}
-			}
-		}
-	}
-	return -1;
+bool valid(int x, int y) {
+    if(x < 0 || y < 0 || x >= 8 || y >= 8) return false;
+    return true;
 }
 
+int bfs(pair<int, int> src, pair<int, int> dest) {
+    map<pair<int, int>, int> visited;
+    queue<Node> q;
+    q.push({src.F, src.S, 0});
+    visited[src] = 1;
+    while(!q.empty()){
+        Node node = q.front();
+        q.pop();
+        int x = node.x;
+        int y = node.y;
+        int dist = node.dist;
 
-int32_t main()
-{
-	IOS
-	ll t,k,x,y,z,p,q,u,v,ct=0,flag=0;
-	Node src={0,7};
-	Node dest={7,0};
-	cout<<bfs(src, dest);
+        if(x == dest.F && y == dest.S){
+            return dist;
+        }
+
+        f(i, 8) {
+            int x1 = x + row[i];
+            int y1 = y + col[i];
+            if(!visited[{x1, y1}] && valid(x1, y1)) {
+                q.push({x1, y1, dist+1});
+                visited[{x1, y1}] = 1;
+            }
+        }
+    }
+    return -1;
+}
+
+void solve() {
+    int u, v;
+    pair<int, int> src = {4, 5};
+    pair<int, int> dest = {1, 1};
+    // pair<int, int> src = {0, 7};
+    // pair<int, int> dest = {7, 0};
+    cout << bfs(src, dest);
+}
+
+int32_t main() {
+    IOS
+    int T = 1;
+    // cin >> T;
+    while(T--)
+    solve();
+    return 0;
 }
