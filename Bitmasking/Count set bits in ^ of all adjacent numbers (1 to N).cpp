bf13bdfdb2b854e04f9c09a16dddb03d6de635bf
Ashwani Yadav <ashyadavash@gmail.com>
2021-01-16 00:26:49 +0530

    Create Count set bits in ^ of all adjacent numbers (1 to N).cpp

---
+++ Bitmasking/Count set bits in ^ of all adjacent numbers (1 to N).cpp
@@ -0,0 +1,52 @@
+// https://www.geeksforgeeks.org/count-set-bits-in-bitwise-xor-of-all-adjacent-elements-upto-n/
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// Funtion to count of set bits in Bitwise XOR of adjacent elements up to N
+
+int countXORSetBitsAdjElemRange1_N(int N) {
+
+    // Stores count of set bits by Bitwise XOR on adjacent elements of [0, N]
+
+    int total_set_bits = 0;
+ 
+
+    // Stores all possible values on right most set bit over [0, N]
+
+    int bit_Position = 1;
+ 
+
+    // Iterate over the range [0, N]
+
+    while (N) {
+
+        total_set_bits += ((N + 1) / 2 * bit_Position);
+ 
+
+        // Update N
+
+        N -= (N + 1) / 2;
+ 
+
+        // Update bit_Position
+
+        bit_Position++;
+
+    }
+ 
+
+    return total_set_bits;
+}
+
+
+int main() {
+
+    int N = 4;
+ 
+
+    cout << countXORSetBitsAdjElemRange1_N(N);
+ 
+
+    return 0;
+}
