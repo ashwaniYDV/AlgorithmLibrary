c7d819ce06ae6b50d02d0afb9b5b7e50967744f3
Ashwani Yadav <ashyadavash@gmail.com>
2021-01-18 16:46:28 +0530

    Update Can You Handle the Weights!.cpp

--- Number Theory/Questions/Can You Handle the Weights!.cpp
+++ Number Theory/Questions/Can You Handle the Weights!.cpp
@@ -141,3 +141,119 @@ int32_t main() {
     solve();
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Method 2: Efficient for number of non negative solutions of equation [ax+by=d] 
+// https://youtu.be/gFgRYepIXDY
+
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
+using namespace __gnu_pbds;
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+ 
+typedef long long int ll;
+#define int long long int
+#define ld long double
+#define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define FRE freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
+#define PRECISION(x) cout << setprecision(x); cout << fixed;
+#define debug(x) cout << #x << " is: " << (x) << endl;
+#define debugx(x) cout << #x << " is: " << (x) << endl; exit(0);
+#define f(i,n) for(int i=0;i<n;i++)
+#define fa(i,a,n) for(int i=a;i<n;i++)
+#define loop(i,a, n) for(int i = a; i <= n; i++)
+#define loopb(i,a, n) for(int i = a; i >= n; i--)
+#define pb push_back
+#define pf push_front
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define uniq(v) (v).erase(unique(all(v)),(v).end())
+#define ppc __builtin_popcount
+#define ppcll __builtin_popcountll
+#define PI 3.1415926535897932384626
+#define MOD 1000000007
+#define MOD2 998244353
+#define INT_INF 1011111111
+#define INF 1000111000111000111LL
+// comment below line in interactive mode (since endl flushes stdout)
+#define endl "\n"
+typedef vector<vector<int>> matrix;
+int dx[] = {0, 1, 0, -1, -1, 1, -1, 1};
+int dy[] = {-1, 0, 1, 0, -1, 1, 1, -1};
+const int MAXN = 1e5+5;
+const int N = 2e5+5;
+int n, m;
+
+int extended_euclidean(int a, int b, int& x, int& y) {
+    if (b == 0) {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    int x1, y1;
+    int d = extended_euclidean(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - y1 * (a / b);
+    return d;
+}
+
+int modInverse(int a, int m) {
+    int x, y;
+    int g = extended_euclidean(a, m, x, y);
+    x = (x % m + m) % m;
+    return x;
+}
+
+void solve() {
+    ll x, y, z, a, b, d, g;
+    cin >> a >> b >> d;
+    // a*x + b*y = d
+
+    if(d == 0) {
+        cout << 1 << endl;
+        return;
+    }
+
+    g = extended_euclidean(a, b, x, y);
+
+    if(d % g) {
+        cout << 0 << endl;
+        return;
+    }
+
+    int min_y = ((d % a) * modInverse(b, a)) % a;
+
+    if(d < min_y * b) {
+        cout << 0 << endl;
+        return;
+    }
+
+    n = ((d / b) - min_y) / a;
+    cout << n + 1 << endl;
+}
+ 
+int32_t main() {
+    IOS
+    int T = 1;
+    cin >> T;
+    while(T--)
+    solve();
+    return 0;
+}
