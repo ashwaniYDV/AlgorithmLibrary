53f55ca28a546fb8f3cc9c5e2e8764843c6da01b
Ashwani Yadav <ashyadavash@gmail.com>
2021-01-20 03:05:40 +0530

    Update Chicken_McNugget_Theorem.cpp

--- Number Theory/Chicken_McNugget_Theorem.cpp
+++ Number Theory/Chicken_McNugget_Theorem.cpp
@@ -1,6 +1,5 @@
-// https://www.codechef.com/problems/COPR16G
-// https://youtu.be/0Snuq77afU4
 // https://artofproblemsolving.com/wiki/index.php/Chicken_McNugget_Theorem
+// https://youtu.be/0Snuq77afU4
 
 /* Chicken_McNugget_Theorem
 --------------------------------
@@ -14,49 +13,8 @@ The proof is based on the fact that in each pair of the form (k, a*b-a-b-k), exa
 */
 
 
-#include<bits/stdc++.h>
-using namespace std;
- 
-#include <ext/pb_ds/assoc_container.hpp> 
-#include <ext/pb_ds/tree_policy.hpp> 
-using namespace __gnu_pbds;
-typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
- 
-typedef long long int ll;
-#define int long long int
-#define ld long double
-#define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-#define FRE freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
-#define PRECISION(x) cout << setprecision(x); cout << fixed;
-#define debug(x) cout << #x << " is: " << (x) << endl;
-#define debugx(x) cout << #x << " is: " << (x) << endl; exit(0);
-#define f(i,n) for(int i=0;i<n;i++)
-#define fa(i,a,n) for(int i=a;i<n;i++)
-#define loop(i,a, n) for(int i = a; i <= n; i++)
-#define loopb(i,a, n) for(int i = a; i >= n; i--)
-#define pb push_back
-#define pf push_front
-#define F first
-#define S second
-#define all(x) x.begin(), x.end()
-#define uniq(v) (v).erase(unique(all(v)),(v).end())
-#define ppc __builtin_popcount
-#define ppcll __builtin_popcountll
-#define PI 3.1415926535897932384626
-#define MOD 1000000007
-#define MOD2 998244353
-#define INT_INF 1011111111
-#define INF 1000111000111000111LL
-// comment below line in interactive mode (since endl flushes stdout)
-#define endl "\n"
-typedef vector<vector<int>> matrix;
-int dx[] = {0, 1, 0, -1, -1, 1, -1, 1};
-int dy[] = {-1, 0, 1, 0, -1, 1, 1, -1};
-const int MAXN = 1e5+5;
-const int N = 2e5+5;
-int n, m;
 
-
+// https://www.codechef.com/problems/COPR16G
 void solve() {
     int a, b;
     cin >> a >> b;
@@ -68,7 +26,6 @@ void solve() {
     n = a*b - a - b + 1;
     cout << n << endl;
 }
- 
 int32_t main() {
     IOS
     int T = 1;
@@ -76,4 +33,27 @@ int32_t main() {
     while(T--)
     solve();
     return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// https://codeforces.com/contest/1245/problem/A
+
+void solve() {
+    int x, y;
+    cin >> x >> y;
+    if(__gcd(x, y) == 1) {
+        cout << "Finite\n";
+    } else {
+        cout << "Infinite\n";
+    }
 }
