2f9c03d4bea19c0846af25056e9f3d445854f924
Ashwani Yadav <ashyadavash@gmail.com>
2021-01-20 16:26:37 +0530

    Create Unique Binary Search Trees.cpp

---
+++ Interviews/DS/BST/Unique Binary Search Trees.cpp
@@ -0,0 +1,50 @@
+// https://leetcode.com/problems/unique-binary-search-trees/
+
+// MEthod 1: Using Catalan numbers
+class Solution {
+    int catalan_Number(int n)
+    {
+        int catalan[n+1];
+        catalan[0] = catalan[1] = 1;
+        
+        for(int i=2;i<=n;++i)
+        {
+            catalan[i] = 0;
+            for(int j=0;j<i;++j)
+                catalan[i] += catalan[j]*catalan[i-j-1];
+        }
+        return catalan[n];
+    }
+public:
+    int numTrees(int n) {
+        return catalan_Number(n);
+    }
+};
+
+// MEthod 2: "Using Unique Binary Search Trees II" problem technique
+class Solution {
+public:
+    int helper(int start, int end, vector<vector<int>>& dp) {
+        if(start > end) {
+            return 1;
+        }
+        
+        if(dp[start][end] != -1) return dp[start][end];
+        
+        int res = 0;
+        for(int i = start; i <= end; i++) {
+            int left = helper(start, i-1, dp);
+            int right = helper(i+1, end, dp);
+            
+            res += left * right;
+        }
+        
+        return dp[start][end] = res;   
+    }
+    int numTrees(int n) {
+        if(n == 0) return 0;
+        
+        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
+        return helper(1, n, dp);
+    }
+};
