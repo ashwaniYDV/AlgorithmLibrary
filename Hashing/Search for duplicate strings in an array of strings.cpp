1421eb34df047f86362cc2583d8e6f6b60f4fdac
Ashwani Yadav <ashyadavash@gmail.com>
2021-01-22 20:36:24 +0530

    Create Search for duplicate strings in an array of strings.cpp

---
+++ Hashing/Search for duplicate strings in an array of strings.cpp
@@ -0,0 +1,118 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
+using namespace __gnu_pbds;
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+typedef long long int ll;
+#define int long long int
+#define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define FRE freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
+#define PRECISION(x) cout << setprecision(x); cout << fixed;
+#define debug(x) cout << #x << " is: " << (x) << endl;
+#define debugx(x) cout << #x << " is: " << (x) << endl; exit(0);
+#define f(i,n) for(int i=0;i<n;i++)
+#define fa(i,a,n) for(int i=a;i<n;i++)
+#define loop(i,a, n) for(int i = a; i <= n; i++)
+#define loopb(i,a, n) for(int i = a; i >= n; i--)
+#define pb push_back
+#define pf push_front
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define uniq(v) (v).erase(unique(all(v)),(v).end())
+#define ppc __builtin_popcount
+#define ppcll __builtin_popcountll
+#define PI 3.1415926535897932384626
+#define MOD 1000000007
+#define MOD2 998244353
+#define INT_INF 1011111111
+#define INF 1000111000111000111LL
+// comment below line in interactive mode (since endl flushes stdout)
+#define endl "\n"
+typedef vector<vector<int>> matrix;
+int dx[] = {0, 1, 0, -1, -1, 1, -1, 1};
+int dy[] = {-1, 0, 1, 0, -1, 1, 1, -1};
+const int MAXN = 1e5+5;
+const int N = 1e5;
+int n, m;
+
+int compute_hash(string const& s) {
+    const int p = 31;
+    const int m = 1e9 + 9;
+    int hash_value = 0;
+    int p_pow = 1;
+    for (char c : s) {
+        hash_value = (hash_value + (c - 'a' + 1) * p_pow) % m;
+        p_pow = (p_pow * p) % m;
+    }
+    return hash_value;
+}
+
+vector<vector<int>> group_identical_strings(vector<string> const& s) {
+    int n = s.size();
+    vector<pair<long long, int>> hashes(n);
+    for (int i = 0; i < n; i++)
+        hashes[i] = {compute_hash(s[i]), i};
+
+    sort(hashes.begin(), hashes.end());
+
+    vector<vector<int>> groups;
+    for (int i = 0; i < n; i++) {
+        if (i == 0 || hashes[i].first != hashes[i-1].first) {
+            // add empty vector
+            groups.emplace_back();
+        }
+        groups.back().push_back(hashes[i].second);
+    }
+    return groups;
+}
+
+void solve() {
+    cin >> n;
+    vector<string> s(n);
+    f(i, n) cin >> s[i];
+    vector<vector<int>> res = group_identical_strings(s);
+    for(auto i: res) {
+        cout << "Pos:\n";
+        for(auto j: i) cout << j << " ";
+        cout << "\nStr:\n";
+        for(auto j: i) cout << s[j] << " ";
+        cout << endl << endl;
+    }
+}
+
+int32_t main() {
+    IOS
+    int T = 1;
+    // cin >> T;
+    while(T--)
+    solve();
+    return 0;
+}
+
+/*
+Input
+--------
+5
+aa ab bb ab aa
+
+Output
+--------
+Pos:
+0 4 
+Str:
+aa aa 
+
+Pos:
+1 3 
+Str:
+ab ab 
+
+Pos:
+2 
+Str:
+bb 
+*/
