765bd19fe6ba90adada0cda5220a4a4e2d459c80
Ashwani Yadav <ashyadavash@gmail.com>
2021-01-23 05:05:19 +0530

    Update and rename count_unique_substrings.cpp to count_unique_substrings(cp-algorithms).cpp

--- Hashing/count_unique_substrings(cp-algorithms).cpp
+++ Hashing/count_unique_substrings(cp-algorithms).cpp
@@ -44,16 +44,16 @@ int n, m;
 int count_unique_substrings(string const& s) {
     int n = s.size();
     const int p = 31;
-    const int m = 1e9 + 9;
+    const int mod = 1e9 + 9;
 
     vector<int> p_pow(n);
     p_pow[0] = 1;
     for (int i = 1; i < n; i++)
-        p_pow[i] = (p_pow[i-1] * p) % m;
+        p_pow[i] = (p_pow[i-1] * p) % mod;
 
     vector<int> h(n + 1, 0);
     for (int i = 0; i < n; i++) {
-        h[i+1] = (h[i] + (s[i] - 'a' + 1) * p_pow[i]) % m;
+        h[i+1] = (h[i] + (s[i] - 'a' + 1) * p_pow[i]) % mod;
     }
 
     // for substring s(i, i+l-1): left=i, right=i+l-1
@@ -66,8 +66,8 @@ int count_unique_substrings(string const& s) {
     for (int l = 1; l <= n; l++) {
         set<int> hs;
         for (int i = 0; i <= n - l; i++) {
-            long long cur_h = (h[i + l] - h[i] + m) % m;
-            cur_h = (cur_h * p_pow[n-1-i]) % m;
+            long long cur_h = (h[i + l] - h[i] + mod) % mod;
+            cur_h = (cur_h * p_pow[n-1-i]) % mod;
             hs.insert(cur_h);
         }
         cnt += hs.size();
