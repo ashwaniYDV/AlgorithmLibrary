07a985426210f7af92a502bde77d671921658b5a
Ashwani Yadav <ashyadavash@gmail.com>
2021-01-27 04:24:23 +0530

    Update Pair with max GCD.cpp

--- Number Theory/Pair with max GCD.cpp
+++ Number Theory/Pair with max GCD.cpp
@@ -34,11 +34,50 @@ void solve() {
     
 }
 
-int32_t main() {
-    IOS
-    int T = 1;
-    // cin >> T;
-    while(T--)
-    solve();
-    return 0;
+
+
+
+
+
+
+
+
+
+
+
+// O(N.log(N))
+/*
+This approach is based on the idea of Sieve Of Eratosthenes.
+
+First let’s solve a simpler problem, given a value X we have to tell whether a pair has a GCD equal to X. 
+This can be done by checking that how many elements in the array are multiples of X. 
+If the number of such multiples is greater than 1, then X will be a GCD of some pair.
+Now for pair with maximum GCD, we maintain a count array of the original array. 
+Our method is based on the above problem with Sieve-like approach for loop.
+*/
+void solve() {
+	ll x, y, z, k, sum = 0, d;
+	vi a = { 1, 2, 4, 8, 8, 12 };
+	n = a.size();
+
+	int mx = 0;
+	f(i, n) mx = max(mx, a[i]);
+
+	int cnt[mx+1] = {0};
+
+	f(i, n) cnt[a[i]]++;
+
+	int res = 0;
+	for (int i = 1; i <= mx; i++) {
+		int k = 0; // count of numbers that are divisible by i
+	   	for (int j = i; j <= mx; j += i) {
+			k += cnt[j];
+	   	}
+
+	   	//if count of number more than one, 
+	   	//then we have two numbers with a divider i, 
+	   	//and it is not necessarily maximum divisor of these numbers.
+	   	if (k >= 2) res = max(res, i);
+	}
+	cout << res << endl;
 }
