7d84713234048f39d62ac937be5a42fa02218293
Ashwani Yadav <ashyadavash@gmail.com>
2021-01-29 18:22:32 +0530

    Create Complementary graph - Connected Components.cpp

---
+++ Graph/Complementary graph - Connected Components.cpp
@@ -0,0 +1,121 @@
+/*
+https://codeforces.com/contest/920/problem/E
+Que - Find number of connected components in the original graph if complementary graph is given of the original graph
+Time - O(n*log(n))
+*/
+
+
+// Method 1 (DFS)
+
+const int N = 2e5+5;
+int n, m;
+set<int> unvisited;
+set<int> g[N];
+int cnt = 0;
+
+void dfs(int u) {
+	cnt++;
+	unvisited.erase(u);
+	vector<int> tmp;
+
+	for(int x: unvisited) {
+        if(g[u].count(x) == 0) {
+            tmp.pb(x);
+        }
+    }
+    for(int v: tmp) {
+        unvisited.erase(v);
+    }
+    for(int v: tmp) {
+        dfs(v);
+    }
+}
+
+void solve() {
+	int u, v;
+    cin >> n >> m;
+
+    f(i, m) cin >> u >> v, g[u].insert(v), g[v].insert(u);
+
+    vector<int> res;
+
+    for(int i = 1; i <= n; i++) {
+        unvisited.insert(i);
+    }
+    for(int i = 1; i <= n; i++) { 
+        if(unvisited.count(i)) {
+            cnt = 0;
+            dfs(i);
+            res.pb(cnt);
+        }
+    }
+
+    sort(all(res));
+    cout << res.size() << endl;
+    for(auto i: res) cout << i <<' ';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// Method 2 (BFS)
+const int N = 2e5+5;
+int n, m;
+set<int> unvisited;
+set<int> g[N];
+int cnt = 0;
+
+void bfs(int node){
+    queue <int> q;
+    q.push(node);
+    unvisited.erase(node);
+
+    while(!q.empty()) {
+        int u = q.front();
+        q.pop();
+        cnt++;
+        vector<int> tmp;
+
+        for(int x: unvisited) {
+            if(g[u].count(x) == 0) {
+                tmp.pb(x);
+                q.push(x);
+            }
+        }
+        for(int v: tmp) {
+            unvisited.erase(v);
+        }
+    }
+}
+
+void solve() {
+	int u, v;
+    cin >> n >> m;
+
+    f(i, m) cin >> u >> v, g[u].insert(v), g[v].insert(u);
+
+    vector<int> res;
+
+    for(int i = 1; i <= n; i++) {
+        unvisited.insert(i);
+    }
+    for(int i = 1; i <= n; i++) { 
+        if(unvisited.count(i)) {
+            cnt = 0;
+            bfs(i);
+            res.pb(cnt);
+        }
+    }
+
+    sort(all(res));
+    cout << res.size() << endl;
+    for(auto i: res) cout << i <<' ';
+}
