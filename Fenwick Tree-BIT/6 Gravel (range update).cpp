4dba02b7780400f34b5b3334e0f963a3bc9daf35
Ashwani Yadav <ashyadavash@gmail.com>
2021-02-03 04:41:59 +0530

    Update 6 Gravel (range update).cpp

--- Fenwick Tree-BIT/6 Gravel (range update).cpp
+++ Fenwick Tree-BIT/6 Gravel (range update).cpp
@@ -1,6 +1,11 @@
 // https://youtu.be/WmDEZbTHN5c
 // https://www.codechef.com/problems/SPREAD
 
+/*
+Note this question cannot be solved by using normal range update/query segment tree.
+Because we have to update range [l, ...,r], but query of point should add the update only once (i.e. x) and not (r-l+1)*x 
+*/
+
 const int N = 1e6+5;
 int n, m, k;
  
