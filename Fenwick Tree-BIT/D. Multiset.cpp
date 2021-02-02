bfd0f0660db3e2e1f371ab55cec24b70d0310efc
Ashwani Yadav <ashyadavash@gmail.com>
2021-02-02 15:31:22 +0530

    Update D. Multiset.cpp

--- Fenwick Tree-BIT/D. Multiset.cpp
+++ Fenwick Tree-BIT/D. Multiset.cpp
@@ -2,47 +2,6 @@
 // https://codeforces.com/problemset/problem/1354/D
 
 // Method 1: O[N.(logN)^2]
-#include<bits/stdc++.h>
-using namespace std;
- 
-// #include <ext/pb_ds/assoc_container.hpp> 
-// #include <ext/pb_ds/tree_policy.hpp> 
-// using namespace __gnu_pbds;
-// typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
- 
-typedef long long int ll;
-#define int long long int
-#define ld long double
-#define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-#define FRE freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
-#define PRECISION(x) cout << setprecision(x); cout << fixed;
-#define debug(x) cout << #x << " is: " << (x) << endl;
-#define debug2(x,y) cerr << (#x) <<", "<<(#y)<< " are: " << (x) <<", "<<(y)<< endl;
-#define debugx(x) cout << #x << " is: " << (x) << endl; exit(0);
-#define kickstart(testcase, res) cout << "Case #" << testcase << ": " << res << endl;
-#define f(i,n) for(int i=0;i<n;i++)
-#define fa(i,a,n) for(int i=a;i<n;i++)
-#define loop(i,a, n) for(int i = a; i <= n; i++)
-#define loopb(i,a, n) for(int i = a; i >= n; i--)
-#define pb push_back
-#define pf push_front
-#define F first
-#define S second
-#define all(x) x.begin(), x.end()
-#define uniq(v) (v).erase(unique(all(v)),(v).end())
-#define PI 3.1415926535897932384626
-#define MOD 1000000007
-#define MOD2 998244353
-#define INT_INF 1011111111
-#define INF 1000111000111000111LL
-// comment below line in interactive mode (since endl flushes stdout)
-#define endl "\n"
-typedef vector<int> vi;
-typedef pair<int, int> pii;
-typedef vector<vector<int>> matrix;
-int dx[] = {0, 1, 0, -1, -1, 1, -1, 1};
-int dy[] = {-1, 0, 1, 0, -1, 1, 1, -1};
-const int MAXN = 5e5+5;
 const int N = 1e6+5;
 int n, m, k;
 
@@ -106,11 +65,87 @@ void solve() {
     cout << 0 << endl;
 } 
 
-int32_t main() {
-	IOS
-	int T = 1;
-	// cin >> T;
-	while(T--)
-	solve();
-	return 0;
+
+
+
+
+
+
+
+// Method 2: O[N.(logN)^2]
+const int N = 1e6+5;
+int n, m, k;
+ 
+int BIT[N+5];
+ 
+void update(int i, int val) {
+    while(i > 0 && i < N) {
+        BIT[i] += val;
+        i += (i&-i);
+    }
 }
+ 
+int query(int i) {
+    int sum = 0;
+    while(i > 0) {
+        sum += BIT[i];
+        i -= (i&-i);
+    }
+    return sum;
+}
+ 
+void go(int k) {
+	int low = 1, high = N;
+	while(low <= high) {
+		int mid = low + (high - low) / 2;
+		int val = query(mid);
+ 
+		if(val < k) {
+			low = mid + 1;
+		} else {
+			high = mid - 1;
+		}
+	}
+ 
+	update(low, -1);
+}
+ 
+void solve() {
+	int q, x;
+    cin >> n >> q;
+    loop(i, 1, n) {
+    	cin >> x;
+    	update(x, 1);
+    }
+    while(q--) {
+    	cin >> x;
+    	if(x > 0) {
+    		update(x, 1);
+    	} else {
+    		x = -1*x;
+    		go(x);
+    	}
+    }
+
+    // output part
+    
+    int res = query(N);
+    if(!res) {
+    	cout << 0 << endl;
+    	return;
+    }
+ 
+    int low = 1, high = N;
+	while(low <= high) {
+		int mid = low + (high - low) / 2;
+		int val = query(mid);
+ 
+		if(val < res) {
+			low = mid + 1;
+		} else {
+			high = mid - 1;
+		}
+	}
+
+	cout << low << endl;
+} 
