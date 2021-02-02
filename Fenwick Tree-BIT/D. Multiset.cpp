ca78c91593219285bdc54afa648862e8b8114d6b
Ashwani Yadav <ashyadavash@gmail.com>
2021-02-02 16:06:53 +0530

    Update D. Multiset.cpp

--- Fenwick Tree-BIT/D. Multiset.cpp
+++ Fenwick Tree-BIT/D. Multiset.cpp
@@ -72,6 +72,12 @@ void solve() {
 
 
 
+
+
+
+
+
+
 // Method 2: O[N.(logN)^2]
 const int N = 1e6+5;
 int n, m, k;
@@ -137,5 +143,84 @@ void solve() {
     }
  
     int pos = go(res);
+	cout << pos << endl;
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Method 3: O[N.(logN)]   (Using binary lifting)
+const int N = 1e6+5;
+int n, m, k;
+ 
+int BIT[N+5];
+ 
+void update(int i, int val) {
+    while(i < N) {
+        BIT[i] += val;
+        i += (i&-i);
+    }
+}
+ 
+int query(int i) {
+    int sum = 0;
+    while(i > 0) {
+        sum += BIT[i];
+        i -= (i&-i);
+    }
+    return sum;
+}
+
+int prefixSumLB(int prefixSum) {
+	int curr = 0, prevSum = 0;
+	for (int i = log2(N); i >= 0; i--) {
+		if((curr + (1 << i)) < N && BIT[curr + (1 << i)] + prevSum < prefixSum) {
+			curr += (1 << i);
+			prevSum += BIT[curr];
+		}
+	}
+	return curr + 1;
+}
+
+void solve() {
+	int q, x;
+    cin >> n >> q;
+    loop(i, 1, n) {
+    	cin >> x;
+    	update(x, 1);
+    }
+    while(q--) {
+    	cin >> x;
+    	if(x > 0) {
+    		update(x, 1);
+    	} else {
+    		x = -1*x;
+    		int pos = prefixSumLB(x);
+    		update(pos, -1);
+    	}
+    }
+ 
+    // output part
+    
+    int res = query(N);
+    if(!res) {
+    	cout << 0 << endl;
+    	return;
+    }
+ 
+    int pos = prefixSumLB(res);
 	cout << pos << endl;
 } 
