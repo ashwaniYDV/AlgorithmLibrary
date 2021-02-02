6c6a9e557809240a5d70cffd6368689123d9595b
Ashwani Yadav <ashyadavash@gmail.com>
2021-02-03 04:20:42 +0530

    Create 6 Gravel (range update).cpp

---
+++ Fenwick Tree-BIT/6 Gravel (range update).cpp
@@ -0,0 +1,44 @@
+// https://youtu.be/WmDEZbTHN5c
+// https://www.codechef.com/problems/SPREAD
+
+const int N = 1e6+5;
+int n, m, k;
+ 
+int BIT[N+5];
+ 
+void update(int i, int val) {
+    while(i < N) {
+        BIT[i] += val;
+        i += (i&-i);
+    }
+}
+ 
+int query(int i) {
+    int sum = 0;
+    while(i > 0) {
+        sum += BIT[i];
+        i -= (i&-i);
+    }
+    return sum;
+}
+
+void solve() {
+	int l, r, c, x;
+	char ch;
+    cin >> n >> m >> c;
+
+    update(1, c);
+    update(n+1, -c);
+
+    while(m--) {
+    	cin >> ch;
+    	if(ch == 'Q') {
+    		cin >> x;
+    		cout << query(x) << endl;
+    	} else {
+    		cin >> l >> r >> x;
+    		update(l, x);
+    		update(r+1, -x);
+    	}
+    }
+}
