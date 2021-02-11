8efc0298f0145d839a6d5a2d04988c5ae290d462
Ashwani Yadav <ashyadavash@gmail.com>
2021-02-11 23:56:01 +0530

    Update Longest AP (given n distinct ele).cpp

--- DP/00 Standard DP Que/Longest AP (given n distinct ele).cpp
+++ DP/00 Standard DP Que/Longest AP (given n distinct ele).cpp
@@ -48,7 +48,7 @@ void solve() {
 
 
 
-
+// DP O(n^2.log(n))
 void solve() {
 	int x, y, z, d;
 	cin >> n;
@@ -75,6 +75,62 @@ void solve() {
 			x = a[i] - d;
 			int k = lower_bound(all(a), x) - a.begin();
 			if(k < n && a[k] == x) {
+				dp[i][j] = dp[k][i] + 1;
+			} else {
+				dp[i][j] = 2;
+			}
+		}
+	}
+
+	int res = 0;
+	f(i, n) {
+		f(j, n) res = max(res, dp[i][j]);
+	}
+	cout << res << endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// DP O(n^2)
+void solve() {
+	int x, y, z, d;
+	cin >> n;
+	vi a(n);
+	f(i, n) cin >> a[i];
+
+	if(n <= 2) {
+		cout << n << endl;
+		return;
+	}
+
+	sort(all(a));
+
+	vi pos(a[n-1] + 5, -1);
+	f(i, n) pos[a[i]] = i;
+
+	vector<vector<int>> dp(n, vector<int>(n, 0));
+	// dp[i][j] = longest AP which has a[i] and a[j] as second last and last ele respectively
+	// hence common difference => d = a[j] - a[i]
+	// if we find an element at position k (k < i) such that a[k] = a[i] - d  (i.e a[k], a[i], a[j] are in AP)
+	// then dp[i][j] = dp[k][i] + 1
+	// else dp[i][j] = 2
+
+	for(int i = 0; i < n; i++) {
+		for(int j = i+1; j < n; j++) {
+			d = a[j] - a[i];
+			x = a[i] - d;
+			if(x >= 0 && pos[x] != -1) {
+				int k = pos[x];
 				dp[i][j] = dp[k][i] + 1;
 			} else {
 				dp[i][j] = 2;
