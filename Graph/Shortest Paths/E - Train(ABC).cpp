5fb1cee0425047e049484aa25f2b7c2536685b07
Ashwani Yadav <ashyadavash@gmail.com>
2021-02-20 23:50:28 +0530

    Update E - Train(ABC).cpp

--- Graph/Shortest Paths/E - Train(ABC).cpp
+++ Graph/Shortest Paths/E - Train(ABC).cpp
@@ -83,54 +83,54 @@ void solve() {
 
 
 // TLE using DFS
-const int N = 1e5+5;
+const int N = 1e5 + 5;
 int n, m, x, y;
- 
-vector<pair<int, pii>>g[N];
+
+vector < pair < int, pii >> g[N];
 int vis[N];
 int res = INF;
- 
-int findNextTime(int N, int K) { 
-    int rem = (N + K) % K; 
-  
-    if (rem == 0) 
-        return N; 
+
+int findNextTime(int N, int K) {
+    int rem = (N + K) % K;
+
+    if (rem == 0)
+        return N;
     else
-        return N + K - rem; 
-} 
- 
-void dfs(int u, int time) {
-	if(u == y) {
-		res = min(res, time);
-		return;
-	}
- 
-	vis[u] = 1;
- 
-	for(auto i: g[u]) {
-		int v = i.F, t = i.S.F, k = i.S.S;
-		if(vis[v]) continue;
- 
-		int tempT = findNextTime(time, k);
- 
-		dfs(v, tempT + t);
-	}
- 
-	vis[u] = 0;
+        return N + K - rem;
 }
- 
+
+void dfs(int u, int time) {
+    if (u == y) {
+        res = min(res, time);
+        return;
+    }
+
+    vis[u] = 1;
+
+    for (auto i: g[u]) {
+        int v = i.F, t = i.S.F, k = i.S.S;
+        if (vis[v]) continue;
+
+        int tempT = findNextTime(time, k);
+
+        dfs(v, tempT + t);
+    }
+
+    vis[u] = 0;
+}
+
 void solve() {
-	int u, v, t, k;
-	cin >> n >> m >> x >> y;
-	f(i, m) {
-		cin >> u >> v >> t >> k;
-		g[u].pb({v, {t, k}});
-		g[v].pb({u, {t, k}});
-	}
+    int u, v, t, k;
+    cin >> n >> m >> x >> y;
+    f(i, m) {
+        cin >> u >> v >> t >> k;
+        g[u].pb({v, {t, k}});
+        g[v].pb({u, {t, k}});
+    }
     dfs(x, 0);
-    if(res == INF) {
-    	cout << -1 << endl;
-    	return;
+    if (res == INF) {
+        cout << -1 << endl;
+        return;
     }
     cout << res << endl;
 }
