a9e30d00b6b7d1188e8f641cf61bbc681c0fccd1
Ashwani Yadav <ashyadavash@gmail.com>
2021-02-20 23:47:46 +0530

    Update E - Train(ABC).cpp

--- Graph/Shortest Paths/E - Train(ABC).cpp
+++ Graph/Shortest Paths/E - Train(ABC).cpp
@@ -1,48 +1,7 @@
 // https://atcoder.jp/contests/abc192/tasks/abc192_e
 
-#include<bits/stdc++.h>
-using namespace std;
- 
-// #include <ext/pb_ds/assoc_container.hpp> 
-// #include <ext/pb_ds/tree_policy.hpp> 
-// using namespace __gnu_pbds;
-// typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
-// typedef tree<int,int,less<int>,rb_tree_tag,tree_order_statistics_node_update> ordered_map;
-// methods: find_by_order(k); & order_of_key(k); To make it an ordered_multiset, use pairs of (value, time_of_insertion) to distinguish values which are similar
- 
-typedef long long int ll;
-#define int long long int
-#define ld long double
-#define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-#define FRE freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
-#define PRECISION(x) cout << setprecision(x); cout << fixed;
-#define debug(x) cout << #x << " is: " << (x) << endl;
-#define debug2(x,y) cerr << (#x) <<", "<<(#y)<< " are: " << (x) <<", "<<(y)<< endl;
-#define debugx(x) cout << #x << " is: " << (x) << endl; exit(0);
-#define kickstart(testcase, res) cout << "Case #" << testcase << ": " << res << endl;
-#define f(i,n) for(int i=0;i<n;i++)
-#define fa(i,a,n) for(int i=a;i<n;i++)
-#define loop(i,a, n) for(int i = a; i <= n; i++)
-#define loopb(i,a, n) for(int i = a; i >= n; i--)
-#define pb push_back
-#define pf push_front
-#define F first
-#define S second
-#define all(x) x.begin(), x.end()
-#define uniq(v) (v).erase(unique(all(v)),(v).end())
-#define PI 3.1415926535897932384626
-#define MOD 1000000007
-#define MOD2 998244353
-#define INT_INF 1011111111
-#define INF 1000111000111000111LL
-// comment below line in interactive mode (since endl flushes stdout)
-#define endl "\n"
-typedef vector<int> vi;
-typedef pair<int, int> pii;
-typedef vector<vector<int>> matrix;
-int dx[] = {0, 1, 0, -1, -1, 1, -1, 1};
-int dy[] = {-1, 0, 1, 0, -1, 1, 1, -1};
-const int MAXN = 1e5+5;
+
+// AC solution using BFS (Dijkstra)
 const int N = 1e5+5;
 int n, m, x, y;
 
@@ -98,12 +57,80 @@ void solve() {
 	}
     dijkstra(x);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// TLE using DFS
+const int N = 1e5+5;
+int n, m, x, y;
  
-int32_t main() {
-	IOS
-	int T = 1;
-	// cin >> T;
-	while(T--)
-	solve();
-	return 0;
+vector<pair<int, pii>>g[N];
+int vis[N];
+int res = INF;
+ 
+int findNextTime(int N, int K) { 
+    int rem = (N + K) % K; 
+  
+    if (rem == 0) 
+        return N; 
+    else
+        return N + K - rem; 
+} 
+ 
+void dfs(int u, int time) {
+	if(u == y) {
+		res = min(res, time);
+		return;
+	}
+ 
+	vis[u] = 1;
+ 
+	for(auto i: g[u]) {
+		int v = i.F, t = i.S.F, k = i.S.S;
+		if(vis[v]) continue;
+ 
+		int tempT = findNextTime(time, k);
+ 
+		dfs(v, tempT + t);
+	}
+ 
+	vis[u] = 0;
+}
+ 
+void solve() {
+	int u, v, t, k;
+	cin >> n >> m >> x >> y;
+	f(i, m) {
+		cin >> u >> v >> t >> k;
+		g[u].pb({v, {t, k}});
+		g[v].pb({u, {t, k}});
+	}
+    dfs(x, 0);
+    if(res == INF) {
+    	cout << -1 << endl;
+    	return;
+    }
+    cout << res << endl;
 }
