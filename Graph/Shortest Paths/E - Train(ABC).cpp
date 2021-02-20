23879fc1919be062d37358bab76133c9f3715cd3
Ashwani Yadav <ashyadavash@gmail.com>
2021-02-20 23:52:25 +0530

    Update E - Train(ABC).cpp

--- Graph/Shortest Paths/E - Train(ABC).cpp
+++ Graph/Shortest Paths/E - Train(ABC).cpp
@@ -2,59 +2,59 @@
 
 
 // AC solution using BFS (Dijkstra)
-const int N = 1e5+5;
+const int N = 1e5 + 5;
 int n, m, x, y;
 
-vector<pair<int, pii>>g[N];
+vector < pair < int, pii >> g[N];
 
-int findNextTime(int N, int K) { 
-    int rem = (N + K) % K; 
-  
-    if (rem == 0) 
-        return N; 
+int findNextTime(int N, int K) {
+    int rem = (N + K) % K;
+
+    if (rem == 0)
+        return N;
     else
-        return N + K - rem; 
-} 
+        return N + K - rem;
+}
 
 void dijkstra(int x) {
-	int dp[n+5];
-	f(i, n+5) dp[i] = INF;
+    int dp[n + 5];
+    f(i, n + 5) dp[i] = INF;
 
-    priority_queue <pii, vector<pii>, greater<pii>> pq;
+    priority_queue < pii, vector < pii > , greater < pii >> pq;
     pq.push({0, x});
     dp[x] = 0;
 
-    while(!pq.empty()) {
+    while (!pq.empty()) {
         auto x = pq.top();
         pq.pop();
         int u = x.S;
 
-        for(auto i: g[u]) {
-        	int v = i.F, t = i.S.F, k = i.S.S;
-			int tempT = findNextTime(dp[u], k);
+        for (auto i: g[u]) {
+            int v = i.F, t = i.S.F, k = i.S.S;
+            int tempT = findNextTime(dp[u], k);
 
-            if(dp[v] > (tempT + t)) {
+            if (dp[v] > (tempT + t)) {
                 dp[v] = (tempT + t);
                 pq.push({dp[v], v});
             }
         }
     }
 
-    if(dp[y] == INF) {
-    	cout << -1 << endl;
-    	return;
+    if (dp[y] == INF) {
+        cout << -1 << endl;
+        return;
     }
     cout << dp[y] << endl;
 }
 
 void solve() {
-	int u, v, t, k;
-	cin >> n >> m >> x >> y;
-	f(i, m) {
-		cin >> u >> v >> t >> k;
-		g[u].pb({v, {t, k}});
-		g[v].pb({u, {t, k}});
-	}
+    int u, v, t, k;
+    cin >> n >> m >> x >> y;
+    f(i, m) {
+        cin >> u >> v >> t >> k;
+        g[u].pb({v, {t, k}});
+        g[v].pb({u, {t, k}});
+    }
     dijkstra(x);
 }
 
