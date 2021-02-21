1a5aedc4fc1555c2467f183bbc1f8da73e2fbe01
Ashwani Yadav <ashyadavash@gmail.com>
2021-02-21 23:16:24 +0530

    Create Students, heights and their IQs.cpp

---
+++ DP/LIS/Students, heights and their IQs.cpp
@@ -0,0 +1,82 @@
+// https://www.codechef.com/CCHI2021/problems/CCBTS04
+
+#include<bits/stdc++.h>
+using namespace std;
+ 
+// #include <ext/pb_ds/assoc_container.hpp> 
+// #include <ext/pb_ds/tree_policy.hpp> 
+// using namespace __gnu_pbds;
+// typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+// typedef tree<int,int,less<int>,rb_tree_tag,tree_order_statistics_node_update> ordered_map;
+// methods: find_by_order(k); & order_of_key(k); To make it an ordered_multiset, use pairs of (value, time_of_insertion) to distinguish values which are similar
+ 
+typedef long long int ll;
+#define int long long int
+#define ld long double
+#define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define FRE freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
+#define PRECISION(x) cout << setprecision(x); cout << fixed;
+#define debug(x) cout << #x << " is: " << (x) << endl;
+#define debug2(x,y) cerr << (#x) <<", "<<(#y)<< " are: " << (x) <<", "<<(y)<< endl;
+#define debugx(x) cout << #x << " is: " << (x) << endl; exit(0);
+#define kickstart(testcase, res) cout << "Case #" << testcase << ": " << res << endl;
+#define f(i,n) for(int i=0;i<n;i++)
+#define fa(i,a,n) for(int i=a;i<n;i++)
+#define loop(i,a, n) for(int i = a; i <= n; i++)
+#define loopb(i,a, n) for(int i = a; i >= n; i--)
+#define pb push_back
+#define pf push_front
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define uniq(v) (v).erase(unique(all(v)),(v).end())
+#define PI 3.1415926535897932384626
+#define MOD 1000000007
+#define MOD2 998244353
+#define INT_INF 1011111111
+#define INF 1000111000111000111LL
+// comment below line in interactive mode (since endl flushes stdout)
+#define endl "\n"
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+typedef vector<vector<int>> matrix;
+int dx[] = {0, 1, 0, -1, -1, 1, -1, 1};
+int dy[] = {-1, 0, 1, 0, -1, 1, 1, -1};
+const int MAXN = 1e5+5;
+const int N = 1e7+5;
+int n, m;
+
+void solve() {
+	cin >> n;
+	vector<pair<int, int>> a(n);
+    f(i, n) {
+        cin >> a[i].F;
+    }
+    f(i, n) {
+        cin >> a[i].S;
+    }
+    
+    vector<int> dp(n, 1);
+    
+    int res = 0;
+    for(int i = 1; i < n; ++i) {
+        for(int j = 0; j < i; j++) {
+            if(a[i].F > a[j].F && a[i].S < a[j].S) {
+                dp[i] = max(dp[i], dp[j] + 1); 
+            }
+        }
+        res = max(res, dp[i]); 
+    }
+    
+    cout << res << endl;
+
+}
+
+int32_t main() {
+	IOS
+	int T = 1;
+	cin >> T;
+	while(T--)
+	solve();
+	return 0;
+}
