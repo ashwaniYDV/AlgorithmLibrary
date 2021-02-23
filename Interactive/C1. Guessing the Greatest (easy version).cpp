c6273e7296a00279702d3dc1feb4834cf4b3e1d8
Ashwani Yadav <ashyadavash@gmail.com>
2021-02-23 20:14:42 +0530

    Create C1. Guessing the Greatest (easy version).cpp

---
+++ Interactive/C1. Guessing the Greatest (easy version).cpp
@@ -0,0 +1,106 @@
+// https://codeforces.com/contest/1486/problem/C1
+
+#include<bits/stdc++.h>
+using namespace std;
+#pragma GCC optimize ("O3")
+#pragma GCC target ("avx")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize ("unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+ 
+// #include <ext/pb_ds/assoc_container.hpp> 
+// #include <ext/pb_ds/tree_policy.hpp> 
+// using namespace __gnu_pbds;
+// typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+// typedef tree<int,int,less<int>,rb_tree_tag,tree_order_statistics_node_update> ordered_map;
+// methods: find_by_order(k); & order_of_key(k); To make it an ordered_multiset, use pairs of (value, time_of_insertion) to distinguish values which are similar
+ 
+typedef long long int ll;
+#define int long long int
+#define ld long double
+#define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define FRE freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
+#define PRECISION(x) cout << setprecision(x); cout << fixed;
+#define debug(x) cout << #x << " is: " << (x) << endl;
+#define debug2(x,y) cerr << (#x) <<", "<<(#y)<< " are: " << (x) <<", "<<(y)<< endl;
+#define debugx(x) cout << #x << " is: " << (x) << endl; exit(0);
+#define kickstart(testcase, res) cout << "Case #" << testcase << ": " << res << endl;
+#define f(i,n) for(int i=0;i<n;i++)
+#define fa(i,a,n) for(int i=a;i<n;i++)
+#define loop(i,a, n) for(int i = a; i <= n; i++)
+#define loopb(i,a, n) for(int i = a; i >= n; i--)
+#define pb push_back
+#define pf push_front
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define uniq(v) (v).erase(unique(all(v)),(v).end())
+#define PI 3.1415926535897932384626
+#define MOD 1000000007
+#define MOD2 998244353
+#define INT_INF 1011111111
+#define INF 1000111000111000111LL
+// comment below line in interactive mode (since endl flushes stdout)
+// #define endl "\n"
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+typedef vector<vector<int>> matrix;
+int dx[] = {0, 1, 0, -1, -1, 1, -1, 1};
+int dy[] = {-1, 0, 1, 0, -1, 1, 1, -1};
+const int MAXN = 1e5+5;
+const int N = 1e5+5;
+int n, m;
+
+void query(int l, int r) {
+    cout << "? " << l << " " << r <<endl;
+}
+
+void solve(){
+    int x, y, z;
+    cin >> n;
+    int l = 1, r = n, m;
+
+    while(l < r) {
+        m = (l + r) / 2;
+        query(l, r);
+        cin >> x;
+
+        if(r - l == 1) {
+            if (x == l) {
+                cout << "! " << r << endl;
+            } else {
+                cout << "! " << l << endl;
+            }
+            return;
+        }
+
+        if (x <= m) {
+            query(l, m);
+            cin >> y;
+            if (x == y) {
+                r = m;
+            } else {
+                l = m;
+            }
+        } else {
+            query(m, r);
+            cin >> y;
+            if (x == y) {
+                l = m;
+            } else {
+                r = m;
+            }
+        }
+    }
+    cout << "! " << l << endl;
+    
+}
+
+int32_t main() {
+	IOS
+	int T = 1;
+	// cin >> T;
+	while(T--)
+	solve();
+	return 0;
+}
