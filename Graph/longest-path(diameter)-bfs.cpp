e62ab95e1e983891f88bb9814e29f30e341a79ff
Ashwani Yadav <ashyadavash@gmail.com>
2021-03-01 05:46:00 +0530

    Update longest-path(diameter)-bfs.cpp

--- Graph/longest-path(diameter)-bfs.cpp
+++ Graph/longest-path(diameter)-bfs.cpp
@@ -1,65 +1,119 @@
 #include<bits/stdc++.h>
 using namespace std;
+#pragma GCC optimize ("O3")
+#pragma GCC target ("avx")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize ("unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+
+// #include <ext/pb_ds/assoc_container.hpp> 
+// #include <ext/pb_ds/tree_policy.hpp> 
+// using namespace __gnu_pbds;
+// typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+// typedef tree<int,int,less<int>,rb_tree_tag,tree_order_statistics_node_update> ordered_map;
+// methods: find_by_order(k); & order_of_key(k); To make it an ordered_multiset, use pairs of (value, time_of_insertion) to distinguish values which are similar
  
-#define ll long long int
+typedef long long int ll;
+#define int long long int
+#define ld long double
 #define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 #define FRE freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
-#define f(i,n) for(ll i=0;i<n;i++)
-#define fa(i,a,n) for(ll i=a;a<n?i<n:i>n;a<n?i+=1:i-=1)
+#define PRECISION(x) cout << setprecision(x); cout << fixed;
+#define debug(x) cout << #x << " is: " << (x) << endl;
+#define debug2(x,y) cout << (#x) <<", "<<(#y)<< " are: " << (x) <<", "<<(y)<< endl;
+#define debugx(x) cout << #x << " is: " << (x) << endl; exit(0);
+#define kickstart(testcase, res) cout << "Case #" << testcase << ": " << res << endl;
+#define f(i,n) for(int i=0;i<n;i++)
+#define fa(i,a,n) for(int i=a;i<n;i++)
+#define loop(i,a, n) for(int i = a; i <= n; i++)
+#define loopb(i,a, n) for(int i = a; i >= n; i--)
 #define pb push_back
+#define pf push_front
 #define F first
 #define S second
+#define all(x) x.begin(), x.end()
+#define uniq(v) (v).erase(unique(all(v)),(v).end())
 #define PI 3.1415926535897932384626
 #define MOD 1000000007
- 
+#define MOD2 998244353
+#define INT_INF 1011111111
+#define INF 1000111000111000111LL
+// comment below line in interactive mode (since endl flushes stdout)
+// #define endl "\n"
+typedef vector<int> vi;
 typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-typedef vector<int>	vi;
-typedef vector<ll> vl;
-typedef vector<pii> vpii;
-typedef vector<pll> vpll;
-typedef vector<vi> vvi;
-typedef vector<vl> vvl;
+typedef vector<vector<int>> matrix;
+int dx[] = {0, 1, 0, -1, -1, 1, -1, 1};
+int dy[] = {-1, 0, 1, 0, -1, 1, 1, -1};
+const int MAXN = 1e5+5;
+const int N = 1e5+5;
+int n, m;
  
-const ll mod = 1000000007;
-const ll N = 3e5, M = N;
-vl g[N];
-ll a[N], dp[N], in[N], out[N];
+vector<int> g[N];
  
-ll n;
+pair<int, int> bfs(int root) {
+	vector<int> dist(n+1, 0);
+	queue<int> q;
+	q.push(root);
+	dist[root] = 0;
  
-pair<ll,ll>bfs(ll root){
-    vector<ll>dis(n+1,0);
-    queue<ll>q;
-    q.push(root);
-    while(!q.empty()){
-        ll node=q.front();
+	while(!q.empty()) {
+		int u = q.front();
         q.pop();
-        for(auto next:g[node]){
-            if(dis[next] || next==root) continue;
-            dis[next]=dis[node]+1;
-            q.push(next);
-        }
-    }
-    ll mx=0,pos=1;
-    for(ll i=1;i<=n;i++)
-    	if(dis[i]>mx) mx=dis[i], pos=i;
-    		return {pos,mx};
+ 
+		for(int v: g[u]) {
+			if(dist[v] || v == root) continue;
+            dist[v] = dist[u] + 1;
+            q.push(v);
+		}
+	}
+
+	int pos = 0, mx = 0;
+	for (int i = 1; i <= n; i++) {
+		if(dist[i] > mx) {
+			mx = dist[i];
+			pos = i;
+		}
+	}
+
+	return {pos, mx};
+}
+
+void diameterOfTree() {
+	pair<int, int> x = bfs(1);
+	int diameter = bfs(x.first).second;
+	cout << diameter << endl;
+	// cout << diameter + 1 << endl;
+}
+
+void solve() {
+	int u, v;
+	cin >> n;
+	for(int i = 1; i <= n-1; i++) {
+		cin >> u>> v;
+		g[u].push_back(v);
+		g[v].push_back(u);
+	}
+	diameterOfTree();
 }
  
-ll diameter(ll root){
-    pair<ll,ll>p1=bfs(root);
-    return bfs(p1.F).S;
-}
- 
- 
-int32_t main()
-{
+int32_t main() {
 	IOS
-	FRE
-	ll t,m,k,x,y,z,p,q,u,v,ct=0,flag=0;
-	cin>>n;
-	f(i,n-1) cin>>u>>v, g[u].pb(v), g[v].pb(u);
-	ll ans=diameter(1);
-	cout<<ans;
+	int T = 1;
+	// cin >> T;
+	while(T--)
+	solve();
+	return 0;
 }
+
+/*
+Input:
+5
+1 2
+1 3
+2 4
+2 5
+
+Output:
+3
+*/
