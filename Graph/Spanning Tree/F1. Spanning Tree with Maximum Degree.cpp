4c408dd8294277ce811864cc764dff2ea22c608d
Ashwani Yadav <ashyadavash@gmail.com>
2021-03-19 22:47:48 +0530

    Create F1. Spanning Tree with Maximum Degree.cpp

---
+++ Graph/Spanning Tree/F1. Spanning Tree with Maximum Degree.cpp
@@ -0,0 +1,112 @@
+// https://codeforces.com/contest/1133/problem/F1
+
+#include<bits/stdc++.h>
+using namespace std;
+#pragma GCC optimize ("O3")
+#pragma GCC target ("avx")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize ("unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+ 
+// #include <ext/pb_ds/assoc_container.hpp> 
+// #include <ext/pb_ds/tree_policy.hpp> 
+// using namespace __gnu_pbds;
+// typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+// typedef tree<int,int,less<int>,rb_tree_tag,tree_order_statistics_node_update> ordered_map;
+// methods: find_by_order(k); & order_of_key(k); To make it an ordered_multiset, use pairs of (value, time_of_insertion) to distinguish values which are similar
+ 
+typedef long long int ll;
+#define int long long int
+#define ld long double
+#define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define FRE freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
+#define PRECISION(x) cout << setprecision(x); cout << fixed;
+#define debug(x) cout << #x << " is: " << (x) << endl;
+#define debug2(x,y) cout << (#x) <<", "<<(#y)<< " are: " << (x) <<", "<<(y)<< endl;
+#define debugx(x) cout << #x << " is: " << (x) << endl; exit(0);
+#define kickstart(testcase, res) cout << "Case #" << testcase << ": " << res << endl;
+#define f(i,n) for(int i=0;i<n;i++)
+#define fa(i,a,n) for(int i=a;i<n;i++)
+#define loop(i,a, n) for(int i = a; i <= n; i++)
+#define loopb(i,a, n) for(int i = a; i >= n; i--)
+#define pb push_back
+#define pf push_front
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define uniq(v) (v).erase(unique(all(v)),(v).end())
+#define PI 3.1415926535897932384626
+#define MOD 1000000007
+#define MOD2 998244353
+#define INT_INF 1011111111
+#define INF 1000111000111000111LL
+// comment below line in interactive mode (since endl flushes stdout)
+#define endl "\n"
+typedef vector<int> vi;
+typedef vector<pair<int, int>> vpii;
+typedef pair<int, int> pii;
+typedef vector<vector<int>> matrix;
+int dx[] = {0, 1, 0, -1, -1, 1, -1, 1};
+int dy[] = {-1, 0, 1, 0, -1, 1, 1, -1};
+const int MAXN = 1e5+5;
+const int N = 2e5+5;
+int n, m; 
+
+vpii res;
+int vis[N];
+vi g[N];
+
+void dfs(int u) {
+	vis[u] = 1;
+
+	for(int v: g[u]) {
+		if(vis[v]) continue;
+		res.pb({u, v});
+		dfs(v);
+	}
+}
+
+void solve() { 
+	int u, v;
+    cin >> n >> m;
+    while(m--) {
+    	cin >> u >> v;
+    	g[u].pb(v); 
+    	g[v].pb(u);
+    }
+
+    int mx = 0, vertex = 0;
+    loop(i, 1, n) {
+    	if(g[i].size() > mx) {
+    		vertex = i;
+    		mx = g[i].size();
+    	}
+    }
+
+    vi temp;
+    vis[vertex] = 1;
+
+    for(int v: g[vertex]) {
+    	res.pb({vertex, v});
+    	temp.pb(v);
+    	vis[v] = 1;
+    }
+
+    for(int u: temp) {
+    	dfs(u);
+    }
+
+    for(auto i: res) {
+    	cout << i.F << " " << i.S << endl;
+    }
+
+} 
+ 
+signed main() {
+	IOS
+	int T = 1;
+	// cin >> T;
+	while(T--)
+	solve();
+	return 0;
+}
