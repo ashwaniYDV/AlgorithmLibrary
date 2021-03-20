f1405b68b6ddc49d5f9c99dbb7a08cc22a46028c
Ashwani Yadav <ashyadavash@gmail.com>
2021-03-21 02:29:29 +0530

    Create Plates (Kick Start 2020-A).cpp

---
+++ DP/Knapsack/Plates (Kick Start 2020-A).cpp
@@ -0,0 +1,99 @@
+#include<bits/stdc++.h>
+using namespace std;
+#pragma GCC optimize ("O3")
+#pragma GCC target ("avx")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize ("unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+ 
+// #include <ext/pb_ds/assoc_container.hpp> 
+// #include <ext/pb_ds/tree_policy.hpp> 
+// using namespace __gnu_pbds;
+// typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+// typedef tree<int,int,less<int>,rb_tree_tag,tree_order_statistics_node_update> ordered_map;
+// methods: find_by_order(k); & order_of_key(k); To make it an ordered_multiset, use pairs of (value, time_of_insertion) to distinguish values which are similar
+ 
+typedef long long int ll;
+#define int long long int
+#define ld long double
+#define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define FRE freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
+#define PRECISION(x) cout << setprecision(x); cout << fixed;
+#define debug(x) cout << #x << " is: " << (x) << endl;
+#define debug2(x,y) cout << (#x) <<", "<<(#y)<< " are: " << (x) <<", "<<(y)<< endl;
+#define debugx(x) cout << #x << " is: " << (x) << endl; exit(0);
+#define kickstart(testcase, res) cout << "Case #" << testcase << ": " << res << endl;
+#define f(i,n) for(int i=0;i<n;i++)
+#define fa(i,a,n) for(int i=a;i<n;i++)
+#define loop(i,a, n) for(int i = a; i <= n; i++)
+#define loopb(i,a, n) for(int i = a; i >= n; i--)
+#define pb emplace_back
+#define pf push_front
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define uniq(v) (v).erase(unique(all(v)),(v).end())
+#define PI 3.1415926535897932384626
+#define MOD 1000000007
+#define MOD2 998244353
+#define INT_INF 1011111111
+#define INF 1000111000111000111LL
+// comment below line in interactive mode (since endl flushes stdout)
+#define endl "\n"
+typedef vector<int> vi;
+typedef vector<pair<int, int>> vpii;
+typedef pair<int, int> pii;
+typedef vector<vector<int>> matrix;
+int dx[] = {0, 1, 0, -1, -1, 1, -1, 1};
+int dy[] = {-1, 0, 1, 0, -1, 1, 1, -1};
+const int MAXN = 1e5+5;
+const int N = 51;
+int n, m, k;
+
+const int P = 51 * 30;
+
+int a[N][31];
+int dp[N][P];
+
+int go(int i, int p) {
+	if(p == 0) return 0;
+	if(p < 0 || i >= n) return -INF;
+
+	if(dp[i][p] != -1) return dp[i][p];
+
+	int res = -INF, sum = 0;
+	
+	f(j, k) {
+		if(j + 1 > p) break;
+		sum += a[i][j];
+		res = max(res, sum + go(i+1, p - (j+1)));
+	}
+
+	res = max(res, go(i+1, p));
+	
+	return dp[i][p] = res;
+}
+
+void solve(int tc) {
+	int p;
+	cin >> n >> k >> p;
+	memset(dp, -1, sizeof dp);
+
+	f(i, n) {
+		f(j, k) {
+			cin >> a[i][j];
+		}
+	}
+
+	kickstart(tc, go(0, p));
+}
+ 
+signed main() {
+	IOS
+	int T = 1;
+	cin >> T;
+	int ct = 1;
+	while(T--)
+	solve(ct++);
+	return 0;
+}
