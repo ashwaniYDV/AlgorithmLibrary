18a3cc9e9fb776a046b43bd368136d1c9eff9a46
Ashwani Yadav <ashyadavash@gmail.com>
2021-03-24 01:53:30 +0530

    Update Jump Game (Print all paths).cpp

--- DP/00 Standard DP Que/Jump Game (Print all paths).cpp
+++ DP/00 Standard DP Que/Jump Game (Print all paths).cpp
@@ -1,6 +1,76 @@
 // https://youtu.be/phgjL7SbsWs
 // Jump Game (Minimum nuber of paths required)(Print all paths)
 
+// Method 1
+struct node {
+	int index;
+	int sizeOfJump;
+	int jumpsRequired;
+	string pathSoFar;
+};
+
+void solve() {
+	cin >> n;
+	vector<int> jump(n);
+	f(i, n) cin >> jump[i];
+
+	vector<int> dp(n, INF);
+	dp[0] = 0;
+	for(int i = 0; i < n; i++) {
+		for(int j = 1; j <= jump[i]; j++) {
+			if(i + j >= n) break;
+			dp[i + j] = min(dp[i + j], dp[i] + 1);
+		}
+	}
+    cout << "Min number of jumps = " << dp[n - 1] << endl;
+
+    // reverse engineering
+    queue<node> q;
+    q.push({0, jump[0], dp[0], "0"});
+
+    while(q.size()) {
+    	node x = q.front();
+    	q.pop();
+
+    	if(x.index == n - 1) {
+    		cout << x.pathSoFar << endl;
+    	}
+
+    	for(int j = 1; j <= x.sizeOfJump; j++) {
+    		int nextIndex = x.index + j;
+    		if(nextIndex >= n) break;
+
+    		if(nextIndex != n-1 && jump[nextIndex] == 0) continue;
+
+    		if(dp[nextIndex] == x.jumpsRequired + 1) {
+    			q.push({nextIndex, jump[nextIndex], dp[nextIndex], x.pathSoFar + " -> " + to_string(nextIndex)});
+    		}
+    	}
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Method 2
 struct node {
 	int index;
 	int sizeOfJump;
@@ -36,14 +106,18 @@ void solve() {
     	}
 
     	for(int j = 1; j <= x.sizeOfJump; j++) {
-    		int curIndex = x.index + j;
-    		if(curIndex >= n) break;
-    		if(dp[curIndex] != INF && dp[curIndex] == x.jumpsRequired - 1) {
-    			q.push({curIndex, jump[curIndex], dp[curIndex], x.pathSoFar + "->" + to_string(curIndex)});
+    		int nextIndex = x.index + j;
+    		if(nextIndex >= n) break;
+
+    		if(dp[nextIndex] == INF) {
+    			continue;
+    		}
+
+    		if(dp[nextIndex] == x.jumpsRequired - 1) {
+    			q.push({nextIndex, jump[nextIndex], dp[nextIndex], x.pathSoFar + " -> " + to_string(nextIndex)});
     		}
     	}
     }
-
 }
  
 
