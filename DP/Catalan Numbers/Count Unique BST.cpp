2035a445f137d9a383d67d7db57f787adb71f783
Ashwani Yadav <ashyadavash@gmail.com>
2021-03-30 00:51:40 +0530

    Create Count Unique BST.cpp

---
+++ DP/Catalan Numbers/Count Unique BST.cpp
@@ -0,0 +1,60 @@
+// https://leetcode.com/problems/unique-binary-search-trees/
+
+// Method 1: Catalan Number
+class Solution {
+    int catalan_Number(int n) {
+        int catalan[n+1];
+        catalan[0] = catalan[1] = 1;
+        
+        for(int i = 2; i <= n; ++i) {
+            catalan[i] = 0;
+            for(int j = 0; j < i; ++j)
+                catalan[i] += catalan[j] * catalan[i-1 - j];
+        }
+        return catalan[n];
+    }
+public:
+    int numTrees(int n) {
+        return catalan_Number(n);
+    }
+};
+
+
+
+
+
+
+
+
+
+
+// Method 2
+class Solution {
+public:
+    int helper(int start, int end, vector<vector<int>>& dp) {
+        if(start > end) {
+            return 1;
+        }
+        
+        if(dp[start][end] != -1) return dp[start][end];
+        
+        int res = 0;
+      
+        // making i as the root for each i from start to end
+        for(int i = start; i <= end; i++) {
+            int left = helper(start, i-1, dp);
+            int right = helper(i+1, end, dp);
+            
+            res += left * right;
+        }
+        
+        return dp[start][end] = res;
+        
+    }
+    int numTrees(int n) {
+        if(n == 0) return 0;
+        
+        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
+        return helper(1, n, dp);
+    }
+};
