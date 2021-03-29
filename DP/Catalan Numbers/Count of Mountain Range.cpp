f85f9fcc5bda04a8b31ab2f350e15446dcba6e92
Ashwani Yadav <ashyadavash@gmail.com>
2021-03-30 02:58:36 +0530

    Create Count of Mountain Range.cpp

---
+++ DP/Catalan Numbers/Count of Mountain Range.cpp
@@ -0,0 +1,8 @@
+/*
+Cn is the number of ways to form a "mountain range" with n upstrokes and n downstrokes that all stay above a horizontal line. 
+The mountain range interpretation is that the mountains will never go below the horizon (i.e sea level).
+*/
+
+// https://en.wikipedia.org/wiki/Catalan_number
+// https://www.pepcoding.com/resources/data-structures-and-algorithms-in-java-levelup/dynamic-programming/count-valleys-mountains-official/ojquestion
+// https://youtu.be/hM_FJnrP1kk
