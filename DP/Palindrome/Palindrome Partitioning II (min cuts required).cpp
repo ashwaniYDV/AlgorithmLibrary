f8aa6f4893b65f6479db928fa0bf0062f22dae7c
Ashwani Yadav <ashyadavash@gmail.com>
2021-03-30 22:16:10 +0530

    Update Palindrome Partitioning II (min cuts required).cpp

--- DP/Palindrome/Palindrome Partitioning II (min cuts required).cpp
+++ DP/Palindrome/Palindrome Partitioning II (min cuts required).cpp
@@ -1,6 +1,6 @@
 // https://leetcode.com/problems/palindrome-partitioning-ii/
 
-// Method 1
+// Method 1 (accepted)
 class Solution {
 public:
     int n;
@@ -35,5 +35,35 @@ public:
         vector<int> dp(n+1, -1);
         // no of cuts = no of palin strings - 1
         return fun(0, dp) - 1;
+    }
+};
+
+
+// Method 2 (TLE)
+class Solution {
+public:
+    bool isPalindrome(int start, int end, string s) {
+        while(start <= end) {
+            if(s[start++] != s[end--]) return false;
+        }
+        return true;
+    }
+    
+    int minCut(string s) {
+        int n = s.size();
+        vector<int> dp(n+1, INT_MAX);
+        
+        dp[0] = 0;
+        dp[1] = 1;
+        
+        for(int i = 2; i <= n; i++) {
+            for(int j = 0; j < i; j++) {
+                if(isPalindrome(j, i - 1, s)) {
+                    dp[i] = min(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        
+        return dp[n] - 1;
     }
 };
