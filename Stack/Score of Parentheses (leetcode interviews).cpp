675b1035e990c10483bb240100682cf02dc57c8e
Ashwani Yadav <ashyadavash@gmail.com>
2021-04-05 23:03:54 +0530

    Update and rename Score of Parentheses.cpp to Score of Parentheses (leetcode interviews).cpp

--- Stack/Score of Parentheses (leetcode interviews).cpp
+++ Stack/Score of Parentheses (leetcode interviews).cpp
@@ -1,5 +1,6 @@
 // https://leetcode.com/problems/score-of-parentheses/
 
+// Time: O(n), Space: O(1)
 class Solution {
 public:
     int scoreOfParentheses(string S) {
@@ -20,6 +21,45 @@ public:
 };
 
 
+
+
+
+
+// Time: O(n), Space: O(n)
+class Solution {
+public:
+    static const int N = 55;
+    int closingBrac[N];
+    
+    int fun(int l, int r) {
+        if(l + 1 == r) return 1;
+        
+        if(closingBrac[l] == r) return 2 * fun(l+1, r-1);
+        
+        return fun(l, closingBrac[l]) + fun(closingBrac[l] + 1, r);
+    }
+    int scoreOfParentheses(string s) {
+        int n = s.size();
+        stack<int> st;
+        for(int i = 0; i < n; i++) {
+            if(s[i] == '(') {
+                st.push(i);
+            } else {
+                int x = st.top();
+                closingBrac[x] = i;
+                st.pop();
+            }
+        }
+        return fun(0, n-1);
+    }
+};
+
+
+
+
+
+
+// Time: O(n), Space: O(n)
 class Solution {
 public:
     int scoreOfParentheses(string s) {
