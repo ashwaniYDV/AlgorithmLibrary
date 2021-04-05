ca2dc0ec5afe3c97d1b7a7fd166a6eadc9656296
Ashwani Yadav <ashyadavash@gmail.com>
2021-04-05 21:22:53 +0530

    Create Score of Parentheses.cpp

---
+++ Stack/Score of Parentheses.cpp
@@ -0,0 +1,41 @@
+// https://leetcode.com/problems/score-of-parentheses/
+
+class Solution {
+public:
+    int scoreOfParentheses(string S) {
+        int ans = 0, bal = 0;
+        for (int i = 0; i < S.size(); ++i) {
+            if (S[i] == '(') {
+                bal++;
+            } else {
+                bal--;
+                if (S[i-1] == '(') {
+                    ans += (1 << bal);
+                }
+            }
+        }
+
+        return ans;
+    }
+};
+
+/*
+class Solution {
+public:
+    int scoreOfParentheses(string s) {
+        int n = s.size();
+        int res = 0;
+        stack<int> st;
+        for(char x: s) {
+            if(x == '(') {
+                st.push(res);
+                res = 0;
+            } else {
+                res = st.top() + max(2*res, 1);
+                st.pop();
+            }
+        }
+        return res;
+    }
+};
+*/
