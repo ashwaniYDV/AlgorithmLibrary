f9eb78bf8dd9d5bdba9cfd9c5852cf29ace3663c
Ashwani Yadav <ashyadavash@gmail.com>
2021-04-07 16:02:24 +0530

    Create 3 Race condition.c

---
+++ OS/Unix Threads in C (CodeVault)/3 Race condition.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+
+int mails = 0;
+
+void* routine() {
+    for (int i = 0; i < 1000000; i++) {
+        mails++;
+        // read mails
+        // increment
+        // write mails
+    }
+}
+
+int main(int argc, char* argv[]) {
+    pthread_t p1, p2, p3, p4;
+    if (pthread_create(&p1, NULL, &routine, NULL) != 0) {
+        return 1;
+    }
+    if (pthread_create(&p2, NULL, &routine, NULL) != 0) {
+        return 2;
+    }
+    if (pthread_join(p1, NULL) != 0) {
+        return 3;
+    }
+    if (pthread_join(p2, NULL) != 0) {
+        return 4;
+    }
+    printf("Number of mails: %d\n", mails);
+    return 0;
+}
