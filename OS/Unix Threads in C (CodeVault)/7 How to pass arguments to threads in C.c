07f0f498d578a842f925a25018fd598fc84b7935
Ashwani Yadav <ashyadavash@gmail.com>
2021-04-07 19:08:37 +0530

    Create 7 How to pass arguments to threads in C.c

---
+++ OS/Unix Threads in C (CodeVault)/7 How to pass arguments to threads in C.c
@@ -0,0 +1,71 @@
+// Create 10 threads, each taking a unique prime from the primes array and print it on the screen
+
+// correct implementation
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+int primes[10] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 };
+
+void* routine(void* arg) {
+    int index = *(int*)arg;
+    printf("%d ", primes[index]);
+    free(arg);
+}
+
+int main(int argc, char* argv[]) {
+    pthread_t th[10];
+    for (int i = 0; i < 10; i++) {
+        int* a = malloc(sizeof(int));
+        *a = i;
+        if (pthread_create(&th[i], NULL, &routine, a) != 0) {
+            perror("Failed to created thread");
+        }
+    }
+    for (int i = 0; i < 10; i++) {
+        if (pthread_join(th[i], NULL) != 0) {
+            perror("Failed to join thread");
+        }
+    }
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+// incorrect implementation
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+int primes[10] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 };
+
+void* routine(void* arg) {
+    int index = *(int*)arg;
+    printf("%d ", primes[index]);
+}
+
+int main(int argc, char* argv[]) {
+    pthread_t th[10];
+    for (int i = 0; i < 10; i++) {
+        if (pthread_create(&th[i], NULL, &routine, &i) != 0) {
+            perror("Failed to created thread");
+        }
+    }
+    for (int i = 0; i < 10; i++) {
+        if (pthread_join(th[i], NULL) != 0) {
+            perror("Failed to join thread");
+        }
+    }
+    
+    return 0;
+}
