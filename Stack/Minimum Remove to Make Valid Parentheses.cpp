9f840f9f32cda6d243cfcc6fdb625c650a241cc6
Ashwani Yadav <ashyadavash@gmail.com>
2021-04-09 16:49:01 +0530

    Update Minimum Remove to Make Valid Parentheses.cpp

--- Stack/Minimum Remove to Make Valid Parentheses.cpp
+++ Stack/Minimum Remove to Make Valid Parentheses.cpp
@@ -37,3 +37,68 @@ public:
         return res;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// TLE solution
+class Solution {
+public:
+    bool isBalanced(string s) {
+        int cnt = 0;
+        for(char x: s) {
+            if(x == '(') cnt++;
+            if(x == ')') cnt--;
+            if(cnt < 0) return false;
+        }
+        return (cnt == 0);
+    }
+    
+    map<string, bool> vis;
+    
+    string minRemoveToMakeValid(string s) {
+        queue<string> q;
+        q.push(s);
+        
+        while(!q.empty()) {
+            int n = q.size();
+            for(int i = 0; i < n; i++) {
+                string u = q.front();
+                q.pop();
+                
+                if(vis[u]) continue;
+                vis[u] = 1;
+
+                if(isBalanced(u)) {
+                    return u;
+                }
+
+                for(int i = 0; i < u.size(); i++) {
+                    if(s[i] == '(' or s[i] == ')') {
+                        string v = u.substr(0, i) + u.substr(i+1);
+                        q.push(v);
+                    }
+                }
+            }
+        }
+        return "";
+    }
+};
