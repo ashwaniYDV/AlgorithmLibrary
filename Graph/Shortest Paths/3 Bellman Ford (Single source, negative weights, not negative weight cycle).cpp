5bfd395a3a47b155a25018b6b2311978882e1926
Ashwani Yadav <ashyadavash@gmail.com>
2021-04-19 14:52:18 +0530

    Update and rename 3 Bellman Ford.cpp to 3 Bellman Ford (Single source, negative weights, not negative weight cycle).cpp

--- Graph/Shortest Paths/3 Bellman Ford (Single source, negative weights, not negative weight cycle).cpp
+++ Graph/Shortest Paths/3 Bellman Ford (Single source, negative weights, not negative weight cycle).cpp
@@ -2,7 +2,7 @@
 // this is directed version
 // For questions related to visiting atleast K edges only consider relaxation from 0 to K only instead of (n-1)  
 
-// drawback: If there is a cycle with negative (ie negative weight cycle), it fails
+// drawback: If there is a cycle with negative weight, it fails
 // it will work fine for cycles of positive weight
 
 // therefore after relaxing >= n times, then also path weights are changing that means there is a negative weight cycle in the graph
