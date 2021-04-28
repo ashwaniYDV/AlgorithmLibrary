5357e64955716fafba8d69d02f8a0f7bd67b170a
Ashwani Yadav <ashyadavash@gmail.com>
2021-04-29 04:33:57 +0530

    Update D. Powerful array.cpp

--- Algos/MO’s Algorithm/D. Powerful array.cpp
+++ Algos/MO’s Algorithm/D. Powerful array.cpp
@@ -7,125 +7,140 @@ using namespace std;
 #pragma GCC optimize("Ofast")
 #pragma GCC optimize ("unroll-loops")
 #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
- 
+
 typedef long long int ll;
 #define int long long int
+#define ld long double
 #define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 #define FRE freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
+#define PRECISION(x) cout << setprecision(x); cout << fixed;
+#define UNIQUE(a) sort(a.begin(), a.end() ); a.erase( unique( a.begin(), a.end() ), a.end() );
 #define debug(x) cout << #x << " is: " << (x) << endl;
-#define f(i,n) for(ll i=0;i<n;i++)
-#define fa(i,a,n) for(ll i=a;a<n?i<n:i>=n;a<n?i+=1:i-=1)
-#define loop(i,a, n) for(ll i = a; i <= n; i++)
-#define loopb(i,a, n) for(ll i = a; i >= n; i--)
+#define debug2(x,y) cout << (#x) <<", "<<(#y)<< " are: " << (x) <<", "<<(y)<< endl;
+#define debugx(x) cout << #x << " is: " << (x) << endl; exit(0);
+#define kickstart(testcase, res) cout << "Case #" << testcase << ": " << res << endl;
+#define f(i,n) for(int i=0;i<n;i++)
+#define fa(i,a,n) for(int i=a;i<n;i++)
+#define loop(i,a, n) for(int i = a; i <= n; i++)
+#define loopb(i,a, n) for(int i = a; i >= n; i--)
 #define pb push_back
 #define pf push_front
 #define F first
 #define S second
 #define all(x) x.begin(), x.end()
-#define setmem(x, k) memset(x, k, sizeof(x))
-#define clr(x) memset(x, 0, sizeof(x))
-#define sortall(x) sort(all(x))
+#define uniq(v) (v).erase(unique(all(v)),(v).end())
 #define PI 3.1415926535897932384626
 #define MOD 1000000007
+#define MOD2 998244353
+#define INT_INF 1011111111
+#define INF 1000111000111000111LL
+// comment below line in interactive mode (since endl flushes stdout)
+#define endl "\n"
+typedef vector<int> vi;
+typedef vector<pair<int, int>> vpii;
 typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-typedef vector<int>	vi;
-typedef vector<ll> vl;
-typedef long double ld;
-typedef vector<vector<ll>> matrix;
-ll dx[] = {0, 1, 0, -1};
-ll dy[] = {-1, 0, 1, 0};
-const ll N = 2e5+5;
-// ll a[N], b[N], dp[N], level[N], vis[N], in[N], out[N];
-// vl g[N];
-ll n, m;
+typedef vector<vector<int>> matrix;
+int dx[] = {0, 1, 0, -1, -1, 1, -1, 1};
+int dy[] = {-1, 0, 1, 0, -1, 1, 1, -1};
+const int MAXN = 1e4+5;
+const int N = 2e5+5;
+int n, m;
+
+
 
 // sqrt(n)
-ll BLOCK = 555;
+int BLOCK = 555;
 
 struct query{
-	ll l;
-	ll r;
-	ll i;
+	int l;
+	int r;
+	int i;
 };
-query Q[200001];
-ll ar[200001] , ans[200001];
-ll freq[1000001];
-ll power = 0;
 
-bool compare(query a , query b) {
+// struct query {
+//     int l, r, i;
+//     bool operator<(const query &o) const {
+//         if((l / BLOCK) != (o.l / BLOCK))
+//             return l < o.l;
+//         return l / BLOCK & 1 ? r < o.r : r > o.r;
+//     }
+// };
+
+query Q[200001];
+int ar[200001] , ans[200001];
+int freq[1000001];
+int powerRes = 0;
+
+bool compare(query a, query b) {
 	if(a.l / BLOCK != b.l/BLOCK)
-	return a.l/BLOCK < b.l/BLOCK;
+		return a.l/BLOCK < b.l/BLOCK;
  
 	return a.r < b.r;
 }
 
-void add(ll pos) {
-	ll ele = ar[pos];
-	ll prevFreq = freq[ele];
-	ll currFreq = ++freq[ele];
-	power -= prevFreq * prevFreq * ele;
-	power += currFreq * currFreq * ele;
+void add(int pos) {
+	int ele = ar[pos];
+	int prevFreq = freq[ele];
+	int currFreq = ++freq[ele];
+	powerRes -= prevFreq * prevFreq * ele;
+	powerRes += currFreq * currFreq * ele;
 }
  
-void remove(ll pos) {
-	ll ele = ar[pos];
-	ll prevFreq = freq[ele];
-	ll currFreq = --freq[ele];
-	power -= prevFreq * prevFreq * ele;
-	power += currFreq * currFreq * ele;
+void remove(int pos) {
+	int ele = ar[pos];
+	int prevFreq = freq[ele];
+	int currFreq = --freq[ele];
+	powerRes -= prevFreq * prevFreq * ele;
+	powerRes += currFreq * currFreq * ele;
 }
 
  
 void solve() {
-	ll q;
+	int q;
 	cin >> n >> q;
 	f(i, n) cin >> ar[i];
-	f(i, q){
+	f(i, q) {
 		cin >> Q[i].l >> Q[i].r;
 		Q[i].i = i;
 		Q[i].l--, Q[i].r--;
 	}
 	sort(Q, Q + q, compare);
 
-	ll ML = 0 , MR = -1;
-	f(i, q){
-		ll L = Q[i].l;
-		ll R = Q[i].r;
+	int ML = 0 , MR = -1;
+	f(i, q) {
+		int L = Q[i].l;
+		int R = Q[i].r;
  		
  		// Its always suggested to use in this method : R A A R 
 
  		// contraction
-		while(ML < L){
+		while(ML < L) {
 			remove(ML) , ML++;
 		}
  		// expansion
-		while(ML > L){
+		while(ML > L) {
 			ML-- , add(ML);
 		}
 		// expansion
-		while(MR < R){
+		while(MR < R) {
 			MR++ , add(MR);
 		}
  		// contraction
-		while(MR > R){
+		while(MR > R) {
 			remove(MR) , MR--;
 		}
  
-		ans[Q[i].i] = power;
+		ans[Q[i].i] = powerRes;
 	}
  
 	f(i, q) cout << ans[i] << endl;
-
-}	
- 
-int32_t main() {
-	IOS
-	clock_t begin = clock();
-	ll T = 1;
-	// cin >> T;
-	while(T--)
-	solve();
-	cerr<<"Time elapsed : "<<(clock()-begin)*1000.0/CLOCKS_PER_SEC<<"ms"<<'\n';
-	return 0;
 }
+ 
+signed main() {
+	IOS
+	int t = 1;
+	// cin >> t;
+	while(t--) {
+		solve();
+	}
+} 
