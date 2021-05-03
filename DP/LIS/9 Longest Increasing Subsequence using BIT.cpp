4e2e371f094b03611c200df1ec1e11c3ae8773f1
Ashwani Yadav <ashyadavash@gmail.com>
2021-05-04 01:33:08 +0530

    Update 9 Longest Increasing Subsequence using BIT.cpp

--- DP/LIS/9 Longest Increasing Subsequence using BIT.cpp
+++ DP/LIS/9 Longest Increasing Subsequence using BIT.cpp
@@ -11,6 +11,107 @@ Approach:
    Let this value be x. If x + 1 is greater than the current element in BIT, update the BIT.
 */
 
+// Method 1
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+
+// Function to map numbers using coordinate compression
+void coordinateCompression(vector<int>& arr) {
+    set<int> s;
+    for (int i = 0; i < n; i++) {
+        s.insert(arr[i]);
+    }
+
+    // Map will store the new elements
+    int index = 1;
+    map<int, int> mp;
+    for (auto it: s) {
+        mp[it] = index++;
+    }
+
+    for (int i = 0; i < n; i++) {
+        // now change the current element to range 1 to N.
+        arr[i] = mp[arr[i]];
+    }
+}
+
+// Function to calculate length of maximum increasing sequence till i'th index
+int query(vector<int>& BIT, int index) {
+    int ans = 0;
+    while (index > 0) {
+        ans = max(ans, BIT[index]);
+        index -= index & (-index);
+    }
+    return ans;
+}
+
+// Function for updating BIT array for maximum increasing sequence till i'th index
+void update(vector<int>& BIT, int index) {
+
+    // If 4 is inserted in BIT,
+    // check for maximum length
+    // subsequence till element 3.
+    // Let this subsequence length be x.
+    // If x + 1 is greater than
+    // the current element in BIT,
+    // update the BIT array
+    int x = query(BIT, index - 1);
+    int value = x + 1;
+    while (index <= n) {
+
+        // Store maximum length subsequence length till index
+        // Here index is the coordinate compressed element
+        BIT[index] = max(BIT[index], value);
+
+        // Go to child node and update that node
+        index += index & (-index);
+    }
+}
+
+// Driver code
+int main() {
+    vector<int> arr = { 6, 5, 6, 1, 1, 1, 3, 2, 4, 8, 7 };
+    n = arr.size();
+
+    coordinateCompression(arr);
+    vector<int> BIT(n+1, 0);
+
+    for (int i = 0; i < n; i++) {
+        // Add elements from left to right in BIT
+        update(BIT, arr[i]);
+    }
+
+    int ans = query(BIT, n);
+
+    cout << ans << endl;
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Method 2
+
 class Solution {
 public:
     static const int N = 1e5+5;
