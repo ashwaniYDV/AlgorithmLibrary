fadc653f6f921982cdb3aa09edf1a687c4aa08e6
Ashwani Yadav <ashyadavash@gmail.com>
2021-05-05 01:40:19 +0530

    Update Edit Distance (min no of operations required to convert word1 to word2).cpp

--- DP/00 Standard DP Que/Edit Distance (min no of operations required to convert word1 to word2).cpp
+++ DP/00 Standard DP Que/Edit Distance (min no of operations required to convert word1 to word2).cpp
@@ -1,5 +1,14 @@
 // https://leetcode.com/problems/edit-distance/
 
+/*
+Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2.
+
+You have the following three operations permitted on a word:
+1) Insert a character
+2) Delete a character
+3) Replace a character
+*/
+
 // Method 1
 class Solution {
 public:
