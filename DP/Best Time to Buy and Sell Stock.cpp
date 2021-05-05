add77832ed8f200effc1b1ea7b27b369a70153e2
Ashwani Yadav <ashyadavash@gmail.com>
2021-05-05 21:23:16 +0530

    Update Best Time to Buy and Sell Stock.cpp

--- DP/Best Time to Buy and Sell Stock.cpp
+++ DP/Best Time to Buy and Sell Stock.cpp
@@ -261,6 +261,30 @@ public:
 
 // Share my DP solution (By State Machine Thinking)
 // https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/discuss/75928/Share-my-DP-solution-(By-State-Machine-Thinking)
+
+class Solution {
+    public:
+        int maxProfit(vector < int > & prices) {
+            int n = prices.size();
+            if (n <= 1)
+                return 0;
+            
+            vector < int > noStock(n, 0), inHand(n, 0), Sold(n, 0);
+
+            noStock[0] = 0;
+            inHand[0] = -prices[0]; //bcoz we have bought a stock and never sold it
+            Sold[0] = 0;
+
+            for (int i = 1; i < n; ++i) {
+                noStock[i] = max(noStock[i - 1], Sold[i - 1]);
+                inHand[i] = max(inHand[i - 1], noStock[i - 1] - prices[i]);
+                Sold[i] = inHand[i - 1] + prices[i];
+            }
+            int result = max(noStock[n - 1], Sold[n - 1]);
+            return result;
+        }
+};
+
 class Solution {
     public:
         int maxProfit(vector < int > & prices) {
