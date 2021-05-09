d515ef69c1fd7dc951a451bd25dfedd37da181c6
Ashwani Yadav <ashyadavash@gmail.com>
2021-05-10 01:01:38 +0530

    Update Ugly Number II.cpp

--- DP/00 Standard DP Que/Ugly Number II.cpp
+++ DP/00 Standard DP Que/Ugly Number II.cpp
@@ -11,6 +11,29 @@ a[2] = min( a[1]*2, a[0]*3, a[0]*5). And so on.
 Be careful about the cases such as 6, in which we need to forward both pointers of 2 and 3.
 */
 
+/*
+Let me try to explain this using an example. 
+Let's say we have the first 3 ugly numbers 1, 2, 3. What would be the next number? 
+Given the definition, the next number has to be the the smallest number among 2*(1,2,3), 3*(1,2,3), 5*(1,2,3). 
+Obviously, the smallest number is 2 * 1 = 2. But wait, 2 is already in there. 
+So precisely speaking, the next number has to be the the smallest number among all the existing numbers multiplied by 2, 3,5 that isn't in the list already. 
+Now, we can traverse all numbers and maintain a hashset if we want, but it would become O(N^2). 
+Good news is that we can solve this in a DP-like approach. First, we assume there is only one number in the list, which is 1. 
+The next number is Min(2 * 1, 3 * 1, 5 * 1) = 2 and it is not in the list. 
+Because we have already considered 2*1 (we move the pointer to its next position, which is 2), 
+now we only need to consider 2 * 2, 3 * 1, 5 * 1 in the next iteration. This way, we don't have to worry about finding a number that is already in the list.
+
+Edge case:
+a[i] = min({a[t2]*2, a[t3]*3, a[t5]*5});
+Conside the case when a[t2]*2 == a[t3]*3
+in this case we update both t1 and t2 as
+    if(a[i] == a[t2]*2) t2++; 
+    if(a[i] == a[t3]*3) t3++;
+i.e both if() will be called here
+
+same goes for any combination of t2, t3, t5
+*/
+
 class Solution {
 public:
     int nthUglyNumber(int n) {
