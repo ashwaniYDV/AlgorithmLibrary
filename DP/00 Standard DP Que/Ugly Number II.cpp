1b06232bda7e5b4c26d595edc89327bdb782ded7
Ashwani Yadav <ashyadavash@gmail.com>
2021-05-10 00:52:55 +0530

    Update Ugly Number II.cpp

--- DP/00 Standard DP Que/Ugly Number II.cpp
+++ DP/00 Standard DP Que/Ugly Number II.cpp
@@ -1,11 +1,14 @@
 // https://leetcode.com/problems/ugly-number-ii/
 
 /*
-We have an array k of first n ugly number. We only know, at the beginning, the first one, which is 1. Then
+We have an array a of first n ugly number. We only know, at the beginning, the first one, which is 1. 
+a[0] = 1
+Then,
+a[1] = min( a[0]*2, a[0]*3, a[0]*5). The answer is a[0]*2. So we move 2's pointer to 1. 
+Then we test:
+a[2] = min( a[1]*2, a[0]*3, a[0]*5). And so on. 
 
-k[1] = min( k[0]x2, k[0]x3, k[0]x5). The answer is k[0]x2. So we move 2's pointer to 1. Then we test:
-
-k[2] = min( k[1]x2, k[0]x3, k[0]x5). And so on. Be careful about the cases such as 6, in which we need to forward both pointers of 2 and 3.
+Be careful about the cases such as 6, in which we need to forward both pointers of 2 and 3.
 */
 
 class Solution {
