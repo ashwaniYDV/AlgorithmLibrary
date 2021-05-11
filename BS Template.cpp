7d6546f3e256a276e9714c3232676b670fcca047
Ashwani Yadav <ashyadavash@gmail.com>
2021-05-11 14:31:00 +0530

    Update BS Template.cpp

--- BS Template.cpp
+++ BS Template.cpp
@@ -1,6 +1,28 @@
+// Find smallest number x such that fun(x) = k wher f(n) is of following monotic shape
 /*
 
-// Find smallest number num such that fun(num) = k
+
+
+                                                                                #
+                                                                              #
+                                                                           #
+                                                                        #
+                                                                   #
+                                                            #
+                                                    #
+                                          #
+                                 #        ^
+                        #                 |
+               #                          |
+       #                                  |
+  #                                       |
+                                          |
+                                 This is the optimam point
+                          
+                          
+In this case ham right to optimum move krenge na i left to optimum
+If f(n) would have been just mirror image of this about y-axix then ham left to optimum point move krte 
+*/
 
 while(l <= r) {
     int mid = (l+r)/2;
@@ -14,4 +36,3 @@ while(l <= r) {
         l = mid + 1;
     }
 }
-*/
