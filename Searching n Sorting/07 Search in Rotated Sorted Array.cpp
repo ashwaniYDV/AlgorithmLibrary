6c748b98552d487ab511f8835b2c9d03b5a3826a
Ashwani Yadav <ashyadavash@gmail.com>
2021-05-12 03:14:04 +0530

    Create 07 Search in Rotated Sorted Array.cpp

---
+++ Searching n Sorting/07 Search in Rotated Sorted Array.cpp
@@ -0,0 +1,79 @@
+// https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        if (n == 0) return -1;
+
+        int left = 0, right = n - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (target == nums[mid]) {
+                return mid;
+            }
+            
+            //if left part is monotonically increasing, or the pivot point is on the right part
+            if (nums[left] <= nums[mid]) {
+                //must use "<=" at here since we need to make sure target is in the left part,
+                //then safely drop the right part
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    //right bias
+                    left = mid + 1;
+                }
+            }
+
+            //if right part is monotonically increasing, or the pivot point is on the left part
+            else {
+                //must use "<=" at here since we need to make sure target is in the right part,
+                //then safely drop the left part
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l=0;
+        int r = nums.size()-1;
+        while(l <= r) {
+            int m = l+(r-l)/2;
+            if(nums[m] == target) {
+                return m;
+            }
+            if(nums[l] <= nums[m]) {
+                if(target >= nums[l] && target < nums[m]) r = m-1;
+                else l = m+1;
+            } else {
+                if(target > nums[m] && target <= nums[r]) l = m+1;
+                else r = m-1;
+            }
+        }
+        return -1;
+    }
+};
