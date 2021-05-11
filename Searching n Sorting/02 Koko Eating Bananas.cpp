037c524be5031feee92425b8b7544d44e9821c61
Ashwani Yadav <ashyadavash@gmail.com>
2021-05-11 19:18:20 +0530

    Update 02 Koko Eating Bananas.cpp

--- Searching n Sorting/02 Koko Eating Bananas.cpp
+++ Searching n Sorting/02 Koko Eating Bananas.cpp
@@ -23,3 +23,29 @@ public:
         return left;
     }
 };
+
+
+
+
+
+
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int H) {
+        int n = piles.size();
+        int l = 1, r = 0;
+        for(int x: piles) {
+            r = max(r, x);
+        }
+        while (l < r) {
+            int m = (l + r) / 2, total = 0;
+            for (int p : piles)
+                total += (p + m - 1) / m;
+            if (total > H)
+                l = m + 1;
+            else
+                r = m;
+        }
+        return l;
+    }
+};
