fc8c3b0e7b6c439ba5fe089959ad773767a82b4c
Ashwani Yadav <ashyadavash@gmail.com>
2021-05-13 01:04:51 +0530

    Update and rename 10 Standard Merge Sort Problem (Find no of ordered pairs).cpp to 10 Standard Merge Sort Prob (Count Inversions in an array).cpp

--- Searching n Sorting/10 Standard Merge Sort Prob (Count Inversions in an array).cpp
+++ Searching n Sorting/10 Standard Merge Sort Prob (Count Inversions in an array).cpp
@@ -1,7 +1,22 @@
+// https://www.geeksforgeeks.org/counting-inversions/
 // https://www.hackerearth.com/practice/algorithms/sorting/merge-sort/tutorial/
 
 /*
 Given an array A on size N, you need to find the number of ordered pairs (i, j) such that i < j  and A[i] > A[j].
+
+Algorithm: 
+1) The idea is similar to merge sort, divide the array into two equal or almost equal halves in each step until the base case is reached.
+2) Create a function merge that counts the number of inversions when two halves of the array are merged, 
+   create two indices i and j, i is the index for the first half, and j is an index of the second half. 
+   if a[i] is greater than a[j], then there are (mid – i) inversions. because left and right subarrays are sorted, 
+   so all the remaining elements in left-subarray (a[i+1], a[i+2] ... a[mid]) will be greater than a[j].
+*/
+
+/*
+This is known as “Inversion Counting” problem.
+It can be solved using a Binary Indexed Tree (BIT) or Merge Sort.
+
+http://www.geeksforgeeks.org/count-inversions-array-set-3-using-bit/ 121
 */
 
 
