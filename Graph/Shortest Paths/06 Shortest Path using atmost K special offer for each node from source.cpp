bd9a4a394097aed37e4a845fff38632a40cfa0ce
Ashwani Yadav <ashyadavash@gmail.com>
2021-05-15 20:43:02 +0530

    Update 06 Shortest Path using atmost K special offer for each node from source.cpp

--- Graph/Shortest Paths/06 Shortest Path using atmost K special offer for each node from source.cpp
+++ Graph/Shortest Paths/06 Shortest Path using atmost K special offer for each node from source.cpp
@@ -37,60 +37,9 @@ dp[v][j] = weight of edge(u, v) + dp[u][j]
 dp[v][j+1] = dp[u][j]
 */
 
-#include<bits/stdc++.h>
-using namespace std;
-#pragma GCC optimize ("O3")
-#pragma GCC target ("avx")
-#pragma GCC optimize("Ofast")
-#pragma GCC optimize ("unroll-loops")
-#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
- 
-// #include <ext/pb_ds/assoc_container.hpp> 
-// #include <ext/pb_ds/tree_policy.hpp> 
-// using namespace __gnu_pbds;
-// typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
-// typedef tree<int,int,less<int>,rb_tree_tag,tree_order_statistics_node_update> ordered_map;
-// methods: find_by_order(k); & order_of_key(k); To make it an ordered_multiset, use pairs of (value, time_of_insertion) to distinguish values which are similar
- 
-typedef long long int ll;
-#define int long long int
-#define ld long double
-#define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-#define FRE freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
-#define PRECISION(x) cout << setprecision(x); cout << fixed;
-#define UNIQUE(a) sort(a.begin(), a.end() ); a.erase( unique( a.begin(), a.end() ), a.end() );
-#define debug(x) cout << #x << " is: " << (x) << endl;
-#define debug2(x,y) cout << (#x) <<", "<<(#y)<< " are: " << (x) <<", "<<(y)<< endl;
-#define debugx(x) cout << #x << " is: " << (x) << endl; exit(0);
-#define kickstart(testcase, res) cout << "Case #" << testcase << ": " << res << endl;
-#define f(i,n) for(int i=0;i<n;i++)
-#define fa(i,a,n) for(int i=a;i<n;i++)
-#define loop(i,a, n) for(int i = a; i <= n; i++)
-#define loopb(i,a, n) for(int i = a; i >= n; i--)
-#define pb push_back
-#define pf push_front
-#define F first
-#define S second
-#define all(x) x.begin(), x.end()
-#define uniq(v) (v).erase(unique(all(v)),(v).end())
-#define PI 3.1415926535897932384626
-#define MOD 1000000007
-#define MOD2 998244353
-#define INT_INF 1011111111
-#define INF 1000111000111000111LL
-// comment below line in interactive mode (since endl flushes stdout)
-// #define endl "\n"
-typedef vector<int> vi;
-typedef vector<pair<int, int>> vpii;
-typedef pair<int, int> pii;
-typedef vector<vector<int>> matrix;
-int dx[] = {0, 1, 0, -1, -1, 1, -1, 1};
-int dy[] = {-1, 0, 1, 0, -1, 1, 1, -1};
-const int MAXN = 1e5+5;
+
 const int N = 5e5+5;
 int n, m;
-
-
 vector<pair<int,int>> g[N];
 int k;
 int dp[N][20];
@@ -147,13 +96,3 @@ void solve() {
 
     dijkstra();
 }
-
-
-signed main() {
-    IOS
-    int t = 1;
-    // cin >> t;
-    while(t--) {
-        solve();
-    }
-} 
