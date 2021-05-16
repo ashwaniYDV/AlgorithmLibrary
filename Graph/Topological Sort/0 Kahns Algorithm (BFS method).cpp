d858c99f65cfc754cc7bf12caed1faa0dc4fc612
Ashwani Yadav <ashyadavash@gmail.com>
2021-05-16 22:38:28 +0530

    Update 0 Kahns Algorithm (BFS method).cpp

--- Graph/Topological Sort/0 Kahns Algorithm (BFS method).cpp
+++ Graph/Topological Sort/0 Kahns Algorithm (BFS method).cpp
@@ -1,57 +1,12 @@
 // https://cses.fi/problemset/task/1679/
 
-#include<bits/stdc++.h>
-using namespace std;
-#pragma GCC optimize ("O3")
-#pragma GCC target ("avx")
-#pragma GCC optimize("Ofast")
-#pragma GCC optimize ("unroll-loops")
-#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
- 
-// #include <ext/pb_ds/assoc_container.hpp> 
-// #include <ext/pb_ds/tree_policy.hpp> 
-// using namespace __gnu_pbds;
-// typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
-// typedef tree<int,int,less<int>,rb_tree_tag,tree_order_statistics_node_update> ordered_map;
-// methods: find_by_order(k); & order_of_key(k); To make it an ordered_multiset, use pairs of (value, time_of_insertion) to distinguish values which are similar
- 
-typedef long long int ll;
-#define int long long int
-#define ld long double
-#define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-#define FRE freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
-#define PRECISION(x) cout << setprecision(x); cout << fixed;
-#define debug(x) cout << #x << " is: " << (x) << endl;
-#define debug2(x,y) cout << (#x) <<", "<<(#y)<< " are: " << (x) <<", "<<(y)<< endl;
-#define debugx(x) cout << #x << " is: " << (x) << endl; exit(0);
-#define kickstart(testcase, res) cout << "Case #" << testcase << ": " << res << endl;
-#define f(i,n) for(int i=0;i<n;i++)
-#define fa(i,a,n) for(int i=a;i<n;i++)
-#define loop(i,a, n) for(int i = a; i <= n; i++)
-#define loopb(i,a, n) for(int i = a; i >= n; i--)
-#define pb push_back
-#define pf push_front
-#define F first
-#define S second
-#define all(x) x.begin(), x.end()
-#define uniq(v) (v).erase(unique(all(v)),(v).end())
-#define PI 3.1415926535897932384626
-#define MOD 1000000007
-#define MOD2 998244353
-#define INT_INF 1011111111
-#define INF 1000111000111000111LL
-// comment below line in interactive mode (since endl flushes stdout)
-#define endl "\n"
-typedef vector<int> vi;
-typedef vector<pair<int, int>> vpii;
-typedef pair<int, int> pii;
-typedef vector<vector<int>> matrix;
-int dx[] = {0, 1, 0, -1, -1, 1, -1, 1};
-int dy[] = {-1, 0, 1, 0, -1, 1, 1, -1};
-const int MAXN = 1e5+5;
+/*
+Topological sorting for Directed Acyclic Graph (DAG) is a linear ordering of vertices such that for every directed edge u v, 
+vertex u comes before v in the ordering. Topological Sorting for a graph is not possible if the graph is not a DAG.
+*/
+
 const int N = 2e5+5;
 int n, m; 
-
 int in[N];
 vi g[N];
 vector<int> res;
@@ -90,13 +45,4 @@ void solve() {
 		in[v]++;
 	}
 	kahn(n);
-}
- 
-signed main() {
-	IOS
-	int T = 1;
-	// cin >> T;
-	while(T--)
-	solve();
-	return 0;
 }
