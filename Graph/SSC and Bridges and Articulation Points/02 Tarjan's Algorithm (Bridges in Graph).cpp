3de9775dfc1fce48fd7b272dc0768ab721608e2a
Ashwani Yadav <ashyadavash@gmail.com>
2021-05-17 16:42:19 +0530

    Update 02 Tarjan's Algorithm (Bridges in Graph).cpp

--- Graph/SSC and Bridges and Articulation Points/02 Tarjan's Algorithm (Bridges in Graph).cpp
+++ Graph/SSC and Bridges and Articulation Points/02 Tarjan's Algorithm (Bridges in Graph).cpp
@@ -4,7 +4,9 @@
 The idea is similar to O(V+E) algorithm for Articulation Points. 
 We do DFS traversal of the given graph. 
 In DFS tree an edge (u, v) is bridge if there does not exist any other alternative to reach u or an ancestor of u from subtree rooted with v. 
-The value low[v] indicates earliest visited vertex reachable from subtree rooted with v. The condition for an edge (u, v) to be a bridge is, “low[v] > in-time[u]”.
+
+The value low[v] indicates earliest visited vertex reachable from subtree rooted with v. 
+The condition for an edge (u, v) to be a bridge is, “low[v] > in-time[u]”.
 */
 
 #include<bits/stdc++.h>
