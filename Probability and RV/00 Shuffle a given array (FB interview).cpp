aa0aab561562a0b7cf372d3c2d9dfd78f0c3f16b
Ashwani Yadav <ashyadavash@gmail.com>
2021-05-20 17:58:21 +0530

    Update 00 Shuffle a given array (FB interview).cpp

--- Probability and RV/00 Shuffle a given array (FB interview).cpp
+++ Probability and RV/00 Shuffle a given array (FB interview).cpp
@@ -1,7 +1,8 @@
 // Shuffle a given array using Fisher–Yates shuffle Algorithm
 // https://www.geeksforgeeks.org/shuffle-a-given-array-using-fisher-yates-shuffle-algorithm/
+// Rachit: https://youtu.be/hSZARPLUSDM 
 
-// C++ Program to shuffle a given array
+// Method 1
 #include<bits/stdc++.h>
 #include <stdlib.h>
 #include <time.h>
@@ -23,9 +24,7 @@ void printArray (int arr[], int n) {
 
 // A function to generate a random permutation of arr[]
 void randomize (int arr[], int n) {
-	// Use a different seed value so that
-	// we don't get same result each time
-	// we run this program
+	// Use a different seed value so that we don't get same result each time we run this program
 	srand (time(NULL));
 
 	// Start from the last element and swap one by one. We don't need to run for the first element that's why i > 0
@@ -52,3 +51,73 @@ int main() {
 7 8 4 6 3 1 2 5
 Note: Output will differ every time as it selects and prints random elements
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Method 2
+#include<bits/stdc++.h>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+// A utility function to swap to integers
+void swap (int *a, int *b) {
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+// A utility function to print an array
+void printArray (int arr[], int n) {
+	for (int i = 0; i < n; i++)
+		cout << arr[i] << " ";
+	cout << "\n";
+}
+
+// A function to generate a random permutation of arr[]
+void randomize (int arr[], int n) {
+	// Use a different seed value so that we don't get same result each time we run this program
+	srand (time(NULL));
+
+	for (int i = 0; i < n; i++) {
+		// Pick a random index from i to n-1
+		int mn = i, mx = n-1;
+		int j = mn + (rand() % (mx - mn + 1));
+
+		// Swap arr[i] with the element at random index
+		swap(&arr[i], &arr[j]);
+	}
+}
+
+// Driver Code
+int main() {
+	int arr[] = {1, 2, 3, 4, 5, 6, 7, 8};
+	int n = sizeof(arr) / sizeof(arr[0]);
+	randomize (arr, n);
+	printArray(arr, n);
+
+	return 0;
+}
