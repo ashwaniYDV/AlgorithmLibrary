bda3d87d232256263258e592c610f441eaf6b3a5
Ashwani Yadav <ashyadavash@gmail.com>
2021-05-21 23:31:49 +0530

    Create Linear Sieve.cpp

---
+++ Number Theory/Sieve/Linear Sieve.cpp
@@ -0,0 +1,15 @@
+// https://cp-algorithms.com/algebra/prime-sieve-linear.html
+// https://unacademy.com/class/number-theoretic-functions-and-linear-sieve/5DJIRORJ
+
+const int N = 10000000;
+int lp[N+1]; // lowest prime
+vector<int> pr; // primes <= N
+
+for (int i=2; i<=N; ++i) {
+    if (lp[i] == 0) {
+        lp[i] = i;
+        pr.push_back (i);
+    }
+    for (int j=0; j<(int)pr.size() && pr[j]<=lp[i] && i*pr[j]<=N; ++j)
+        lp[i * pr[j]] = pr[j];
+}
