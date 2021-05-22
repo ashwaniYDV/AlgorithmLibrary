b986950acfbf2944317eed1712bbce0a7bb900d4
Ashwani Yadav <ashyadavash@gmail.com>
2021-05-22 18:52:29 +0530

    Update SegSieve (1 to large n).cpp

--- Number Theory/Sieve/SegSieve (1 to large n).cpp
+++ Number Theory/Sieve/SegSieve (1 to large n).cpp
@@ -64,109 +64,109 @@ vector<int> prGlobal;
 // found primes in vector prime[]
 void simpleSieve(int limit, vector<int> &prime)
 {
-	// Create a boolean array "mark[0..n-1]" and initialize
-	// all entries of it as true. A value in mark[p] will
-	// finally be false if 'p' is Not a prime, else true.
-	vector<bool> mark(limit + 1, true);
+    // Create a boolean array "mark[0..n-1]" and initialize
+    // all entries of it as true. A value in mark[p] will
+    // finally be false if 'p' is Not a prime, else true.
+    vector<bool> mark(limit + 1, true);
 
-	for (int p=2; p*p<limit; p++)
-	{
-		// If p is not changed, then it is a prime
-		if (mark[p] == true)
-		{
-			// Update all multiples of p
-			for (int i=p*p; i<limit; i+=p)
-				mark[i] = false;
-		}
-	}
+    for (int p=2; p*p<=limit; p++)
+    {
+        // If p is not changed, then it is a prime
+        if (mark[p] == true)
+        {
+            // Update all multiples of p
+            for (int i=p*p; i<=limit; i+=p)
+                mark[i] = false;
+        }
+    }
 
-	// Print all prime numbers and store them in prime
-	for (int p=2; p<limit; p++)
-	{
-		if (mark[p] == true)
-		{
-			prime.push_back(p);
-			prGlobal.push_back(p);
-		}
-	}
+    // Print all prime numbers and store them in prime
+    for (int p=2; p<=limit; p++)
+    {
+        if (mark[p] == true)
+        {
+            prime.push_back(p);
+            prGlobal.push_back(p);
+        }
+    }
 }
 
 // Prints all prime numbers smaller than 'n'
 void segmentedSieve(int n) {
-	// Compute all primes smaller than or equal
-	// to square root of n using simple sieve
-	int limit = floor(sqrt(n))+1;
-	vector<int> prime;
-	prime.reserve(limit);
-	simpleSieve(limit, prime);
+    // Compute all primes smaller than or equal
+    // to square root of n using simple sieve
+    int limit = floor(sqrt(n))+1;
+    vector<int> prime;
+    prime.reserve(limit);
+    simpleSieve(limit, prime);
 
-	// Divide the range [0..n-1] in different segments
-	// We have chosen segment size as sqrt(n).
-	int low = limit;
-	int high = 2*limit;
+    // Divide the range [0..n-1] in different segments
+    // We have chosen segment size as sqrt(n).
+    int low = limit;
+    int high = 2*limit;
 
-	// While all segments of range [0..n-1] are not processed,
-	// process one segment at a time
-	while (low < n)
-	{
-		if (high >= n)
-		high = n;
-		
-		// To mark primes in current range. A value in mark[i]
-		// will finally be false if 'i-low' is Not a prime,
-		// else true.
-		bool mark[limit+1];
-		memset(mark, true, sizeof(mark));
+    // While all segments of range [0..n-1] are not processed,
+    // process one segment at a time
+    while (low < n)
+    {
+        if (high >= n)
+        high = n;
+        
+        // To mark primes in current range. A value in mark[i]
+        // will finally be false if 'i-low' is Not a prime,
+        // else true.
+        bool mark[limit+1];
+        memset(mark, true, sizeof(mark));
 
-		// Use the found primes by simpleSieve() to find
-		// primes in current range
-		for (int i = 0; i < prime.size(); i++)
-		{
-			// Find the minimum number in [low..high] that is
-			// a multiple of prime[i] (divisible by prime[i])
-			// For example, if low is 31 and prime[i] is 3,
-			// we start with 33.
-			int loLim = floor(low/prime[i]) * prime[i];
-			if (loLim < low)
-				loLim += prime[i];
+        // Use the found primes by simpleSieve() to find
+        // primes in current range
+        for (int i = 0; i < prime.size(); i++)
+        {
+            // Find the minimum number in [low..high] that is
+            // a multiple of prime[i] (divisible by prime[i])
+            // For example, if low is 31 and prime[i] is 3,
+            // we start with 33.
+            int loLim = floor(low/prime[i]) * prime[i];
+            if (loLim < low)
+                loLim += prime[i];
 
-			/* Mark multiples of prime[i] in [low..high]:
-				We are marking j - low for j, i.e. each number
-				in range [low, high] is mapped to [0, high-low]
-				so if range is [50, 100] marking 50 corresponds
-				to marking 0, marking 51 corresponds to 1 and
-				so on. In this way we need to allocate space only
-				for range */
-			for (int j=loLim; j<high; j+=prime[i])
-				mark[j-low] = false;
-		}
+            /* Mark multiples of prime[i] in [low..high]:
+                We are marking j - low for j, i.e. each number
+                in range [low, high] is mapped to [0, high-low]
+                so if range is [50, 100] marking 50 corresponds
+                to marking 0, marking 51 corresponds to 1 and
+                so on. In this way we need to allocate space only
+                for range */
+            for (int j=loLim; j<=high; j+=prime[i])
+                mark[j-low] = false;
+        }
 
-		// Numbers which are not marked as false are prime
-		for (int i = low; i<high; i++) {
-			if (mark[i - low] == true) {
-				prGlobal.push_back(i);
-			}
-		}
+        // Numbers which are not marked as false are prime
+        for (int i = low; i<=high; i++) {
+            if (mark[i - low] == true) {
+                prGlobal.push_back(i);
+            }
+        }
 
-		// Update low and high for next segment
-		low = low + limit;
-		high = high + limit;
-	}
+        // Update low and high for next segment
+        low = low + limit;
+        high = high + limit;
+    }
 }
 
 vector<int> pref;
 void precalc() {
-	// int n = 1000000531;
-	int n = 1000000009+1;
-	segmentedSieve(n);
-	for(int i = 0; i < prGlobal.size() - 1; i++) {
-		pref.push_back(prGlobal[i] * prGlobal[i+1]);
-	}
+    // int n = 1000000531;
+    int n = 1000000009+1;
+    segmentedSieve(n);
+    for(int i = 0; i < prGlobal.size() - 1; i++) {
+        pref.push_back(prGlobal[i] * prGlobal[i+1]);
+    }
 }
 
 void solve(int testcase) {
-	int x, y, z;
-	cin >> n;
+    int x, y, z;
+    cin >> n;
     x = lower_bound(pref.begin(), pref.end(), n) - pref.begin();
     cout << "Case #" << testcase << ": ";
     if(pref[x] == n)
@@ -176,11 +176,11 @@ void solve(int testcase) {
 }
  
 signed main() {
-	IOS
-	precalc();
-	int t = 1;
-	cin >> t;
-	f(i, t) {
-		solve(i+1);
-	}
+    IOS
+    precalc();
+    int t = 1;
+    cin >> t;
+    f(i, t) {
+        solve(i+1);
+    }
 } 
