7f2ae0fe7f3fb8c846c4f6eecb06a5e00f5d6a48
Ashwani Yadav <ashyadavash@gmail.com>
2021-05-26 17:21:50 +0530

    Update J - Sushi.cpp

--- DP/Atcoder Educational DP Contest/J - Sushi.cpp
+++ DP/Atcoder Educational DP Contest/J - Sushi.cpp
@@ -39,6 +39,7 @@ dp[x][y][z]                    = (1 + p3 * dp[x-1][y+1][z] + p2 * dp[x][y-1][z+1
 */
 
 
+// Method 1
 ld dp[305][305][305];
 
 void solve() {
@@ -96,6 +97,117 @@ void solve() {
     }
 
     cout << dp[cnt[3]][cnt[2]][cnt[1]] << endl;
+}
+
+signed main() {
+    IOS
+    int t = 1;
+    PRECISION(10)
+    // cin >> t;
+    f(i, t) {
+        solve();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Method 2
+ld dp[305][305][305];
+
+void solve() {
+    int x;
+    cin >> n;
+    vi cnt(4, 0);
+
+    f(i, n) {
+        cin >> x;
+        cnt[x]++;
+    }
+
+    // base case (zero = n)
+    dp[0][0][0] = 0;
+
+    for (int three = 0; three <= n; three++) {
+        for (int two = 0; two <= n; two++) {
+            for (int one = 0; one <= n; one++) {
+                int zero = n - one - two - three;
+                
+                // this is base case already covered
+                if(zero == n) {
+                    continue;
+                }
+
+                // not possible state
+                if(one + two + three > n) {
+                    continue;
+                }
+
+                ld value = 1.0;
+
+                ld p0 = (1.0 * zero / n);
+                ld p1 = (1.0 * one / n);
+                ld p2 = (1.0 * two / n);
+                ld p3 = (1.0 * three / n);
+
+                // pick 1
+                if(one > 0)
+                    value += p1 * dp[one - 1][two][three]; 
+
+                // pick 2
+                if(two > 0)
+                    value += p2 * dp[one + 1][two - 1][three]; 
+
+                // pick 3
+                if(three > 0)
+                    value += p3 * dp[one][two + 1][three - 1]; 
+
+                value /= (1 - p0);
+
+                dp[one][two][three] = value;
+            }
+        }
+    }
+
+    cout << dp[cnt[1]][cnt[2]][cnt[3]] << endl;
 }
 
 signed main() {
