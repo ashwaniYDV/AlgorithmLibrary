ff0b9ba014677a32282036fe4e55f326cdbbe81e
Ashwani Yadav <ashyadavash@gmail.com>
2021-05-30 16:09:40 +0530

    Update 01 Assignment Problem.cpp

--- DP/Bitmask DP/01 Assignment Problem.cpp
+++ DP/Bitmask DP/01 Assignment Problem.cpp
@@ -31,47 +31,9 @@ N <= 20
 
 
 // Method 1: (Less memory efficient)
-#include<bits/stdc++.h>
-using namespace std;
- 
-#include <ext/pb_ds/assoc_container.hpp> 
-#include <ext/pb_ds/tree_policy.hpp> 
-using namespace __gnu_pbds;
-typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
- 
-typedef long long int ll;
-#define int long long int
-#define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-#define FRE freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
-#define PRECISION(x) cout << setprecision(x); cout << fixed;
-#define debug(x) cout << #x << " is: " << (x) << endl;
-#define debugx(x) cout << #x << " is: " << (x) << endl; exit(0);
-#define f(i,n) for(int i=0;i<n;i++)
-#define fa(i,a,n) for(int i=a;i<n;i++)
-#define loop(i,a, n) for(int i = a; i <= n; i++)
-#define loopb(i,a, n) for(int i = a; i >= n; i--)
-#define pb push_back
-#define pf push_front
-#define F first
-#define S second
-#define all(x) x.begin(), x.end()
-#define uniq(v) (v).erase(unique(all(v)),(v).end())
-#define ppc __builtin_popcount
-#define ppcll __builtin_popcountll
-#define PI 3.1415926535897932384626
-#define MOD 1000000007
-#define MOD2 998244353
-#define INT_INF 1011111111
-#define INF 1000111000111000111LL
-// comment below line in interactive mode (since endl flushes stdout)
-#define endl "\n"
-typedef vector<vector<int>> matrix;
-int dx[] = {0, 1, 0, -1, -1, 1, -1, 1};
-int dy[] = {-1, 0, 1, 0, -1, 1, 1, -1};
-const int MAXN = 1e5+5;
+
 const int N = 20;
 int n, m;
-
 
 int dp[20][1 << 20];
 vector<vector<int>> cost;
@@ -104,14 +66,13 @@ void solve() {
     }   
     cout << go(0, 0);
 }
-int32_t main() {
-    IOS
-    int T = 1;
-    // cin >> T;
-    while(T--)
-    solve();
-    return 0;
-}
+
+
+
+
+
+
+
 
 
 
@@ -125,47 +86,9 @@ int32_t main() {
 
 
 // Method 2: (More memory efficient)
-#include<bits/stdc++.h>
-using namespace std;
- 
-#include <ext/pb_ds/assoc_container.hpp> 
-#include <ext/pb_ds/tree_policy.hpp> 
-using namespace __gnu_pbds;
-typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
- 
-typedef long long int ll;
-#define int long long int
-#define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-#define FRE freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
-#define PRECISION(x) cout << setprecision(x); cout << fixed;
-#define debug(x) cout << #x << " is: " << (x) << endl;
-#define debugx(x) cout << #x << " is: " << (x) << endl; exit(0);
-#define f(i,n) for(int i=0;i<n;i++)
-#define fa(i,a,n) for(int i=a;i<n;i++)
-#define loop(i,a, n) for(int i = a; i <= n; i++)
-#define loopb(i,a, n) for(int i = a; i >= n; i--)
-#define pb push_back
-#define pf push_front
-#define F first
-#define S second
-#define all(x) x.begin(), x.end()
-#define uniq(v) (v).erase(unique(all(v)),(v).end())
-#define ppc __builtin_popcount
-#define ppcll __builtin_popcountll
-#define PI 3.1415926535897932384626
-#define MOD 1000000007
-#define MOD2 998244353
-#define INT_INF 1011111111
-#define INF 1000111000111000111LL
-// comment below line in interactive mode (since endl flushes stdout)
-#define endl "\n"
-typedef vector<vector<int>> matrix;
-int dx[] = {0, 1, 0, -1, -1, 1, -1, 1};
-int dy[] = {-1, 0, 1, 0, -1, 1, 1, -1};
-const int MAXN = 1e5+5;
+
 const int N = 20;
 int n, m;
-
 
 int dp[1 << 20];
 vector<vector<int>> cost;
@@ -198,13 +121,4 @@ void solve() {
         }
     }   
     cout << go(0);
-}
-
-int32_t main() {
-    IOS
-    int T = 1;
-    // cin >> T;
-    while(T--)
-    solve();
-    return 0;
 }
