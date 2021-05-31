17ecee1aa6345817840491db3f62056db0f50eb8
Ashwani Yadav <ashyadavash@gmail.com>
2021-05-31 23:27:59 +0530

    Update 01 Longest Common Subsequence.cpp

--- DP/LCS/01 Longest Common Subsequence.cpp
+++ DP/LCS/01 Longest Common Subsequence.cpp
@@ -1,32 +1,3 @@
-#include<bits/stdc++.h>
-using namespace std;
- 
-typedef long long int ll;
-#define int long long int
-#define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-#define FRE freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
-#define debug(x) cout << #x << " is: " << (x) << endl;
-#define debugx(x) cout << #x << " is: " << (x) << endl; exit(0);
-#define f(i,n) for(int i=0;i<n;i++)
-#define fa(i,a,n) for(int i=a;i<n;i++)
-#define loop(i,a, n) for(int i = a; i <= n; i++)
-#define loopb(i,a, n) for(int i = a; i >= n; i--)
-#define pb push_back
-#define pf push_front
-#define F first
-#define S second
-#define all(x) x.begin(), x.end()
-#define PI 3.1415926535897932384626
-#define MOD 1000000007
-#define INF 1000111000111000111LL
-// comment below line in interactive mode (since endl flushes stdout)
-#define endl "\n"
-typedef vector<vector<int>> matrix;
-int dx[] = {0, 1, 0, -1, -1, 1, -1, 1};
-int dy[] = {-1, 0, 1, 0, -1, 1, 1, -1};
-const int MAXN = 1e5 + 5;
-const int N = 1e5+5;
-int n, m;
 
 ll fun(int pos1, int pos2, string s1, string s2) {
     if(pos1 < 0 || pos2 < 0) return 0;
@@ -78,12 +49,43 @@ void solve() {
     LC_Subsequence_Bottom_Up(s1, s2);
     cout << fun(s1.size() - 1, s2.size() - 1, s1, s2) << endl;
 }
- 
-int32_t main() {
-    IOS
-    int T = 1;
-    // cin >> T;
-    while(T--)
-    solve();
-    return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+string lcs(string& A, string& B) {
+    int n = A.size(), m = B.size();
+    vector<vector<string>> dp(n + 1, vector<string>(m + 1, ""));
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < m; ++j)
+            if (A[i] == B[j])
+                dp[i + 1][j + 1] = dp[i][j] + A[i];
+            else
+                dp[i + 1][j + 1] = dp[i + 1][j].size() > dp[i][j + 1].size() ?  dp[i + 1][j] : dp[i][j + 1];
+    return dp[n][m];
 }
