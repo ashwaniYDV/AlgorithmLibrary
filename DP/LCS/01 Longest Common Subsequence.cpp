402650b5a957855b89d5fe5996f4f4bd2940ee7f
Ashwani Yadav <ashyadavash@gmail.com>
2021-05-31 23:33:11 +0530

    Update 01 Longest Common Subsequence.cpp

--- DP/LCS/01 Longest Common Subsequence.cpp
+++ DP/LCS/01 Longest Common Subsequence.cpp
@@ -81,11 +81,16 @@ void solve() {
 string lcs(string& A, string& B) {
     int n = A.size(), m = B.size();
     vector<vector<string>> dp(n + 1, vector<string>(m + 1, ""));
-    for (int i = 0; i < n; ++i)
-        for (int j = 0; j < m; ++j)
-            if (A[i] == B[j])
-                dp[i + 1][j + 1] = dp[i][j] + A[i];
-            else
-                dp[i + 1][j + 1] = dp[i + 1][j].size() > dp[i][j + 1].size() ?  dp[i + 1][j] : dp[i][j + 1];
+    for (int i = 0; i <= n; ++i) {
+        for (int j = 0; j <= m; ++j) {
+            if(i == 0 || j == 0) {
+                dp[i][j] = "";
+            } else if (A[i-1] == B[j-1]) {
+                dp[i][j] = dp[i - 1][j - 1] + A[i - 1];
+            } else {
+                dp[i][j] = dp[i - 1][j].size() > dp[i][j - 1].size() ?  dp[i - 1][j] : dp[i][j - 1];
+            }
+        }
+    }
     return dp[n][m];
 }
