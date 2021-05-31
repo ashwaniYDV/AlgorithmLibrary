40cde004c92a73c8a4e4952c20079d02898cda4d
Ashwani Yadav <ashyadavash@gmail.com>
2021-05-31 13:16:17 +0530

    Update 00 SOS.cpp

--- DP/SOS DP/00 SOS.cpp
+++ DP/SOS DP/00 SOS.cpp
@@ -30,6 +30,7 @@ void solve() {
         dp[mask][0] = a[mask];
     }
 
+    // ordering of these 2 for loops doesn't matter. we can interchange these
     for(int mask = 0; mask < size; mask++) {
         for(int i = 1; i <= n; i++) {
             // case 1: ith bit is on (since binary rep is 0 indexed, we'll use (i-1) in code)
@@ -51,6 +52,62 @@ void solve() {
         cout << f[mask] << " ";
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void solve() {
+    int x, y, z;
+    cin >> n;
+    int size = (1 << n);
+    int a[size];
+    for(int mask = 0; mask < size; mask++) 
+        cin >> a[mask];
+
+
+    int f[size];
+    int dp[size][n+1];
+
+    // base case
+    for(int mask = 0; mask < size; mask++) {
+        dp[mask][0] = a[mask];
+    }
+
+    for(int i = 1; i <= n; i++) {
+        for(int mask = 0; mask < size; mask++) {
+            // case 1: ith bit is on (since binary rep is 0 indexed, we'll use (i-1) in code)
+            if(mask & (1 << (i-1))) {
+                dp[mask][i] = dp[mask][i-1] + dp[mask ^ (1 << (i-1))][i-1];
+            }
+            // case 2: ith bit is off
+            else {
+                dp[mask][i] = dp[mask][i-1];
+            }
+        }
+    }
+
+    for(int mask = 0; mask < size; mask++) {
+        f[mask] = dp[mask][n];
+    }
+
+    for(int mask = 0; mask < size; mask++) {
+        cout << f[mask] << " ";
+    }
+}
+
+
+
+
 
 
 
@@ -101,6 +158,11 @@ void solve() {
         cout << f[mask] << " ";
     }
 }
+
+
+
+
+
 
 
 
