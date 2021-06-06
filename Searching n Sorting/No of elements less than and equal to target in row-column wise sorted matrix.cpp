7dedc1ec55a151f77d833141f51f87df3a9407e7
Ashwani Yadav <ashyadavash@gmail.com>
2021-06-06 14:00:28 +0530

    Update No of elements less than and equal to target in row-column wise sorted matrix.cpp

--- Searching n Sorting/No of elements less than and equal to target in row-column wise sorted matrix.cpp
+++ Searching n Sorting/No of elements less than and equal to target in row-column wise sorted matrix.cpp
@@ -1,16 +1,21 @@
-class Solution {
-public:
-    int countNoOfEleUptoTarget(vector<vector<int>> &mat, int target) {
-        int n = mat.size(), m = mat[0].size();
-        int i = n-1, j = 0, cnt = 0;
-        while(i >= 0 && j < m) {
-            if(mat[i][j] <= target) {
-                cnt += i + 1;
-                j++;
-            } else {
-                i--;
-            }
+int countNoOfEleUptoTarget(vector<vector<int>> &mat, int target) {
+    int n = mat.size(), m = mat[0].size();
+    int i = n-1, j = 0, cnt = 0;
+    while(i >= 0 && j < m) {
+        if(mat[i][j] <= target) {
+            cnt += i + 1;
+            j++;
+        } else {
+            i--;
         }
-        return cnt;
     }
-};
+    return cnt;
+}
+
+void solve() {
+    matrix a = {{ -4, -3, -1, 3, 4 },
+                { -3, -2, 2, 3, 5 },
+                { -1, 1, 3, 5, 8 },
+                { 3, 4, 7, 8, 9 }};
+    cout << countNoOfEleUptoTarget(a, 3);
+}
