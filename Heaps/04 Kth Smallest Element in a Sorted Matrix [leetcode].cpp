90afefb6f0a7a448c8fd8e96f3d4511177fe5946
Ashwani Yadav <ashyadavash@gmail.com>
2021-06-06 12:36:21 +0530

    Update 04 Kth Smallest Element in a Sorted Matrix [leetcode].cpp

--- Heaps/04 Kth Smallest Element in a Sorted Matrix [leetcode].cpp
+++ Heaps/04 Kth Smallest Element in a Sorted Matrix [leetcode].cpp
@@ -7,6 +7,25 @@ Note that it is the kth smallest element in the sorted order, not the kth distin
 */
 
 
+
+/*
+Using Heap :- Here, we know that the row and column is already sorted. 
+Therefore, instead of traversing in the entire matrix we can take advantage of that. 
+We can take a Min Heap and push the First row(value, indexes) into Min heap. 
+You can push first column also, and write your answer according to that.
+
+Now,follow these steps:-
+
+Pop the element from min heap and check whether it is the Kth smallest element. If it is return the value.
+Else push the next row value (Note:- column is going to remain same).
+The reason behind this logic is, 
+when we remove the smallest element from Min Heap (i.e our 1st smallest element, which is always present in the 1st row and 1st column), 
+then the 2nd smallest element may lie in that 2nd row and 1st column or it may be present in the First Row and column greater than 1.
+
+Time Complexity:- O(NlogN)
+Space Complexity:- O(N)
+*/
+
 class Solution {
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
