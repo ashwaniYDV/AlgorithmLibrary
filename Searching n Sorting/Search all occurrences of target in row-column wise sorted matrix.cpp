4eb58aefd40c2e154d7d3b57a4c0db332ae2f1cd
Ashwani Yadav <ashyadavash@gmail.com>
2021-06-06 13:45:52 +0530

    Update Search all occurrences of target in row-column wise sorted matrix.cpp

--- Searching n Sorting/Search all occurrences of target in row-column wise sorted matrix.cpp
+++ Searching n Sorting/Search all occurrences of target in row-column wise sorted matrix.cpp
@@ -1,10 +1,25 @@
+void findInRow(vector<vector<int>> &mat, int x, int i, int j) {
+    while(j >= 0 && mat[i][j] == x) {
+        cout << "Element " << x << " is found at position (" << i << ", " << j << ")" << endl;
+        j--;
+    }
+}
 
-void matSearch (vector <vector <int>> &mat, int x) {
+void findInColumn(vector<vector<int>> &mat, int x, int i, int j) {
+    while(i < mat.size() && mat[i][j] == x) {
+        cout << "Element " << x << " is found at position (" << i << ", " << j << ")" << endl;
+        i++;
+    }
+}
+
+void matSearch (vector<vector<int>> &mat, int x) {
     int n = mat.size(), m = mat[0].size();
     int i = 0, j = m-1;
     while(i < n && j >= 0) {
         if(mat[i][j] == x) {
             cout << "Element " << x << " is found at position (" << i << ", " << j << ")" << endl;
+            findInRow(mat, x, i, j-1);
+            findInColumn(mat, x, i+1, j);
             i++; j--;
         } else if(x > mat[i][j]) {
             i++;
@@ -12,4 +27,14 @@ void matSearch (vector <vector <int>> &mat, int x) {
             j--;
         }
     }
+}
+
+void solve() {
+    matrix a = {
+        { -4, -3, -1, 3, 4 },
+        { -3, -2, 2, 3, 5 },
+        { -1, 1, 3, 5, 8 },
+        { 3, 4, 7, 8, 9 }
+    };
+    matSearch(a, 3);
 }
