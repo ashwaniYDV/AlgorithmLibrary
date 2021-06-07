5043c892994b3a7377f32b75bb8bfd2066075524
Ashwani Yadav <ashyadavash@gmail.com>
2021-06-07 19:20:49 +0530

    Update 01 Maximum Performance of a Team [leetcode].cpp

--- Heaps/01 Maximum Performance of a Team [leetcode].cpp
+++ Heaps/01 Maximum Performance of a Team [leetcode].cpp
@@ -1,5 +1,14 @@
 // https://leetcode.com/problems/maximum-performance-of-a-team/
 
+/*
+You are given two integers n and k and two integer arrays speed and efficiency both of length n. 
+There are n engineers numbered from 1 to n. speed[i] and efficiency[i] represent the speed and efficiency of the ith engineer respectively.
+Choose at most k different engineers out of the n engineers to form a team with the maximum performance.
+
+The performance of a team is the sum of their engineers' speeds multiplied by the minimum efficiency among their engineers.
+Return the maximum performance of this team. Since the answer can be a huge number, return it modulo 109 + 7.
+*/
+
 class Solution {
 public:
     #define ll long long
