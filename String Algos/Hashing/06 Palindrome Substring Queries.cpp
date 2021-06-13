0a48b58372d7d61a7ecd6356dfce9b194692c01d
Ashwani Yadav <ashyadavash@gmail.com>
2021-06-13 19:02:16 +0530

    Update and rename Palindrome Substring Queries.cpp to 06 Palindrome Substring Queries.cpp

--- String Algos/Hashing/06 Palindrome Substring Queries.cpp
+++ String Algos/Hashing/06 Palindrome Substring Queries.cpp
@@ -1,4 +1,18 @@
- // Using Cumulative Hash
+// https://www.geeksforgeeks.org/palindrome-substring-queries/ 
+// Using Cumulative Hash
+
+/*
+Given a string and several queries on the substrings of the given input string to check whether the substring is a palindrome or not.
+
+Suppose our input string is “abaaabaaaba” and the queries- [0, 10], [5, 8], [2, 5], [5, 9]
+
+We have to tell that the substring having the starting and ending indices as above is a palindrome or not.
+
+[0, 10] → Substring is “abaaabaaaba” which is a palindrome.
+[5, 8] → Substring is “baaa” which is not a palindrome.
+[2, 5] → Substring is “aaab” which is not a palindrome.
+[5, 9] → Substring is “baaab” which is a palindrome.
+*/
  
 #include<bits/stdc++.h>
 using namespace std;
