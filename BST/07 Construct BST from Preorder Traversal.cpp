1e24017c72180e4002a014423d6ded42d323c1b6
Ashwani Yadav <ashyadavash@gmail.com>
2021-06-17 23:20:27 +0530

    Update 07 Construct BST from Preorder Traversal.cpp

--- BST/07 Construct BST from Preorder Traversal.cpp
+++ BST/07 Construct BST from Preorder Traversal.cpp
@@ -1,4 +1,3 @@
-
 // O(n^2)
 class Solution {
 public:
@@ -24,6 +23,14 @@ public:
 
 
 
+
+
+
+/*
+pos variable is taken as an address since we want to instantly update pos for any function called in the stack 
+so that the array element is used only once in process of building the BST
+*/
+
 // O(n)
 class Solution {
 public:
@@ -45,5 +52,33 @@ public:
         if(n == 0) return NULL;
         int pos = 0;
         return build(preorder, pos, INT_MIN, INT_MAX);
+    }
+};
+
+
+
+
+
+// O(n)
+class Solution {
+public:
+    int n, pos = 0;
+    TreeNode* build(vector<int>&preorder, int min, int max){
+        if (pos >= n) return NULL;
+        int val = preorder[pos];
+        if (val > max || val <min) return NULL;
+        
+        TreeNode* node = new TreeNode(val);
+        pos++;
+        node->left = build(preorder, min, val);
+        node->right = build(preorder, val, max);
+        
+        return node;
+    }
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        this->n = preorder.size();
+        if(n == 0) return NULL;
+        pos = 0;
+        return build(preorder, INT_MIN, INT_MAX);
     }
 };
