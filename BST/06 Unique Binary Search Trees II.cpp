a54693455bece318e9423ddcb46f29f06e8a2ae0
Ashwani Yadav <ashyadavash@gmail.com>
2021-06-17 22:33:57 +0530

    Update and rename Unique Binary Search Trees II.cpp to 06 Unique Binary Search Trees II.cpp

--- BST/06 Unique Binary Search Trees II.cpp
+++ BST/06 Unique Binary Search Trees II.cpp
@@ -1,18 +1,12 @@
 // https://leetcode.com/problems/unique-binary-search-trees-ii/
 // https://youtu.be/hQn61BjdA7M
 
+/*
+Given an integer n, return all the structurally unique BST's (binary search trees), which has exactly n nodes of unique values from 1 to n. 
+Return the answer in any order.
+*/
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 public:
     vector<TreeNode*> helper(int start, int end) {
