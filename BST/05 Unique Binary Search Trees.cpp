15330bc7d2252a97c28f0185ba55ce081ec77279
Ashwani Yadav <ashyadavash@gmail.com>
2021-06-17 22:33:00 +0530

    Update 05 Unique Binary Search Trees.cpp

--- BST/05 Unique Binary Search Trees.cpp
+++ BST/05 Unique Binary Search Trees.cpp
@@ -1,5 +1,9 @@
 // https://leetcode.com/problems/unique-binary-search-trees/
 
+/*
+Given an integer n, return the number of structurally unique BST's (binary search trees) which has exactly n nodes of unique values from 1 to n.
+*/
+
 // MEthod 1: Using Catalan numbers
 class Solution {
     int catalan_Number(int n)
