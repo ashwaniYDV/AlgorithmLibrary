27d71edc85372de339e5eb7587eac21b81c3b57b
Ashwani Yadav <ashyadavash@gmail.com>
2021-06-19 01:41:35 +0530

    Update 00 KMP.cpp

--- String Algos/KMP/00 KMP.cpp
+++ String Algos/KMP/00 KMP.cpp
@@ -1,44 +1,46 @@
-#include<bits/stdc++.h>
-using namespace std;
-#pragma GCC optimize ("O3")
-#pragma GCC target ("avx")
-#pragma GCC optimize("Ofast")
-#pragma GCC optimize ("unroll-loops")
-#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
- 
-typedef long long int ll;
-// #define int long long int
-#define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-#define FRE freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
-#define debug(x) cout << #x << " is: " << (x) << endl;
-#define debugx(x) cout << #x << " is: " << (x) << endl; exit(0);
-#define f(i,n) for(ll i=0;i<n;i++)
-#define fa(i,a,n) for(ll i=a;i<n;i++)
-#define loop(i,a, n) for(ll i = a; i <= n; i++)
-#define loopb(i,a, n) for(ll i = a; i >= n; i--)
-#define pb push_back
-#define pf push_front
-#define F first
-#define S second
-#define all(x) x.begin(), x.end()
-#define setmem(x, k) memset(x, k, sizeof(x))
-#define clr(x) memset(x, 0, sizeof(x))
-#define sortall(x) sort(all(x))
-#define PI 3.1415926535897932384626
-#define MOD 1000000007
-#define INF 1000111000111000111LL
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-typedef vector<int> vi;
-typedef vector<ll> vl;
-typedef long double ld;
-typedef vector<vector<ll>> matrix;
-ll dx[] = {0, 1, 0, -1};
-ll dy[] = {-1, 0, 1, 0};
-const ll N = 1e5+2;
-// ll a[N], b[N], dp[N], level[N], vis[N], in[N], out[N];
-// vl g[N];
-ll n, m;
+/*
+KMP algorithm depends on prefix function (PI array) for its implementation
+
+You are given a string s of length n. 
+The prefix function for this string is defined as an array π of length n.
+π[i] = length of the longest proper prefix of the substring s[0…i] which is also a suffix of this substring. 
+
+NOTE: A proper prefix of a string is a prefix that is not equal to the string itself. By definition, π[0] = 0.
+
+Exampple:
+String         : a b c a b c d
+Prefix function: 0 0 0 1 2 3 0
+
+Exampple:
+String         : a a b a a a b
+Prefix function: 0 1 0 1 2 2 3
+*/
+
+
+
+
+/*
+Trivial Algorithm:
+
+Mathematically the definition of the prefix function can be written as follows:
+π[i] = max k (k from 0...i {s[0...k-1] == s[i-(k-1)...i]})
+*/
+// O(n^3)
+vector<int> prefix_function(string s) {
+    int n = (int)s.length();
+    vector<int> pi(n);
+    for (int i = 0; i < n; i++) {
+        for (int k = 0; k <= i; k++) {
+            if (s.substr(0, k) == s.substr(i-k+1, k)) {
+                pi[i] = k;
+            }
+        }
+    }
+    return pi;
+}
+
+
+
 
 // generates PI array (or LPS [largest prefix suffix])
 vector<int> prefix_function(string &s) {
