15336cf0b80be029c927095a44697431ad8cceec
Ashwani Yadav <ashyadavash@gmail.com>
2021-06-19 01:58:43 +0530

    Update and rename 00 KMP.cpp to 01 KMP (Pattern matching).cpp

--- String Algos/KMP/01 KMP (Pattern matching).cpp
+++ String Algos/KMP/01 KMP (Pattern matching).cpp
@@ -1,45 +1,4 @@
-/*
-KMP algorithm depends on prefix function (PI array) for its implementation
-
-You are given a string s of length n. 
-The prefix function for this string is defined as an array π of length n.
-π[i] = length of the longest proper prefix of the substring s[0…i] which is also a suffix of this substring. 
-
-NOTE: A proper prefix of a string is a prefix that is not equal to the string itself. By definition, π[0] = 0.
-
-Exampple:
-String         : a b c a b c d
-Prefix function: 0 0 0 1 2 3 0
-
-Exampple:
-String         : a a b a a a b
-Prefix function: 0 1 0 1 2 2 3
-*/
-
-
-
-
-/*
-Trivial Algorithm:
-
-Mathematically the definition of the prefix function can be written as follows:
-π[i] = max k (k from 0...i {s[0...k-1] == s[i-(k-1)...i]})
-*/
-// O(n^3)
-vector<int> prefix_function(string s) {
-    int n = (int)s.length();
-    vector<int> pi(n);
-    for (int i = 0; i < n; i++) {
-        for (int k = 0; k <= i; k++) {
-            if (s.substr(0, k) == s.substr(i-k+1, k)) {
-                pi[i] = k;
-            }
-        }
-    }
-    return pi;
-}
-
-
+// https://cp-algorithms.com/string/prefix-function.html
 
 
 // generates PI array (or LPS [largest prefix suffix])
