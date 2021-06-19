34f40283000182df199b05087f97cacc135867e1
Ashwani Yadav <ashyadavash@gmail.com>
2021-06-19 18:27:21 +0530

    Update 00 Z Function.cpp

--- String Algos/Z Algorithm/00 Z Function.cpp
+++ String Algos/Z Algorithm/00 Z Function.cpp
@@ -4,24 +4,52 @@
 In Z algo z[0] doesn't matter. It can be anything
 */
 
-// method 2
-vector<ll> z_function(string s){
-	ll n=s.size();
-	vector<ll> z(n);
-	for(ll i=1,l=0,r=0;i<n;i++){
-		if(i<=r){
-			z[i]=min(r-i+1, z[i-l]);
-		}
-		while(i+z[i]<n && s[z[i]]==s[i+z[i]]) {
-			z[i]++;
-		}
-		if(i+z[i]-1>r){
-			l=i, r=i+z[i]-1;
-		}
-	}
-	return z;
+/*
+cp-algorithms:
+
+Trivial algorithm (Naive approach)
+Formal definition can be represented in the following elementary O(n^2) implementation.
+*/
+vector<int> z_function_trivial(string s) {
+    int n = (int) s.length();
+    vector<int> z(n);
+    for (int i = 1; i < n; ++i)
+        while (i + z[i] < n && s[z[i]] == s[i + z[i]])
+            ++z[i];
+    return z;
 }
 
+
+/*
+cp-algorithms:
+
+Efficient algorithm to compute the Z-function O(n)
+*/
+vector<int> z_function(string s) {
+    int n = (int) s.length();
+    vector<int> z(n);
+    for (int i = 1, l = 0, r = 0; i < n; ++i) {
+        if (i <= r)
+            z[i] = min (r - i + 1, z[i - l]);
+        while (i + z[i] < n && s[z[i]] == s[i + z[i]])
+            ++z[i];
+        if (i + z[i] - 1 > r)
+            l = i, r = i + z[i] - 1;
+    }
+    return z;
+}
+
+
+
+
+
+
+
+
+
+
+
+// method 2
 auto z_function(string s) {
 	ll n = s.size();
 	ll l = 0, r = 0;
