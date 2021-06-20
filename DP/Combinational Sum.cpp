5ddeb7775e2c45f91860511be1a48ae3b655a2b4
Ashwani Yadav <ashyadavash@gmail.com>
2021-06-20 16:12:43 +0530

    Update Combinational Sum.cpp

--- DP/Combinational Sum.cpp
+++ DP/Combinational Sum.cpp
@@ -1,48 +1,19 @@
-#include <iostream>
-#include<bits/stdc++.h>
-#include<vector>
-using namespace std;
- 
-#define ll long long int
-#define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-#define FRE freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
-#define f(i,n) for(ll i=0;i<n;i++)
-#define fa(i,a,n) for(ll i=a;a<n?i<n:i>n;a<n?i+=1:i-=1)
-#define pb push_back
-#define pf push_front
-#define F first
-#define S second
-#define all(x) x.begin(), x.end()
-#define setmem(x, k) memset(x, k, sizeof(x))
-#define clr(x) memset(x, 0, sizeof(x))
-#define sortall(x) sort(all(x))
-#define PI 3.1415926535897932384626
-#define MOD 1000000007
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-typedef vector<int> vi;
-typedef vector<ll> vl;
-typedef vector<pii> vpii;
-typedef vector<pll> vpll;
-typedef vector<vi> vvi;
-typedef vector<vl> vvl;
-typedef long double ld;
-// const ll N = 1e6+5, M = N, ninf = -2e5, inf = (int)1e9;
-// vl g[N], dist;
-// ll a[N], b[N], dp[N], in[N], out[N], vis[N], level[N];
-ll n, m;
+/*
+Given an array of positive integers arr[] and a sum x, find all unique combinations in arr[] where the sum is equal to x. 
+The same repeated number may be chosen from arr[] unlimited number of times. 
+Elements in a combination (a1, a2, …, ak) must be printed in non-descending order. (ie, a1 <= a2 <= … <= ak). 
+The combinations themselves must be sorted in ascending order, i.e., the combination with smallest first element should be printed first. 
+*/
 
 void combinationSum(vector<ll>& ar, ll sum, vector<vector<ll> >& res, vector<ll>& curr, ll index) { 
-    // If  current sum becomes negative 
     if (sum < 0) return;
-    // if we get exact answer 
     if (sum == 0) { 
         res.push_back(curr); 
         return;
     } 
   
     // Recur for all remaining elements that have value smaller than sum. 
-    // for(ll i=index; i<ar.size() && sum-ar[i]>=0; i++){
+    // for(ll i=index; i<ar.size() && sum-ar[i]>=0; i++) {
     for(ll i=index; i<ar.size(); i++){
         // Till every element in the array starting from i which can contribute to the sum add them to list
         curr.push_back(ar[i]); 
@@ -53,37 +24,25 @@ void combinationSum(vector<ll>& ar, ll sum, vector<vector<ll> >& res, vector<ll>
     } 
 } 
 
-int32_t main()
-{
-    IOS
-    ll t,k,x,y,z,p,q,u,v,ct=0,flag=0;
-
-    vector<ll> ar; 
-    ar.push_back(2); 
-    ar.push_back(4); 
-    ar.push_back(8); 
-    ar.push_back(6); 
-    ar.push_back(8); 
-    ll n = ar.size();
-  
-    // set value of sum 
-    ll sum = 8;
+void solve() {
+    vector<int> ar = {2, 4, 8, 6, 8}; 
+    int sum = 8;
+ 
+    n = ar.size();
 
     // sort input array 
     sort(ar.begin(), ar.end()); 
     // remove duplicates 
     ar.erase(unique(ar.begin(), ar.end()), ar.end()); 
 
-    vector<ll> curr; 
-    vector<vector<ll> > res; 
+    vector<int> curr; 
+    vector<vector<int> > res; 
 
     combinationSum(ar, sum, res, curr, 0); 
 
-    for (ll i = 0; i < res.size(); i++) { 
-        if (res[i].size() > 0) { 
-            for (ll j = 0; j < res[i].size(); j++) 
-                cout << res[i][j] << " "; 
-            cout << "\n"; 
-        } 
+    for (int i = 0; i < res.size(); i++) { 
+        for (int j = 0; j < res[i].size(); j++) 
+            cout << res[i][j] << " "; 
+        cout << "\n"; 
     } 
 }
