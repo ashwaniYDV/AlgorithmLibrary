1fb86a548675d039985c141afb68b75f09322e9d
Ashwani Yadav <ashyadavash@gmail.com>
2021-06-23 01:13:25 +0530

    Update 00 Dijkstra.cpp

--- Graph/Shortest Paths/00 Dijkstra.cpp
+++ Graph/Shortest Paths/00 Dijkstra.cpp
@@ -1,39 +1,6 @@
 // Single source shortest path for weighted graphs
 // It fails for negative weights (in that case we can use bellman ford)
 
-#include <iostream>
-#include<bits/stdc++.h>
-#include<vector>
-using namespace std;
- 
-#define ll long long int
-#define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-#define FRE freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
-#define f(i,n) for(ll i=0;i<n;i++)
-#define fa(i,a,n) for(ll i=a;a<n?i<n:i>n;a<n?i+=1:i-=1)
-#define pb push_back
-#define F first
-#define S second
-#define all(x) x.begin(), x.end()
-#define clr(x) memset(x, 0, sizeof(x))
-#define sortall(x) sort(all(x))
-#define PI 3.1415926535897932384626
-#define MOD 1000000007
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-typedef vector<int>	vi;
-typedef vector<ll> vl;
-typedef vector<pii> vpii;
-typedef vector<pll> vpll;
-typedef vector<vi> vvi;
-typedef vector<vl> vvl;
-typedef long double ld;
-
-const ll mod = 1000000007;
-const ll N = 1e6+5, M = N, ninf = -2e5, inf = (int)1e9;
-vl g[N], dist;
-ll a[N], b[N], dp[N], in[N], out[N], vis[N], level[N];
-ll n, m;
 
 vector<pll> adj[N];
 void dijkstra(ll source){
@@ -70,4 +37,86 @@ int32_t main()
 	for(ll i=1; i<=n; i++){
 		cout<<"Distance of "<<i<<" from "<<x<<" is "<<dp[i]<<endl;
 	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const int N = 1e5+5;
+int n, m;
+int dis[N], parent[N];
+vector<pair<int, int> > g[N];
+ 
+void dijkstra(int source, int destination) {
+	for(int i = 1; i <= n; i++) {
+		dis[i] = 1e18;
+		parent[i] = -1;
+	}
+
+	dis[source] = 0;
+	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+	pq.push({0, source});
+
+	while(!pq.empty()) {
+		auto x = pq.top();
+		pq.pop();
+		int u = x.second;
+
+		for(auto it: g[u]) {
+			int v = it.first;
+			int w = it.second;
+			if(dis[v] > dis[u] + w) {
+				dis[v] = dis[u] + w;
+				pq.push({dis[v], v});
+				parent[v] = u;
+			}
+		}
+	}
+
+	if(dis[destination] == 1e18) {
+		cout << "-1";
+		return;
+	}
+
+	int node = destination;
+	vector<int> ans;
+	while(parent[node] != -1) {
+		ans.push_back(node);
+		node = parent[node];
+	}
+	ans.push_back(source);
+	reverse(ans.begin(), ans.end());
+	for(auto i: ans) {
+		cout << i << " ";
+	}
+}
+ 
+void solve() {
+	int u, v, w;
+	cin >> n >> m;
+	for(int i = 1; i <= m; i++) {
+		cin >> u>> v >> w;
+		g[u].push_back({v, w});
+		g[v].push_back({u, w});
+	}
+	dijkstra(1, n);
 }
