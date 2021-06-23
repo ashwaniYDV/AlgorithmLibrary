3f8dd9a3fedef4b5b5c5ad561d40ff938095e9cb
Ashwani Yadav <ashyadavash@gmail.com>
2021-06-24 02:47:47 +0530

    Update Rod Cutting (Unbounded Knapsack concept).cpp

--- DP/Rod Cutting (Unbounded Knapsack concept).cpp
+++ DP/Rod Cutting (Unbounded Knapsack concept).cpp
@@ -14,27 +14,90 @@ ________________________________________________________________________________
 */
 
 
-void solve() {
-    vi price = { 1, 5, 8, 9, 10, 17, 17, 20 };
-
-    n = price.size();
-    vi length(n);
-    f(i, n) length[i] = i+1;
-
-    int mxLength = n;
-
-    int dp[n + 1][mxLength + 1];
-
-    for(int i = 0; i <= n; i++) {
-        for(int j = 0; j <= mxLength; j++) {
-            if(i == 0 || j == 0) {
-                dp[i][j] = 0;
-            } else if(j < length[i - 1]) {
-                dp[i][j] = dp[i - 1][j];
-            } else {
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - length[i - 1]] + price[i - 1]);
+// Method 1: Unbounded Knapsack concept
+class Solution{
+  public:
+    int cutRod(int price[], int n) {
+        vector<int> length(n);
+        for(int i = 0; i < n; i++) length[i] = i + 1;
+    
+        int mxLength = n;
+    
+        int dp[n + 1][mxLength + 1];
+    
+        for(int i = 0; i <= n; i++) {
+            for(int j = 0; j <= mxLength; j++) {
+                if(i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                } else if(j < length[i - 1]) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - length[i - 1]] + price[i - 1]);
+                }
             }
         }
+        return dp[n][mxLength];
     }
-    cout << dp[n][mxLength] << endl;
-}
+};
+
+
+
+
+
+
+
+// Method 2: O(m) memory
+class Solution{
+  public:
+    int cutRod(int price[], int n) {
+        // dp[i] denotes the max value of rod length i, so answer = dp[n]
+        vector<int> dp(n+1, 0);
+        
+        dp[0] = 0; // when no element is included
+        dp[1] = price[0]; // when only first element is included
+        
+        for(int i = 2; i <= n; i++) {
+            for(int j = 0; j < i; j++) {
+                dp[i] = max(dp[i], dp[j] + price[i - j - 1]);
+            }
+        }
+        
+        return dp[n];
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Method 3
+class Solution{
+  public:
+    int go(int pos, int n, int price[], vector<int>& dp) {
+    	if(pos == n) return 0;
+    	if(pos > n) return INT_MIN;
+    	
+    	if(dp[pos] != -1) return dp[pos];
+    
+    	int res = 0;
+    	for(int i = pos; i < n; i++) {
+    		res = max(res, price[i - pos] + go(i + 1, n, price, dp));
+    	}
+    
+    	return dp[pos] = res;
+    }
+    int cutRod(int price[], int n) {
+        vector<int> dp(10000, -1);
+        return go(0, n, price, dp);
+    }
+};
