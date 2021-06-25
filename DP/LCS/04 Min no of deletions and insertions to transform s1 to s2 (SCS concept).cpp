ab732bf550c786843a7d504097fc56b5846051e1
Ashwani Yadav <ashyadavash@gmail.com>
2021-06-25 12:53:14 +0530

    Update 04 Min no of deletions and insertions to transform s1 to s2 (SCS concept).cpp

--- DP/LCS/04 Min no of deletions and insertions to transform s1 to s2 (SCS concept).cpp
+++ DP/LCS/04 Min no of deletions and insertions to transform s1 to s2 (SCS concept).cpp
@@ -1,48 +1,84 @@
 // https://practice.geeksforgeeks.org/problems/minimum-number-of-deletions-and-insertions0209/1
 // https://www.geeksforgeeks.org/minimum-number-deletions-insertions-transform-one-string-another/
 
-class Solution{
-public:
-    string lcs(string s1, string s2) {
-        int n1 = s1.size(), n2=s2.size();
-        int dp[n1 + 1][n2 + 1];
-        for(int i = 0; i <= n1; i++) {
-            for(int j = 0; j <= n2; j++) {
-                if(i == 0 || j == 0) {
-                    dp[i][j] = 0;
-                } else if(s1[i - 1] == s2[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                }else{
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+
+// Method 1
+class Solution {
+    public:
+        int lcs(string s1, string s2) {
+            int n1 = s1.size(), n2 = s2.size();
+            int dp[n1 + 1][n2 + 1];
+            for (int i = 0; i <= n1; i++) {
+                for (int j = 0; j <= n2; j++) {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 0;
+                    } else if (s1[i - 1] == s2[j - 1]) {
+                        dp[i][j] = dp[i - 1][j - 1] + 1;
+                    } else {
+                        dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                    }
                 }
             }
+            return dp[n1][n2];
         }
 
-        int maxLength = dp[n1][n2];
-        string ans = "";
-        int i = n1, j = n2;
-        while(i > 0 && j > 0) {
-            if(s1[i - 1] == s2[j - 1]) {
-                ans = s1[i-1] + ans;
-                i--, j--;
-            } else if(dp[i - 1][j] > dp[i][j - 1]) {
-                i--;
-            } else {
-                j--;
-            }
-        }
-        return ans;
+    int minOperations(string s1, string s2) {
+        int n1 = s1.size(), n2 = s2.size();
+
+        int LCS = lcs(s1, s2);
+        int deletion = n1 - LCS, insertion = n2 - LCS;
+
+        return insertion + deletion;
     }
-    
-	int minOperations(string s1, string s2)  { 
-	    int n1 = s1.size(), n2 = s2.size();
-        if(n1 == 0) return n2;
-        if(n2 == 0) return n1;
-    
+};
+
+
+
+
+
+// Method 2
+class Solution {
+    public:
+        string lcs(string s1, string s2) {
+            int n1 = s1.size(), n2 = s2.size();
+            int dp[n1 + 1][n2 + 1];
+            for (int i = 0; i <= n1; i++) {
+                for (int j = 0; j <= n2; j++) {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 0;
+                    } else if (s1[i - 1] == s2[j - 1]) {
+                        dp[i][j] = dp[i - 1][j - 1] + 1;
+                    } else {
+                        dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                    }
+                }
+            }
+
+            int maxLength = dp[n1][n2];
+            string ans = "";
+            int i = n1, j = n2;
+            while (i > 0 && j > 0) {
+                if (s1[i - 1] == s2[j - 1]) {
+                    ans = s1[i - 1] + ans;
+                    i--, j--;
+                } else if (dp[i - 1][j] > dp[i][j - 1]) {
+                    i--;
+                } else {
+                    j--;
+                }
+            }
+            return ans;
+        }
+
+    int minOperations(string s1, string s2) {
+        int n1 = s1.size(), n2 = s2.size();
+        if (n1 == 0) return n2;
+        if (n2 == 0) return n1;
+
         int insertion = 0, deletion = 0;
-        
+
         int i = 0, j = 0;
-        for(char c: lcs(s1, s2)) {
+        for (char c: lcs(s1, s2)) {
             while (s1[i] != c) {
                 i++;
                 deletion++;
@@ -53,11 +89,11 @@ public:
             }
             i++, j++;
         }
-        
+
         deletion += (n1 - i);
         insertion += (n2 - j);
-        
+
         return insertion + deletion;
-	    
-	} 
+
+    }
 };
