6d0657adeb8e9a75440b0a2aafc3a5a989f62d76
Ashwani Yadav <ashyadavash@gmail.com>
2021-06-26 13:09:19 +0530

    Update 02 Delete Operation for Two Strings [leetcode].cpp

--- DP/LCS/02 Delete Operation for Two Strings [leetcode].cpp
+++ DP/LCS/02 Delete Operation for Two Strings [leetcode].cpp
@@ -7,8 +7,8 @@ In one step, you can delete exactly one character in either string.
 
 class Solution {
 public:
-    int minDistance(string s1, string s2) {
-        int n1 = s1.size(), n2=s2.size();
+    int lcs(string s1, string s2) {
+        int n1 = s1.size(), n2 = s2.size();
         int dp[n1 + 1][n2 + 1];
         for(int i = 0; i <= n1; i++) {
             for(int j = 0; j <= n2; j++) {
@@ -16,12 +16,17 @@ public:
                     dp[i][j] = 0;
                 } else if(s1[i - 1] == s2[j - 1]) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
-                }else{
+                } else {
                     dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
-
-        return n1 + n2 - 2*dp[n1][n2];
+        return dp[n1][n2];
+    }
+    
+    int minDistance(string s1, string s2) {
+        int n1 = s1.size(), n2 = s2.size();
+        int LCS = lcs(s1, s2);
+        return n1 + n2 - 2 * LCS;
     }
 };
