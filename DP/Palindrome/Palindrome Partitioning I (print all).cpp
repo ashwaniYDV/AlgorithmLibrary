afa367656c70d7c66c8e0e9cb489e2454031284a
Ashwani Yadav <ashyadavash@gmail.com>
2021-06-27 02:44:21 +0530

    Update Palindrome Partitioning I (print all).cpp

--- DP/Palindrome/Palindrome Partitioning I (print all).cpp
+++ DP/Palindrome/Palindrome Partitioning I (print all).cpp
@@ -1,5 +1,10 @@
 // https://leetcode.com/problems/palindrome-partitioning/
 
+/*
+Given a string s, partition s such that every substring of the partition is a palindrome. 
+Return all possible palindrome partitioning of s.
+*/
+
 class Solution {
 public:
     int n;
