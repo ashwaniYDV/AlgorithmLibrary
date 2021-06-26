df8d0778f91adc18dc36f828558605b5f06b89b3
Ashwani Yadav <ashyadavash@gmail.com>
2021-06-26 13:11:18 +0530

    Update 07 Longest Repeating Subsequence.cpp

--- DP/LCS/07 Longest Repeating Subsequence.cpp
+++ DP/LCS/07 Longest Repeating Subsequence.cpp
@@ -21,25 +21,25 @@ they shouldn’t be on the same index in the two strings.
 
 
 class Solution {
-	public:
-		int LongestRepeatingSubsequence(string s) {
-		    int n = s.size();
+    public:
+        int LongestRepeatingSubsequence(string s) {
+            int n = s.size();
             int dp[n + 1][n + 1];
-            
-            for(int i = 0; i <= n; i++) {
-                for(int j = 0; j <= n; j++) {
-                    if(i == 0 || j == 0) {
+
+            for (int i = 0; i <= n; i++) {
+                for (int j = 0; j <= n; j++) {
+                    if (i == 0 || j == 0) {
                         dp[i][j] = 0;
-                    } else if(s[i - 1] == s[j - 1] && i != j) {
+                    } else if (s[i - 1] == s[j - 1] && i != j) {
                         dp[i][j] = dp[i - 1][j - 1] + 1;
                     } else {
                         dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
                     }
                 }
             }
-    
+
             return dp[n][n];
-		}
+        }
 };
 
 
@@ -49,24 +49,24 @@ class Solution {
 
 
 class Solution {
-	public:
-		int LongestRepeatingSubsequence(string s) {
-		    string s1 = s, s2 = s;
-		    int n1 = s1.size(), n2 = s2.size();
-		    
+    public:
+        int LongestRepeatingSubsequence(string s) {
+            string s1 = s, s2 = s;
+            int n1 = s1.size(), n2 = s2.size();
+
             int dp[n1 + 1][n2 + 1];
-            for(int i = 0; i <= n1; i++) {
-                for(int j = 0; j <= n2; j++) {
-                    if(i == 0 || j == 0) {
+            for (int i = 0; i <= n1; i++) {
+                for (int j = 0; j <= n2; j++) {
+                    if (i == 0 || j == 0) {
                         dp[i][j] = 0;
-                    } else if(s1[i - 1] == s2[j - 1] && i != j) {
+                    } else if (s1[i - 1] == s2[j - 1] && i != j) {
                         dp[i][j] = dp[i - 1][j - 1] + 1;
                     } else {
                         dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
                     }
                 }
             }
-    
+
             return dp[n1][n2];
-		}
+        }
 };
