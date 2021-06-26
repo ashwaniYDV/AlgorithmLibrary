e9c2c630342976cd0f7c1f7cc65b2c7b420c9734
Ashwani Yadav <ashyadavash@gmail.com>
2021-06-27 02:28:28 +0530

    Update 04 Min no of deletions to make a string palindrome.cpp

--- DP/Palindrome/04 Min no of deletions to make a string palindrome.cpp
+++ DP/Palindrome/04 Min no of deletions to make a string palindrome.cpp
@@ -4,31 +4,34 @@
 Answer = s.size() - LongestPalindromicSubsequenceLength 
 */
 
-class Solution{
-  public:
-    int minimumNumberOfDeletions(string s) { 
+class Solution {
+public:
+    int longestPalindromeSubseq(string s) {
         int n = s.size();
         int dp[n][n];
 
-        for(int gap = 0; gap < n; gap++) {
-            for(int i=0, j=gap;  j<n;  i++, j++) {
+        for (int gap = 0; gap < n; gap++) {
+            for (int i = 0, j = gap; j < n; i++, j++) {
                 dp[i][j] = 0;
-                if(gap == 0) {
+                if (gap == 0) {
                     dp[i][j] = 1;
-                } else if(gap == 1) {
-                    if(s[i] == s[j]) dp[i][j] = 2;
+                } else if (gap == 1) {
+                    if (s[i] == s[j]) dp[i][j] = 2;
                     else dp[i][j] = 1;
                 } else {
-                    if(s[i] == s[j]) {
-                        dp[i][j] = 2 + dp[i+1][j-1];
+                    if (s[i] == s[j]) {
+                        dp[i][j] = 2 + dp[i + 1][j - 1];
                     } else {
-                        dp[i][j] = max(dp[i][j-1], dp[i+1][j]);
+                        dp[i][j] = max(dp[i][j - 1], dp[i + 1][j]);
                     }
                 }
             }
         }
+        return dp[0][n - 1];
+    }
+    int minimumNumberOfDeletions(string s) {
+        int n = s.size();
+        return n - longestPalindromeSubseq(s);
 
-        return n - dp[0][n-1];
-        
-    } 
+    }
 };
