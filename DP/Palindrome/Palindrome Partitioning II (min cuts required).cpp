84cf9b0b3762f6311d6a3b45ca01a45d26943820
Ashwani Yadav <ashyadavash@gmail.com>
2021-06-27 19:13:51 +0530

    Update Palindrome Partitioning II (min cuts required).cpp

--- DP/Palindrome/Palindrome Partitioning II (min cuts required).cpp
+++ DP/Palindrome/Palindrome Partitioning II (min cuts required).cpp
@@ -2,9 +2,11 @@
 // https://youtu.be/qmTtAbOTqcg
 
 /*
+Problem:
 Given a string s, partition s such that every substring of the partition is a palindrome.
 Return the minimum cuts needed for a palindrome partitioning of s.
 */
+
 
 // Method 1 (O(n^3))  [can be made O(n^2) by precomputing isPalindrome using gap strategy as shown in the next method]
 class Solution {
@@ -55,7 +57,90 @@ public:
 
 
 
-// Method 2 (O(n^2))
+
+
+
+
+
+
+
+
+// Method 2 (Method 1 optimized = O(n^2))
+class Solution {
+public:
+    vector<vector<bool>> isPalindrome;
+    int n;
+    string s;
+    
+    void precomputeIsPalindrome(string s) {
+        int n = s.size();
+        isPalindrome.resize(n, vector<bool>(n));
+        for(int gap = 0; gap < n; gap++) {
+            for(int i=0, j=gap;  j<n;  i++, j++) {
+                isPalindrome[i][j] = false;
+                if(gap == 0) {
+                    isPalindrome[i][j] = true;
+                } else if(gap == 1) {
+                    if(s[i] == s[j]) isPalindrome[i][j] = true;
+                    else isPalindrome[i][j] = false;
+                } else {
+                    if(s[i] == s[j] && isPalindrome[i + 1][j - 1]) {
+                        isPalindrome[i][j] = true;
+                    } else {
+                        isPalindrome[i][j] = false;
+                    }
+                }
+            }
+        }
+    }
+    
+    int fun(int pos, vector<int>& dp) {
+        if(pos == n) {
+            return 0;
+        }
+        
+        if(dp[pos] != -1) return dp[pos];
+        
+        int res = 1e9;
+        for(int i = pos; i < n; ++i) {
+            if(isPalindrome[pos][i]) {
+                res = min(res, 1 + fun(i+1, dp));
+            }
+        }
+        
+        return dp[pos] = res;
+    }
+    int minCut(string s) {
+        n = s.size();
+        this->s = s;
+        precomputeIsPalindrome(s);
+        
+        vector<int> dp(n+1, -1);
+        // no of cuts = no of palin strings - 1
+        return fun(0, dp) - 1;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Method 3 (O(n^2))
 class Solution {
 public:
     vector<vector<bool>> isPalindrome;
