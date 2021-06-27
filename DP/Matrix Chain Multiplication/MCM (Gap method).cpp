60b24dcf9d6c27ea83c7f1a37cb47d13838a8c28
Ashwani Yadav <ashyadavash@gmail.com>
2021-06-27 15:37:35 +0530

    Update MCM (Gap method).cpp

--- DP/Matrix Chain Multiplication/MCM (Gap method).cpp
+++ DP/Matrix Chain Multiplication/MCM (Gap method).cpp
@@ -24,27 +24,27 @@ public:
     int matrixMultiplication(int n, int a[]) {
         int m = n - 1;
         int dp[m][m];
-        
-        for(int gap = 0; gap < m; gap++) {
-            for(int i=0, j=gap;  j < m;  i++, j++) {
-                if(gap == 0) {
+
+        for (int gap = 0; gap < m; gap++) {
+            for (int i = 0, j = gap; j < m; i++, j++) {
+                if (gap == 0) {
                     dp[i][j] = 0;
-                } else if(gap == 1) {
-                    dp[i][j] = a[i] * a[j] * a[j+1];
+                } else if (gap == 1) {
+                    dp[i][j] = a[i] * a[j] * a[j + 1];
                 } else {
-                	dp[i][j] = INT_MAX;
-                    for(int k = i; k < j; k++) {
-                    	int leftCost = dp[i][k];
-                    	int rightCost = dp[k+1][j];
-                    	int multiplyCost = a[i] * a[k+1] * a[j+1];
-    
-                    	dp[i][j] = min(dp[i][j], leftCost + rightCost + multiplyCost);
+                    dp[i][j] = INT_MAX;
+                    for (int k = i; k < j; k++) {
+                        int leftCost = dp[i][k];
+                        int rightCost = dp[k + 1][j];
+                        int multiplyCost = a[i] * a[k + 1] * a[j + 1];
+
+                        dp[i][j] = min(dp[i][j], leftCost + rightCost + multiplyCost);
                     }
                 }
             }
         }
-    
-        return dp[0][m-1];
+
+        return dp[0][m - 1];
     }
 };
 
@@ -61,24 +61,24 @@ public:
     int matrixMultiplication(int n, int a[]) {
         int m = n - 1;
         int dp[m][m];
-        
-        for(int gap = 0; gap < m; gap++) {
-            for(int i=0, j=gap;  j < m;  i++, j++) {
-                if(gap == 0) {
+
+        for (int gap = 0; gap < m; gap++) {
+            for (int i = 0, j = gap; j < m; i++, j++) {
+                if (gap == 0) {
                     dp[i][j] = 0;
                 } else {
-                	dp[i][j] = INT_MAX;
-                    for(int k = i; k < j; k++) {
-                    	int leftCost = dp[i][k];
-                    	int rightCost = dp[k+1][j];
-                    	int multiplyCost = a[i] * a[k+1] * a[j+1];
-    
-                    	dp[i][j] = min(dp[i][j], leftCost + rightCost + multiplyCost);
+                    dp[i][j] = INT_MAX;
+                    for (int k = i; k < j; k++) {
+                        int leftCost = dp[i][k];
+                        int rightCost = dp[k + 1][j];
+                        int multiplyCost = a[i] * a[k + 1] * a[j + 1];
+
+                        dp[i][j] = min(dp[i][j], leftCost + rightCost + multiplyCost);
                     }
                 }
             }
         }
-    
-        return dp[0][m-1];
+
+        return dp[0][m - 1];
     }
 };
