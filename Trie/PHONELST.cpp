f82640ee671a2c99a4a878ba62ffdac794ca91f8
Ashwani Yadav <ashyadavash@gmail.com>
2021-07-05 22:04:40 +0530

    Update PHONELST.cpp

--- Trie/PHONELST.cpp
+++ Trie/PHONELST.cpp
@@ -1,5 +1,23 @@
 // https://www.spoj.com/problems/PHONELST/
 
+/*
+Given a list of phone numbers, determine if it is consistent in the sense that no number is the prefix of another. 
+Let’s say the phone catalogue listed these numbers:
+
+Emergency = 911
+Alice = 97625999
+Bob = 91125426
+In this case, it’s not possible to call Bob, because the central would direct your call to the emergency line 
+as soon as you had dialled the first three digits of Bob’s phone number. So this list would not be consistent.
+
+Input
+The first line of input gives a single integer, 1 <= t <= 40, the number of test cases. 
+Each test case starts with n, the number of phone numbers, on a separate line, 1 <= n <= 10000. 
+Then follows n lines with one unique phone number on each line. A phone number is a sequence of at most ten digits.
+
+Output
+For each test case, output “YES” if the list is consistent, or “NO” otherwise.
+*/
 
 int check = 1;
 
