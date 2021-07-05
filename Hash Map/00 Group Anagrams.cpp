db171ccfffe9a73874a449c4284a78a07df9641a
Ashwani Yadav <ashyadavash@gmail.com>
2021-07-06 00:10:43 +0530

    Update 00 Group Anagrams.cpp

--- Hash Map/00 Group Anagrams.cpp
+++ Hash Map/00 Group Anagrams.cpp
@@ -31,21 +31,9 @@ public:
 
 class Solution {
 public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> mp;
-        for (string s: strs) {
-            mp[strCountingSort(s)].push_back(s);
-        }
-        vector<vector<string>> anagrams;
-        for (auto it: mp) { 
-            anagrams.push_back(it.second);
-        }
-        return anagrams;
-    }
-private:
     string strCountingSort(string s) {
         int counter[26] = {0};
-        for (char c: s) {
+        for (char c : s) {
             counter[c - 'a']++;
         }
         string t;
@@ -53,6 +41,18 @@ private:
             t += string(counter[c], c + 'a');
         }
         return t;
+    }
+    
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+        for (string s : strs) {
+            mp[strCountingSort(s)].push_back(s);
+        }
+        vector<vector<string>> anagrams;
+        for (auto p : mp) { 
+            anagrams.push_back(p.second);
+        }
+        return anagrams;
     }
 };
 
