9b070e8b0e5205f660070301d46a4f38731d977a
Ashwani Yadav <ashyadavash@gmail.com>
2021-07-08 03:09:45 +0530

    Update 01 Nearest Greater to Right (NGR).cpp

--- Stack/01 Nearest Greater to Right (NGR).cpp
+++ Stack/01 Nearest Greater to Right (NGR).cpp
@@ -1,3 +1,60 @@
+// https://practice.geeksforgeeks.org/problems/next-larger-element-1587115620/1
+
+/*
+Given an array A[] of size N having distinct elements, the task is to find the next greater element 
+for each element of the array in order of their appearance in the array.
+Next greater element of an element in the array is the nearest element on the right which is greater than the current element.
+If there does not exist next greater of current element, then next greater element for current element is -1. 
+For example, next greater of the last element is always -1.
+
+Input: 
+N = 4, arr[] = [1 3 2 4]
+Output:
+3 4 4 -1
+
+Input: 
+N = 5, arr[] = [6 8 0 1 3]
+Output:
+8 -1 1 3 -1
+*/
+
+class Solution {
+public:
+    vector < long long > nextLargerElement(vector < long long > A, int n) {
+        vector < long long > res(n);
+        stack < long long > st;
+        for (int i = n - 1; i >= 0; i--) {
+            while (st.size() && st.top() <= A[i]) {
+                st.pop();
+            }
+            if (st.size()) {
+                res[i] = st.top();
+            } else {
+                res[i] = -1;
+            }
+
+            st.push(A[i]);
+        }
+        return res;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // https://www.interviewbit.com/problems/nextgreater/
 
 /*
@@ -23,48 +80,3 @@ vector<int> Solution::nextGreater(vector<int> &A) {
     }
     return res;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// https://practice.geeksforgeeks.org/problems/next-larger-element-1587115620/1
-
-/*
-Given an array A[] of size N having distinct elements, the task is to find the next greater element 
-for each element of the array in order of their appearance in the array.
-Next greater element of an element in the array is the nearest element on the right which is greater than the current element.
-If there does not exist next greater of current element, then next greater element for current element is -1. 
-For example, next greater of the last element is always -1.
-*/
-
-class Solution {
-public:
-    vector < long long > nextLargerElement(vector < long long > A, int n) {
-        // Your code here
-        vector < long long > res(n);
-        stack < long long > st;
-        for (int i = n - 1; i >= 0; i--) {
-            while (st.size() && st.top() <= A[i]) {
-                st.pop();
-            }
-            if (st.size()) {
-                res[i] = st.top();
-            } else {
-                res[i] = -1;
-            }
-
-            st.push(A[i]);
-        }
-        return res;
-    }
-};
