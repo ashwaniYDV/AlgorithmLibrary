559aeea848205be8cea0a794335369e2044ea50e
Ashwani Yadav <ashyadavash@gmail.com>
2021-07-08 22:26:20 +0530

    Update 08 Max Area Rectangle in binary matrix.cpp

--- Stack/08 Max Area Rectangle in binary matrix.cpp
+++ Stack/08 Max Area Rectangle in binary matrix.cpp
@@ -78,3 +78,107 @@ public:
         return mxArea;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// https://practice.geeksforgeeks.org/problems/max-rectangle/1
+class Solution {
+public:
+    int largestRectangleAreaInHisto(vector < int > & heights) {
+        int n = heights.size();
+        vector < int > left(n), right(n);
+
+        stack < int > st;
+
+        // Fill left
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && heights[st.top()] >= heights[i]) {
+                st.pop();
+            }
+
+            if (st.empty()) {
+                left[i] = 0;
+            } else {
+                left[i] = st.top() + 1;
+            }
+            st.push(i);
+        }
+
+        // Clear stack (reusing)
+        while (!st.empty()) {
+            st.pop();
+        }
+
+        // Fill right
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && heights[st.top()] >= heights[i]) {
+                st.pop();
+            }
+
+            if (st.empty()) {
+                right[i] = n - 1;
+            } else {
+                right[i] = st.top() - 1;
+            }
+            st.push(i);
+        }
+
+        // Stores mxArea
+        int mxArea = 0;
+        for (int i = 0; i < n; i++) {
+            mxArea = max(mxArea, heights[i] * (right[i] - left[i] + 1));
+        }
+        return mxArea;
+    }
+
+    int maxArea(int matrix[MAX][MAX], int n, int m) {
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 1) {
+                    matrix[i][j] += matrix[i - 1][j];
+                }
+            }
+        }
+
+        vector < int > heights(m);
+        int mxArea = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                heights[j] = matrix[i][j];
+            }
+            mxArea = max(mxArea, largestRectangleAreaInHisto(heights));
+        }
+
+        return mxArea;
+    }
+};
