eb47d685a29712a12b0b1d20f0a0e1f3b9b17dd5
Ashwani Yadav <ashyadavash@gmail.com>
2021-07-09 20:35:40 +0530

    Update 14 Find Peak Element [leetcode].cpp

--- Searching n Sorting/14 Find Peak Element [leetcode].cpp
+++ Searching n Sorting/14 Find Peak Element [leetcode].cpp
@@ -3,7 +3,9 @@
 
 /*
 A peak element is an element that is strictly greater than its neighbors.
-Given an integer array nums, find a peak element, and return its index. If the array contains multiple peaks, return the index to any of the peaks.
+
+Given an integer array nums where nums[i] != nums[i + 1] for all valid i, find a peak element, and return its index. 
+If the array contains multiple peaks, return the index to any of the peaks.
 You may imagine that nums[-1] = nums[n] = -∞.
 You must write an algorithm that runs in O(log n) time.
 
