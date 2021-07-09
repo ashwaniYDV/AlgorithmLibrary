2aee57eb29536dd551340a83cc8759300dc7efac
Ashwani Yadav <ashyadavash@gmail.com>
2021-07-09 20:34:36 +0530

    Update 14 Find Peak Element [leetcode].cpp

--- Searching n Sorting/14 Find Peak Element [leetcode].cpp
+++ Searching n Sorting/14 Find Peak Element [leetcode].cpp
@@ -11,6 +11,10 @@ Example 1:
 Input: nums = [1,2,3,1]
 Output: 2
 Explanation: 3 is a peak element and your function should return the index number 2.
+
+Constraints:
+nums[i] != nums[i + 1] for all valid i.
+-231 <= nums[i] <= 231 - 1
 */
 
 class Solution {
