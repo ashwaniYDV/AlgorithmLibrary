d988d1e109ac4cb2d301f7c2cce19a82040a85ec
Ashwani Yadav <ashyadavash@gmail.com>
2021-07-10 19:08:39 +0530

    Update find 2 unique num.cpp

--- Bitmasking/find 2 unique num.cpp
+++ Bitmasking/find 2 unique num.cpp
@@ -1,40 +1,7 @@
 // every elemnts occurs twice in the array excepttwo numbers which occurs once
+// https://www.geeksforgeeks.org/find-two-non-repeating-elements-in-an-array-of-repeating-elements/
 
-#include<bits/stdc++.h>
-using namespace std;
-#pragma GCC optimize "03"
-#pragma GCC target("sse4")
 
-#define ll long long int
-#define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-#define FRE freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
-#define f(i,n) for(ll i=0;i<n;i++)
-#define fa(i,a,n) for(ll i=a;a<n?i<n:i>=n;a<n?i+=1:i-=1)
-#define loop(i,a, n) for(ll i = a; i <= n; i++)
-#define loopb(i,a, n) for(ll i = a; i >= n; i--)
-#define pb push_back
-#define pf push_front
-#define F first
-#define S second
-#define all(x) x.begin(), x.end()
-#define setmem(x, k) memset(x, k, sizeof(x))
-#define clr(x) memset(x, 0, sizeof(x))
-#define sortall(x) sort(all(x))
-#define PI 3.1415926535897932384626
-#define MOD 1000000007
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-typedef vector<int>	vi;
-typedef vector<ll> vl;
-typedef vector<pii> vpii;
-typedef vector<pll> vpll;
-typedef vector<vi> vvi;
-typedef vector<vl> vvl;
-typedef long double ld;
-const ll N = 1e6+5;
-// ll a[N], b[N], dp[N], level[N], vis[N], in[N], out[N];
-// vl g[N];
-ll n, m;
 
 ll find2UniqueNums(ll a[], ll n) {
 	ll res = 0;
