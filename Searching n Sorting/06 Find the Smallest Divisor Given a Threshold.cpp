918fbc76fa6437ccccfb18e310636bc6e691b5cf
Ashwani Yadav <ashyadavash@gmail.com>
2021-07-12 02:56:35 +0530

    Update 06 Find the Smallest Divisor Given a Threshold.cpp

--- Searching n Sorting/06 Find the Smallest Divisor Given a Threshold.cpp
+++ Searching n Sorting/06 Find the Smallest Divisor Given a Threshold.cpp
@@ -1,48 +1,37 @@
 // https://leetcode.com/problems/find-the-smallest-divisor-given-a-threshold/
 
+/*
+Given an array of integers nums and an integer threshold, we will choose a positive integer divisor, divide all the array by it, 
+and sum the division's result. Find the smallest divisor such that the result mentioned above is less than or equal to threshold.
+
+Each result of the division is rounded to the nearest integer greater than or equal to that element. (For example: 7/3 = 3 and 10/2 = 5).
+
+It is guaranteed that there will be an answer.
+*/
+
+// FFFFFTTTTT
+
 class Solution {
 public:
-    int smallestDivisor(vector<int>& nums, int threshold) {
-        int left = 1, right = 1e6;
-        while(left <= right) {
-            int mid = (left+right) / 2;
-            int sum = 0;
-            for(int x: nums){
-                sum += ceil(double(x) / mid); 
-            }
-            if(sum > threshold) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
+    bool check(int mid, vector<int>& nums, int threshold) {
+        int sum = 0;
+        for (int x: nums) {
+            sum += (x + mid - 1) / mid;
         }
-        return left;
+        return sum <= threshold;
     }
-};
-
-
-
-
-
-
-
-
-
-
-
-class Solution {
-public:
     int smallestDivisor(vector<int>& nums, int threshold) {
-        int left = 1, right = 1e6, m, sum;
-        while (left < right) {
-            m = (left + right) / 2, sum = 0;
-            for (int i : nums)
-                sum += (i + m - 1) / m;
-            if (sum > threshold)
-                left = m + 1;
-            else
-                right = m;
+        int lo = 1, hi = 1e6;
+        
+        while (lo < hi) {
+            int mid = (lo + hi) / 2;
+            
+            if (check(mid, nums, threshold)) {
+                hi = mid;
+            } else {
+                lo = mid + 1;
+            }
         }
-        return left;
+        return lo;
     }
 };
