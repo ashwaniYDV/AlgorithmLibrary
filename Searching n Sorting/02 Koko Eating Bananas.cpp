590cffad1209b786d794a19575e7ea0c31f93577
Ashwani Yadav <ashyadavash@gmail.com>
2021-07-11 09:35:30 +0530

    Update 02 Koko Eating Bananas.cpp

--- Searching n Sorting/02 Koko Eating Bananas.cpp
+++ Searching n Sorting/02 Koko Eating Bananas.cpp
@@ -1,51 +1,31 @@
 // https://leetcode.com/problems/koko-eating-bananas/
 
+// FFFFFFTTTTTTTTTT
+
 class Solution {
 public:
-    int minEatingSpeed(vector<int>& piles, int H) {
-        int n = piles.size();
-        int left = 1, right = 0;
-        for(int x: piles) {
-            right = max(right, x);
+    bool check(int mid, int H, vector<int>& piles) {
+        int total = 0;
+        for (int p : piles) {
+            total += (p + mid - 1) / mid;
         }
-        while(left <= right) {
-            int mid = (left + right) / 2;
-            int sum = 0;
-            for(int x: piles) {
-                sum += (x + mid - 1) / mid;
-            }
-            if(sum > H) {
-                left = mid + 1;
-            }else{
-                right = mid - 1;
-            }
-        }
-        return left;
+
+        return total <= H;
     }
-};
-
-
-
-
-
-
-class Solution {
-public:
+    
     int minEatingSpeed(vector<int>& piles, int H) {
         int n = piles.size();
-        int l = 1, r = 0;
-        for(int x: piles) {
-            r = max(r, x);
-        }
-        while (l < r) {
-            int m = (l + r) / 2, total = 0;
-            for (int p : piles)
-                total += (p + m - 1) / m;
-            if (total > H)
-                l = m + 1;
+        int lo = 1;
+        int hi = *max_element(piles.begin(), piles.end());
+        
+        while (lo < hi) {
+            int mid = (lo + hi) / 2;
+
+            if (check(mid, H, piles))
+                hi = mid;
             else
-                r = m;
+                lo = mid + 1;
         }
-        return l;
+        return lo;
     }
 };
