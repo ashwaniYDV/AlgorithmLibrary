6a679f3e092523c281652a33acdc7a275a04874f
Ashwani Yadav <ashyadavash@gmail.com>
2021-07-16 01:55:21 +0530

    Update and rename crud1.cpp to 00 LinkedList.cpp

--- LinkedList/00 LinkedList.cpp
+++ LinkedList/00 LinkedList.cpp
@@ -1,48 +1,11 @@
-#include <iostream>
-#include<bits/stdc++.h>
-#include<vector>
-using namespace std;
- 
-#define ll long long int
-#define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-#define FRE freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
-#define f(i,n) for(ll i=0;i<n;i++)
-#define fa(i,a,n) for(ll i=a;a<n?i<n:i>=n;a<n?i+=1:i-=1)
-#define rep(i,a, n) for(i = a; i < n; i++)
-#define ren(i,a, n) for(i = a; i >= n; i--)
-#define pb push_back
-#define pf push_front
-#define F first
-#define S second
-#define all(x) x.begin(), x.end()
-#define setmem(x, k) memset(x, k, sizeof(x))
-#define clr(x) memset(x, 0, sizeof(x))
-#define sortall(x) sort(all(x))
-#define PI 3.1415926535897932384626
-#define MOD 1000000007
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-typedef vector<int>	vi;
-typedef vector<ll> vl;
-typedef vector<pii> vpii;
-typedef vector<pll> vpll;
-typedef vector<vi> vvi;
-typedef vector<vl> vvl;
-typedef long double ld;
-// const ll N = 1e6+5, M = N, ninf = -2e5, inf = (int)1e9;
-// vl g[N], dist;
-// ll a[N], b[N], dp[N], in[N], out[N], vis[N], level[N];
-// const ll N=1005;
-// ll n, m;
-
 struct Node{
-	ll data;
+	int data;
 	Node* next;
 };
 
 Node* head;
 
-void Insert(ll data){
+void Insert(int data){
 	Node* temp = new Node();
 	temp->data = data;
 	temp->next = NULL;
