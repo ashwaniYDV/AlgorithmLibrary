77592a9dc528799e62aa4ac9a77d899d42c721c9
Ashwani Yadav <ashyadavash@gmail.com>
2021-07-16 01:58:46 +0530

    Update and rename Reverse Linked List.cpp to 01 Reverse Linked List.cpp

---
+++ LinkedList/01 Reverse Linked List.cpp
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/reverse-linked-list/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head == NULL){
+            return head;
+        }
+        ListNode *prev = NULL, *cur = head, *nxt = NULL;
+        while(cur != NULL){
+            nxt = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = nxt;
+        }
+        return prev;
+    }
+};
