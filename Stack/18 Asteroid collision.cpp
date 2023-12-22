e7e84ec5cd96c1d5a7dbf4d154232c9bedc8b4c8
Ashwani Yadav <ashyadavash@gmail.com>
2023-12-22 17:30:22 +0530

    Update 18 Asteroid collision.cpp

--- Stack/18 Asteroid collision.cpp
+++ Stack/18 Asteroid collision.cpp
@@ -1,5 +1,22 @@
 // https://leetcode.com/problems/asteroid-collision/description/
 
+/*
+We are given an array asteroids of integers representing asteroids in a row.
+For each asteroid, the absolute value represents its size, and the sign represents its direction (positive meaning right, negative meaning left). 
+Each asteroid moves at the same speed.
+Find out the state of the asteroids after all collisions. 
+If two asteroids meet, the smaller one will explode. If both are the same size, both will explode. 
+Two asteroids moving in the same direction will never meet. 
+
+Input: asteroids = [5,10,-5]
+Output: [5,10]
+Explanation: The 10 and -5 collide resulting in 10. The 5 and 10 never collide.
+
+Input: asteroids = [8,-8]
+Output: []
+Explanation: The 8 and -8 collide exploding each other.
+*/
+
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& a) {
