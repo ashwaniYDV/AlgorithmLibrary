e14902aeeb6dc4b916d243c2d3c0cf4a9df779f1
Ashwani Yadav <ashyadavash@gmail.com>
2024-01-02 23:16:08 +0530

    Update 05 Longest Increasing Path in a Matrix (Longest Path in DAG concept) [leetcode].cpp

--- Graph/Topological Sort/05 Longest Increasing Path in a Matrix (Longest Path in DAG concept) [leetcode].cpp
+++ Graph/Topological Sort/05 Longest Increasing Path in a Matrix (Longest Path in DAG concept) [leetcode].cpp
@@ -1,3 +1,11 @@
+// https://leetcode.com/problems/longest-increasing-path-in-a-matrix/description/
+
+/*
+Given an m x n integers matrix, return the length of the longest increasing path in matrix.
+From each cell, you can either move in four directions: left, right, up, or down. 
+You may not move diagonally or move outside the boundary (i.e., wrap-around is not allowed).
+*/
+
 /*
 Consider:
   a cell in the matrix as a node,
