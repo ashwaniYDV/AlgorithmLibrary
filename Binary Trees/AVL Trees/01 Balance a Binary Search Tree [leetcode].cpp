873dd57681adb90977015faa0eb157f4aa007a20
Ashwani Yadav <ashyadavash@gmail.com>
2024-01-03 12:09:19 +0530

    Update 01 Balance a Binary Search Tree [leetcode].cpp

--- Binary Trees/AVL Trees/01 Balance a Binary Search Tree [leetcode].cpp
+++ Binary Trees/AVL Trees/01 Balance a Binary Search Tree [leetcode].cpp
@@ -1,17 +1,7 @@
 // https://leetcode.com/problems/balance-a-binary-search-tree/
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 
+// Method 1: AVL Tree
 class Solution {
 public:
     map<TreeNode*, int> NodeToHeight;
@@ -124,5 +114,41 @@ public:
             return balanceBST(root);
         }
         return root;
+    }
+};
+
+// Method 2: Using "Sorted array to height-balanced BST" concept
+class Solution {
+public:
+    TreeNode* fun(int l, int r, vector<int>& nums) {
+        // base case
+        if(l > r) return NULL;
+
+        int mid = ((l + r) >> 1);
+        TreeNode *root = new TreeNode(nums[mid]);
+        root->left = fun(l, mid - 1, nums);
+        root->right = fun(mid + 1, r, nums);
+
+        return root;
+    }
+    
+    TreeNode* sortedArrayToHeightBalancedBST(vector<int>& nums) {
+        return fun(0, nums.size() - 1, nums);
+    }
+
+    void inorder(TreeNode* root, vector<int>& nums) {
+        if(!root) return;
+        inorder(root->left, nums);
+        nums.push_back(root->val);
+        inorder(root->right, nums);
+    }
+
+    TreeNode* balanceBST(TreeNode* root) {
+        if (!root) return root;
+        
+        vector<int> nums;
+        inorder(root, nums);
+
+        return sortedArrayToHeightBalancedBST(nums);
     }
 };
