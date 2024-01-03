a255f4ad25bbd75cf72c2ccdb88fb6adfb9037a4
Ashwani Yadav <ashyadavash@gmail.com>
2024-01-03 21:49:12 +0530

    Update 06 Shortest Path in a Grid with Obstacles Elimination [leetcode].cpp

--- Graph/Shortest Paths/Dijkstra/06 Shortest Path in a Grid with Obstacles Elimination [leetcode].cpp
+++ Graph/Shortest Paths/Dijkstra/06 Shortest Path in a Grid with Obstacles Elimination [leetcode].cpp
@@ -1,5 +1,14 @@
 // https://leetcode.com/problems/shortest-path-in-a-grid-with-obstacles-elimination/
 
+/*
+You are given an m x n integer matrix grid where each cell is either 0 (empty) or 1 (obstacle). 
+You can move up, down, left, or right from and to an empty cell in one step.
+Return the minimum number of steps to walk from the upper left corner (0, 0) to the lower right corner (m - 1, n - 1) 
+given that you can eliminate at most k obstacles. 
+If it is not possible to find such walk return -1.
+*/
+
+
 // Method 1: Using normal queue instead of priority_queue
 /*
 priority_queue was giving TLE.
