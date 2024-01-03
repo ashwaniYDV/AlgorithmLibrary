7085098b95da2544599df0ed6b7e8e146ca2ced5
Ashwani Yadav <ashyadavash@gmail.com>
2024-01-03 23:46:47 +0530

    Update 07 Shortest Path in a Grid with Obstacles Elimination [leetcode].cpp

--- Graph/Shortest Paths/Dijkstra/07 Shortest Path in a Grid with Obstacles Elimination [leetcode].cpp
+++ Graph/Shortest Paths/Dijkstra/07 Shortest Path in a Grid with Obstacles Elimination [leetcode].cpp
@@ -9,7 +9,7 @@ If it is not possible to find such walk return -1.
 */
 
 
-// Method 1: Using normal queue instead of priority_queue
+// Method 1.1: Using normal queue instead of priority_queue
 /*
 priority_queue was giving TLE.
 Since all edge weights are 1, we can use a normal queue.
@@ -26,7 +26,6 @@ public:
             return false;
         return true;
     }
-
 
     int shortestPath(vector<vector<int>>& grid, int k) {
         n = grid.size(), m = grid[0].size();
@@ -56,6 +55,63 @@ public:
                     if (j < k && dist[nx][ny][j + 1] > dist[x][y][j] + 1) {
                         dist[nx][ny][j + 1] = dist[x][y][j] + 1;
                         pq.push({dist[nx][ny][j + 1], nx, ny, j + 1});
+                    }
+                }
+            }
+        }
+
+        int res = INF;
+        for(int z = 0; z <= k; z++) {
+            res = min(res, dist[n-1][m-1][z]);
+        }
+        
+        return (res == INF ? -1 : res);
+    }
+};
+
+
+// Method 1.2
+class Solution {
+public:
+    #define INF 1e9
+    int dx[4] = {0, 1, 0, -1};
+    int dy[4] = { -1, 0, 1, 0};
+    int n, m;
+
+    bool isSafe(int i, int j) {
+        if (i < 0 || i >= n || j < 0 || j >= m)
+            return false;
+        return true;
+    }
+
+    int shortestPath(vector<vector<int>>& grid, int k) {
+        n = grid.size(), m = grid[0].size();
+        vector<vector<vector<int> > > dist(n, vector<vector<int> >(m, vector<int>(k+1, INF)));
+
+        queue<vector<int>> q;
+        // row, col, j
+        q.push({0, 0, 0});
+        dist[0][0][0] = 0;
+
+        while (!q.empty()) {
+            auto it = q.front();
+            q.pop();
+            int x = it[0], y = it[1], j = it[2];
+
+            for (int z = 0; z < 4; z++) {
+                int nx = x + dx[z], ny = y + dy[z];
+                if (!isSafe(nx, ny)) continue;
+
+                if(grid[nx][ny] == 0) {
+                    if (dist[nx][ny][j] > dist[x][y][j] + 1) {
+                        dist[nx][ny][j] = dist[x][y][j] + 1;
+                        q.push({nx, ny, j});
+                    }
+                }
+                if(grid[nx][ny] == 1) {
+                    if (j < k && dist[nx][ny][j + 1] > dist[x][y][j] + 1) {
+                        dist[nx][ny][j + 1] = dist[x][y][j] + 1;
+                        q.push({nx, ny, j + 1});
                     }
                 }
             }
