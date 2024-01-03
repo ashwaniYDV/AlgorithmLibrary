a9e41b6e4451a3ac8483f7be2fc9fede63acd438
Ashwani Yadav <ashyadavash@gmail.com>
2024-01-03 16:05:15 +0530

    Update 01 Feasible relations (Hackerearth).cpp

--- DSU/01 Feasible relations (Hackerearth).cpp
+++ DSU/01 Feasible relations (Hackerearth).cpp
@@ -4,6 +4,17 @@
 You are given a list of binary relations, equalities and inequalities, like a = b, a != d, b = c etc. 
 Your task is to output YES if you can assign integers to input variables in such a way, that you can satisfy all equalities and inequalities. 
 Otherwise you should output NO.
+
+Input format:
+In the first line there is one integer T denoting the number of test cases. Description of T test cases follow. 
+Each one have two integers N and K given in the first line denoting the number of variables and the number of relations between them for this test case. 
+All variables are represented by integers in range [1, N]. 
+K lines follow. Each of them is of the form "x1 R x2" where x1 and x2 are integers representing variables and 
+R is either "=" or "!=" and denotes the kind of relation between these variables.
+
+Output format:
+Output exactly T lines. 
+In i-th of them, output the answer to the i-th test case.
 */
 
 const int N = 1e6+5;
