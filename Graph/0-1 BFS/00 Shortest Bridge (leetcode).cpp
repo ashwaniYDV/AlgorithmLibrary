1b253705b0cbcdfc35ee9fb0788a7132d1a9149f
Ashwani Yadav <ashyadavash@gmail.com>
2024-01-03 19:15:03 +0530

    Update 00 Shortest Bridge (leetcode).cpp

--- Graph/0-1 BFS/00 Shortest Bridge (leetcode).cpp
+++ Graph/0-1 BFS/00 Shortest Bridge (leetcode).cpp
@@ -2,7 +2,8 @@
 
 /*
 Problem:
-In a given 2D binary array grid, there are two islands. (An island is a 4-directionally connected group of 1s not connected to any other 1s.)
+In a given 2D binary array grid, there are two islands. 
+(An island is a 4-directionally connected group of 1s not connected to any other 1s.)
 Now, we may change 0s to 1s so as to connect the two islands together to form 1 island.
 Return the smallest number of 0s that must be flipped. (It is guaranteed that the answer is at least 1.)
 */
