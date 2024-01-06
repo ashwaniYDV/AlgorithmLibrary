0d2de552460ac9d9450d189ed59cbae0383ef638
Ashwani Yadav <ashyadavash@gmail.com>
2024-01-06 12:49:20 +0530

    Update 16 Painters Partition Problem.cpp

--- Searching n Sorting/16 Painters Partition Problem.cpp
+++ Searching n Sorting/16 Painters Partition Problem.cpp
@@ -2,14 +2,13 @@
 
 /*
 Given 2 integers A and B and an array of integers C of size N. Element C[i] represents the length of ith board.
-You have to paint all N boards [C0, C1, C2, C3 … CN-1]. There are A painters available and each of them takes B units of time to paint 1 unit of the board.
-
+You have to paint all N boards [C0, C1, C2, C3 … CN-1]. 
+There are A painters available and each of them takes B units of time to paint 1 unit of the board.
 Calculate and return the minimum time required to paint all boards under the constraints that any painter will only paint contiguous sections of the board.
-NOTE:
-1. 2 painters cannot share a board to paint. That is to say, a board cannot be painted partially by one painter, and partially by another.
-2. A painter will only paint contiguous boards. This means a configuration where painter 1 paints boards 1 and 3 but not 2 is invalid.
 
-Return the ans % 10000003.
+NOTE:
+a) 2 painters cannot share a board to paint. That is to say, a board cannot be painted partially by one painter, and partially by another.
+b) A painter will only paint contiguous boards. This means a configuration where painter 1 paints boards 1 and 3 but not 2 is invalid.
 */
 
 const int mod = 10000003;
