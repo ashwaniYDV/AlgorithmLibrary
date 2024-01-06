58efe12e6b7234cdb7eb1d630d112b2ad3689bc9
Ashwani Yadav <ashyadavash@gmail.com>
2024-01-06 12:24:01 +0530

    Update 15 Maximum Profit in Job Scheduling.cpp

--- Searching n Sorting/15 Maximum Profit in Job Scheduling.cpp
+++ Searching n Sorting/15 Maximum Profit in Job Scheduling.cpp
@@ -1,3 +1,11 @@
+// https://leetcode.com/problems/maximum-profit-in-job-scheduling/description/
+
+/*
+We have n jobs, where every job is scheduled to be done from startTime[i] to endTime[i], obtaining a profit of profit[i].
+You're given the startTime, endTime and profit arrays, return the maximum profit you can take such that there are no two jobs in the subset with overlapping time range.
+If you choose a job that ends at time X you will be able to start another job that starts at time X.
+*/
+
 class Solution {
 public:
     int n;
