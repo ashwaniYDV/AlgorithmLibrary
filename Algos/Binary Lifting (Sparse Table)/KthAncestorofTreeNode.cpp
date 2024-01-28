171be3cfcd1f0a2545d378f1f31983a35fcff140
Ashwani Yadav <ashyadavash@gmail.com>
2024-01-28 22:26:06 +0530

    Update KthAncestorofTreeNode.cpp

--- Algos/Binary Lifting (Sparse Table)/KthAncestorofTreeNode.cpp
+++ Algos/Binary Lifting (Sparse Table)/KthAncestorofTreeNode.cpp
@@ -61,27 +61,29 @@ public:
 };
 
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
 
 class TreeAncestor {
-    vector<vector<int>>v;
-public:
-    TreeAncestor(int n, vector<int>& parent) {
-        vector<vector<int>> par(n,vector<int>(20));
-        for(int i=0;i<n;i++) par[i][0] = parent[i];
-        for(int j=1;j<20;j++){
-            for(int i=0;i<n;i++){
-                if(par[i][j-1] == -1) par[i][j] = -1;
-                else par[i][j] = par[par[i][j-1]][j-1];
+    vector < vector < int >> v;
+    public:
+        TreeAncestor(int n, vector < int > & parent) {
+            vector < vector < int >> par(n, vector < int > (20));
+            for (int i = 0; i < n; i++) par[i][0] = parent[i];
+            for (int j = 1; j < 20; j++) {
+                for (int i = 0; i < n; i++) {
+                    if (par[i][j - 1] == -1) par[i][j] = -1;
+                    else par[i][j] = par[par[i][j - 1]][j - 1];
+                }
             }
+            swap(v, par);
         }
-        swap(v,par);      
-    }
     int getKthAncestor(int node, int k) {
-        for(int i=0;i<20;i++){
-            if((k>>i)&1){
+        for (int i = 0; i < 20; i++) {
+            if ((k >> i) & 1) {
                 node = v[node][i];
-                if(node == -1) return -1;
+                if (node == -1) return -1;
             }
         }
         return node;
