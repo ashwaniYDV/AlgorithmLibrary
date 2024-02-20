94132dd026bde6a8c65095a6467a82d30dc0143a
Ashwani Yadav <ashyadavash@gmail.com>
2024-02-20 17:40:49 +0530

    Update 08 Linked List Cycle (return true or false).cpp

--- LinkedList/08 Linked List Cycle (return true or false).cpp
+++ LinkedList/08 Linked List Cycle (return true or false).cpp
@@ -1,37 +1,5 @@
 // https://leetcode.com/problems/linked-list-cycle/
-
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
-
-/*
-Given head, the head of a linked list, determine if the linked list has a cycle in it.
-Return true if there is a cycle in the linked list. Otherwise, return false.
-*/
-
-// not memory efficient
-class Solution {
-public:
-    bool hasCycle(ListNode *head) {
-        if(!head) return false;
-        
-        map<ListNode*, int> mp;
-        
-        while(head) {
-            if(mp[head]) return true;
-            mp[head] = 1;
-            head = head->next;
-        }
-        
-        return false;
-    }
-};
-
+// https://youtu.be/wiOo4DC5GGA?si=vt8znZokzn638CIn
 
 // slow fast pointer
 class Solution {
@@ -46,6 +14,24 @@ public:
             fast = fast->next->next;
             
             if(slow == fast) return true;
+        }
+        
+        return false;
+    }
+};
+
+// not memory efficient
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(!head) return false;
+        
+        map<ListNode*, int> mp;
+        
+        while(head) {
+            if(mp[head]) return true;
+            mp[head] = 1;
+            head = head->next;
         }
         
         return false;
