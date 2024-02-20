f91db24b7f3f358d2034d48aafb88971f8f0c5f9
Ashwani Yadav <ashyadavash@gmail.com>
2024-02-20 17:54:54 +0530

    Update and rename 08 Linked List Cycle II (return node).cpp to 08 Linked List Cycle.cpp

--- LinkedList/08 Linked List Cycle.cpp
+++ LinkedList/08 Linked List Cycle.cpp
@@ -1,10 +1,28 @@
+// https://leetcode.com/problems/linked-list-cycle/
+// https://youtu.be/wiOo4DC5GGA?si=vt8znZokzn638CIn
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(!head || !head->next) return false;
+        
+        ListNode *slow = head, *fast = head;
+        
+        while(fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+            
+            if(slow == fast) return true;
+        }
+        
+        return false;
+    }
+};
+
+
 // https://leetcode.com/problems/linked-list-cycle-ii/
 // https://youtu.be/wiOo4DC5GGA?si=vt8znZokzn638CIn
 
-/*
-Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
-*/
- 
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
