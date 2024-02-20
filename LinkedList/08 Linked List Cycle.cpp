37265b59b7ba47678991cca2dc7857625f82ee03
Ashwani Yadav <ashyadavash@gmail.com>
2024-02-20 17:56:34 +0530

    Update 08 Linked List Cycle.cpp

--- LinkedList/08 Linked List Cycle.cpp
+++ LinkedList/08 Linked List Cycle.cpp
@@ -20,6 +20,8 @@ public:
 };
 
 
+
+
 // https://leetcode.com/problems/linked-list-cycle-ii/
 // https://youtu.be/wiOo4DC5GGA?si=vt8znZokzn638CIn
 
@@ -48,3 +50,49 @@ public:
         return NULL;
     }
 };
+
+
+
+
+
+
+// https://www.geeksforgeeks.org/problems/find-length-of-loop/1
+
+Node* getCycleNode(Node *head) {
+    if(!head || !head->next) return NULL;
+    
+    Node* slow = head, *fast = head;
+    
+    while(fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+        
+        // for getting the intersection point
+        if(slow == fast) {
+            Node* slow = head;
+            while(slow != fast) {
+                slow = slow->next;
+                fast = fast->next;
+            }
+            return slow;
+        };
+    }
+    
+    return NULL;
+}
+
+//Function to find the length of a loop in the linked list.
+int countNodesinLoop(struct Node *head)
+{
+    if (!head) return 0;
+    Node* cycleNode = getCycleNode(head);
+    if (!cycleNode) return 0;
+    
+    Node* temp = cycleNode;
+    int res = 1;
+    while(temp->next != cycleNode) {
+        res++;
+        temp = temp->next;
+    }
+    return res;
+}
