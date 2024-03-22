6746fe3c33c77743424788e1e3ccede5edbe258d
Ashwani Yadav <ashyadavash@gmail.com>
2024-03-22 18:45:23 +0530

    Update 08 Find mini in rotated sorted array II (duplicates).cpp

--- Searching n Sorting/08 Find mini in rotated sorted array II (duplicates).cpp
+++ Searching n Sorting/08 Find mini in rotated sorted array II (duplicates).cpp
@@ -1,6 +1,46 @@
 // https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/
-
+// https://youtu.be/nhEMDKMB44g?si=2OdMI-0Fnp0ieeyK
 // https://youtu.be/j3187M1P2Xg
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int lo = 0, hi = nums.size() - 1;
+        int res = INT_MAX;
+        
+        while (lo <= hi) {
+            // this is extra code we have to add
+            if( nums[lo] == nums[hi] ) {
+                res = min(res, nums[lo]);
+                ++lo; --hi;
+                continue;
+            }
+
+            int mid = (lo + hi) / 2;
+
+            if (nums[lo] <= nums[hi]) {
+                res = min(res, nums[lo]);
+                break;
+            }
+            
+            // left part is monotonic thus updating our answer
+            if (nums[lo] <= nums[mid]) {
+                res = min(res, nums[lo]);
+                lo = mid + 1;
+            } 
+            // right part is monotonic thus updating our answer
+            else {
+                res = min(res, nums[mid]);
+                hi = mid - 1;
+            }
+        }
+        
+        return res;
+    }
+};
+
+
+
 
 /*
 Consider case like this
@@ -29,49 +69,16 @@ public:
             // the only difference from the first one, trickly case, just updat left and right
             if( (nums[lo] == nums[mid]) && (nums[hi] == nums[mid]) ) {
                 ++lo; --hi;
+                continue;
             }
             
             // left part is monotonic thus pivot lies in right part
-            else if (nums[lo] <= nums[mid]) {
+            if (nums[lo] <= nums[mid]) {
                 lo = mid + 1;
             } 
             // right part is monotonic thus pivot lies in left part
             else {
                 hi = mid;
-            }
-        }
-        
-        return nums[lo];
-    }
-};
-
-
-
-
-
-
-
-
-
-
-
-class Solution {
-public:
-    int findMin(vector<int>& nums) {
-        int lo = 0, hi = nums.size() - 1;
-        
-        while (lo < hi) {
-            if (nums[lo] < nums[hi]) return nums[lo];
-            
-            int mid = (lo + hi) / 2;
-            
-            if (nums[lo] < nums[mid]) {
-                lo = mid + 1;
-            } else if (nums[mid] < nums[hi]) {
-                hi = mid;
-            } else {
-                if (nums[lo] == nums[mid]) lo++;
-                if (nums[mid] == nums[hi]) hi--;
             }
         }
         
