dd23ff7694eb6c6337a042ed6ec54917b6288c55
Ashwani Yadav <ashyadavash@gmail.com>
2025-02-28 16:14:25 +0530

    Update 01 Longest Common Subsequence.cpp

--- DP/LCS/01 Longest Common Subsequence.cpp
+++ DP/LCS/01 Longest Common Subsequence.cpp
@@ -1,3 +1,9 @@
+// https://leetcode.com/problems/longest-common-subsequence/
+
+/*
+dp[i][j] = longest common subsequence ending at indices i of s1 and j of s2
+*/
+
 
 ll fun(int pos1, int pos2, string s1, string s2) {
     if(pos1 < 0 || pos2 < 0) return 0;
@@ -9,88 +15,69 @@ ll fun(int pos1, int pos2, string s1, string s2) {
     }
 }
 
-void LC_Subsequence_Bottom_Up(string s1, string s2){
-    ll n1 = s1.size(), n2=s2.size();
-    ll dp[n1 + 1][n2 + 1];
-    for(int i = 0; i <= n1; i++) {
-        for(int j = 0; j <= n2; j++) {
-            if(i == 0 || j == 0) {
-                dp[i][j] = 0;
-            } else if(s1[i - 1] == s2[j - 1]) {
-                dp[i][j] = dp[i - 1][j - 1] + 1;
-            }else{
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+
+
+class Solution {
+public:
+    int longestCommonSubsequence(string s1, string s2) {
+        int n1 = s1.size(), n2 = s2.size();
+        vector<vector<int>>dp(n1 + 1, vector<int>(n2 + 1, 0));
+
+        for(int i = 0; i <= n1; i++) {
+            for(int j = 0; j <= n2; j++) {
+                if(i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                } else if(s1[i - 1] == s2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
             }
         }
-    }
 
-    int maxLength = dp[n1][n2];
-    
-    string ans = "";
-    int i = n1, j = n2;
-    while(i > 0 && j > 0) {
-        if(s1[i - 1] == s2[j - 1]) {
-            ans = s1[i-1] + ans;
-            i--, j--;
-        } else if(dp[i - 1][j] > dp[i][j - 1]) {
-            i--;
-        } else {
-            j--;
-        }
-    }
-    cout << maxLength << endl;
-    cout << ans << endl;
-}
-
-void solve() {
-    int x, y;
-    string s1, s2;
-    cin >> s1 >> s2;
-    LC_Subsequence_Bottom_Up(s1, s2);
-    cout << fun(s1.size() - 1, s2.size() - 1, s1, s2) << endl;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-string lcs(string& A, string& B) {
-    int n = A.size(), m = B.size();
-    vector<vector<string>> dp(n + 1, vector<string>(m + 1, ""));
-    for (int i = 0; i <= n; ++i) {
-        for (int j = 0; j <= m; ++j) {
-            if(i == 0 || j == 0) {
-                dp[i][j] = "";
-            } else if (A[i-1] == B[j-1]) {
-                dp[i][j] = dp[i - 1][j - 1] + A[i - 1];
+        int maxLength = dp[n1][n2];
+        string ans = "";
+        int i = n1, j = n2;
+        while(i > 0 && j > 0) {
+            if(s1[i - 1] == s2[j - 1]) {
+                ans += s1[i-1];
+                i--, j--;
+            } else if(dp[i - 1][j] > dp[i][j - 1]) {
+                i--;
             } else {
-                dp[i][j] = dp[i - 1][j].size() > dp[i][j - 1].size() ?  dp[i - 1][j] : dp[i][j - 1];
+                j--;
             }
         }
+        reverse(ans.begin(), ans.end());
+        cout << ans << endl;
+
+        return maxLength;
     }
-    return dp[n][m];
-}
+};
+
+
+
+
+class Solution {
+public:
+    int longestCommonSubsequence(string s1, string s2) {
+        int n1 = s1.size(), n2 = s2.size();
+        vector<vector<string>>dp(n1 + 1, vector<string>(n2 + 1, ""));
+
+        for(int i = 0; i <= n1; i++) {
+            for(int j = 0; j <= n2; j++) {
+                if(i == 0 || j == 0) {
+                    dp[i][j] = "";
+                } else if(s1[i - 1] == s2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + s1[i - 1];
+                } else {
+                    dp[i][j] = dp[i - 1][j].size() > dp[i][j - 1].size() ?  dp[i - 1][j] : dp[i][j - 1];
+                }
+            }
+        }
+
+        cout << dp[n1][n2] << endl;
+
+        return dp[n1][n2].size();
+    }
+};
