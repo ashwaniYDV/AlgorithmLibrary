355cd28ace6eb60b6e6a937c9611875316d69dc1
Ashwani Yadav <ashyadavash@gmail.com>
2025-03-13 12:28:38 +0530

    Update 16 Zero Array Transformation II.cpp

--- Searching n Sorting/16 Zero Array Transformation II.cpp
+++ Searching n Sorting/16 Zero Array Transformation II.cpp
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/zero-array-transformation-ii/
+
 /*
 You are given an integer array nums of length n and a 2D array queries where queries[i] = [li, ri, vali].
 Each queries[i] represents the following action on nums:
@@ -16,16 +18,6 @@ The array will become [1, 0, 1].
 For i = 1 (l = 0, r = 2, val = 1):
 Decrement values at indices [0, 1, 2] by [1, 0, 1] respectively.
 The array will become [0, 0, 0], which is a Zero Array. Therefore, the minimum value of k is 2.
-
-Input: nums = [4,3,2,1], queries = [[1,3,2],[0,2,1]]
-Output: -1
-Explanation:
-For i = 0 (l = 1, r = 3, val = 2):
-Decrement values at indices [1, 2, 3] by [2, 2, 1] respectively.
-The array will become [4, 1, 0, 0].
-For i = 1 (l = 0, r = 2, val = 1):
-Decrement values at indices [0, 1, 2] by [1, 1, 0] respectively.
-The array will become [3, 0, 0, 0], which is not a Zero Array.
  
 
 Constraints:
@@ -76,5 +68,68 @@ public:
             }
         }
         return lo > queries.size() ? -1 : lo;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+// https://leetcode.com/problems/zero-array-transformation-i/
+
+/*
+You are given an integer array nums of length n and a 2D array queries, where queries[i] = [li, ri].
+For each queries[i]:
+* Select a subset of indices within the range [li, ri] in nums.
+* Decrement the values at the selected indices by 1.
+A Zero Array is an array where all elements are equal to 0.
+Return true if it is possible to transform nums into a Zero Array after processing all the queries sequentially, otherwise return false.
+
+
+Input: nums = [1,0,1], queries = [[0,2]]
+Output: true
+Explanation:
+For i = 0:
+Select the subset of indices as [0, 2] and decrement the values at these indices by 1.
+The array will become [0, 0, 0], which is a Zero Array.
+
+Constraints:
+1 <= nums.length <= 10^5
+0 <= nums[i] <= 10^5
+1 <= queries.length <= 10^5
+queries[i].length == 2
+0 <= li <= ri < nums.length
+*/
+
+class Solution {
+public:
+    bool isZeroArray(vector<int>& a, vector<vector<int>>& queries) {
+        int n = a.size();
+        vector<int> maxDeletions(n+1, 0);
+
+        for(auto& it: queries) {
+            int l = it[0], r = it[1];
+            maxDeletions[l] += 1;
+            maxDeletions[r+1] -= 1;
+        }
+
+        // pref sum
+        for(int i = 1; i < n+1; i++) {
+            maxDeletions[i] += maxDeletions[i - 1];
+        }
+
+        for(int i = 0; i < n; i++) {
+            // we cannot make it zero by nax deletions
+            if(a[i] > maxDeletions[i]) return false;
+        }
+
+        return true;
     }
 };
