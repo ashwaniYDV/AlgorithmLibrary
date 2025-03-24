d79e2393f1ba6f51de930c75bef61ffbfbe1b92d
Ashwani Yadav <ashyadavash@gmail.com>
2025-03-25 00:26:15 +0530

    Update 01 0 Merge Intervals.cpp

--- Greedy/01 0 Merge Intervals.cpp
+++ Greedy/01 0 Merge Intervals.cpp
@@ -1,28 +1,26 @@
 // https://leetcode.com/problems/merge-intervals/
 
 class Solution {
-public:
-    static bool compare(vector<int>& a1, vector<int>& a2) {
-        return a1[0] < a2[0];
-    }
-    
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        int n = intervals.size();
+public:    
+    vector<vector<int>> merge(vector<vector<int>>& a) {
+        int n = a.size();
         if (n == 0) return {};
         
         // sort on the basis of starting of interval
-        sort(intervals.begin(), intervals.end(), compare);
+        sort(a.begin(), a.end(), [](vector<int>& a1, vector<int>& a2) {
+        return a1[0] < a2[0];
+    });
         
         vector<vector<int>> res;
-        res.push_back(intervals[0]);
+        res.push_back(a[0]);
         
         for (int i = 1; i < n; i++) {
-            // res.back.end < intervals[i].start
-            if (res.back()[1] < intervals[i][0]) {
-                res.push_back(intervals[i]);
+            // res.back.end < a[i].start
+            if (res.back()[1] < a[i][0]) {
+                res.push_back(a[i]);
             } else {
-                // res.back.end = max(res.back.end, intervals[i].end)
-                res.back()[1] = max(res.back()[1], intervals[i][1]);
+                // res.back.end = max(res.back.end, a[i].end)
+                res.back()[1] = max(res.back()[1], a[i][1]);
             }
         }
         
