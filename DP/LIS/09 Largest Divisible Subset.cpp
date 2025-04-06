6d8d3207d65c3429b2dbd40f6b3fe7659f32a7c4
Ashwani Yadav <ashyadavash@gmail.com>
2025-04-06 23:12:55 +0530

    Update 09 Largest Divisible Subset.cpp

--- DP/LIS/09 Largest Divisible Subset.cpp
+++ DP/LIS/09 Largest Divisible Subset.cpp
@@ -26,36 +26,31 @@ All the integers in nums are unique.
 
 class Solution {
 public:
-    vector<int> largestDivisibleSubset(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> ans;
-        
-        if(n==0)
-            return ans;
-        
-        sort(nums.begin(), nums.end());
-        
-        int mx = 1;
-        vector<int> dp(n+1, 1);
-        
-        for(int i=1; i < n; ++i) {
-            for(int j=0; j < i; ++j) {
-                if(nums[i] % nums[j] == 0){
+    vector<int> largestDivisibleSubset(vector<int>& a) {
+        int n = a.size();
+        sort(a.begin(), a.end());
+        vector<int> dp(n, 1);
+
+        int lis = 1;
+        for(int i = 1; i < n; i++) {
+            for(int j = 0; j < i; j++) {
+                if(a[i] % a[j] == 0) {
                     dp[i] = max(dp[i], dp[j] + 1);
                 }
             }
-            mx = max(mx, dp[i]);
+            lis = max(lis, dp[i]);
         }
-        
-        //Now push the answer LIS
+
+        vector<int> res;
         int prev = -1;
         for(int i = n-1; i >= 0; i--) {
-            if(dp[i] == mx &&  (prev % nums[i] == 0 || prev==-1)) {
-                ans.push_back(nums[i]);
-                mx -= 1;
-                prev = nums[i];
+            if(dp[i] == lis && (prev == -1 || prev % a[i] == 0)) {
+                res.push_back(a[i]);
+                lis--;
+                prev = a[i];
             }
         }
-        return ans;
+
+        return res;
     }
 };
