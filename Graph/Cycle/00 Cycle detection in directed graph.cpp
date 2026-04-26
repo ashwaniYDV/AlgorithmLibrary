bbecb1b1a7d1b6c975b10003b59f98b7c1bae9da
Ashwani Yadav <ashyadavash@gmail.com>
2026-04-27 02:08:11 +0530

    Update 00 Cycle detection in directed graph.cpp

--- Graph/Cycle/00 Cycle detection in directed graph.cpp
+++ Graph/Cycle/00 Cycle detection in directed graph.cpp
@@ -1,33 +1,53 @@
 // since it's for directed graph, therefore works for undirected as well
 
 
-ll cycleDetected=0;
-ll color[N]; // all white
+#include <bits/stdc++.h>
+using namespace std;
 
-void dfs(ll u){
-    color[u]=1; // grey
-    for(ll v: g[u]){
-        if(color[v] == 1){
-            // do anything you want when cycle detected
-            cycleDetected=1;
-        }
-        if(color[v]==0)
+const int N = 1e5 + 5;
+
+vector<int> g[N];
+int color[N]; // 0 = white, 1 = grey, 2 = black
+bool cycleDetected = false;
+
+void dfs(int u) {
+    color[u] = 1; // mark grey (in current DFS path)
+
+    for (int v : g[u]) {
+        if (color[v] == 0) {
+            // not visited
             dfs(v);
+        } 
+        else if (color[v] == 1) {
+            // back edge → cycle found
+            cycleDetected = true;
+        }
     }
-    color[u]=2; // black
+
+    color[u] = 2; // mark black (fully processed)
 }
 
-void solve() {
-    cin>>n>>m;
-    f(i,m) cin>>u>>v, g[u].pb(v);
-    loop(i, 1, n){
-        if(color[i] == 0){
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+        g[u].push_back(v); // directed edge
+    }
+
+    for (int i = 1; i <= n; i++) {
+        if (color[i] == 0) {
             dfs(i);
         }
     }
-    if(cycleDetected){
-        cout<<"Yes, there is/are cycle(s)";
-    }else{
-        cout<<"There is no cycle";
+
+    if (cycleDetected) {
+        cout << "Yes, there is/are cycle(s)";
+    } else {
+        cout << "There is no cycle";
     }
+
+    return 0;
 }
