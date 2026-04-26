ba456c1b4c06dc27bf39fb99bd3fe567d7579ab9
Ashwani Yadav <ashyadavash@gmail.com>
2026-04-27 02:06:55 +0530

    Update 00 Cycle detection in undirected graph.cpp

--- Graph/Cycle/00 Cycle detection in undirected graph.cpp
+++ Graph/Cycle/00 Cycle detection in undirected graph.cpp
@@ -1,18 +1,50 @@
-bool dfs (ll u, ll par) {
-	vis[u] = 1;
-	for (auto v: g[u]) {
-		if (!vis[v]) {
-			if (dfs(v, u)) return true;
-		} else {
-			if(v != par) return true;
-		}
-	}
-	return false;
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+const int N = 1e5 + 5;
+
+vector<int> g[N];
+bool visited[N];
+bool cycleDetected = false;
+
+void dfs(int u, int parent) {
+    visited[u] = true;
+
+    for (int v : g[u]) {
+        if (!visited[v]) {
+            dfs(v, u);
+        } 
+        else if (v != parent) {
+            // visited and not parent → cycle exists
+            cycleDetected = true;
+        }
+    }
 }
 
-void solve() {
-	cin >> n >> m;
-	f(i, m) cin >> u >> v, g[u].pb(v), g[v].pb(u);
-	if (dfs(1, 0)) cout << "Cyclic";
-	else cout << "Acyclic";
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+        g[u].push_back(v);
+        g[v].push_back(u); // undirected graph
+    }
+
+    for (int i = 1; i <= n; i++) {
+        if (!visited[i]) {
+            dfs(i, -1);
+        }
+    }
+
+    if (cycleDetected) {
+        cout << "Yes, there is/are cycle(s)";
+    } else {
+        cout << "There is no cycle";
+    }
+
+    return 0;
 }
