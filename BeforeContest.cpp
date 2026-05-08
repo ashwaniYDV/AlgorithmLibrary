d49fd6956896ed85852ee0ec45e798e52eecd3ec
Ashwani Yadav <ashyadavash@gmail.com>
2026-05-08 21:14:10 +0530

    Update BeforeContest.cpp

--- BeforeContest.cpp
+++ BeforeContest.cpp
@@ -133,7 +133,46 @@ struct Sieve {
     }
 };
 
+struct SPF {
+    vector<int> spf;
 
+    SPF(int n) {
+        spf.resize(n + 1);
+
+        for(int i = 0; i <= n; i++) {
+            spf[i] = i;
+        }
+
+        for(int i = 2; i * i <= n; i++) {
+            if(spf[i] == i) {
+                for(int j = i * i; j <= n; j += i) {
+                    if(spf[j] == j) {
+                        spf[j] = i;
+                    }
+                }
+            }
+        }
+    }
+
+    vector<int> getFactors(int x) {
+        vector<int> v;
+
+        while(x > 1) {
+            int p = spf[x];
+            v.push_back(p);
+
+            while(x % p == 0) {
+                x /= p;
+            }
+        }
+
+        return v;
+    }
+
+    bool isPrime(int x) {
+        return x > 1 && spf[x] == x;
+    }
+};
 
 
 
